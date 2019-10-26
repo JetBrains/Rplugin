655efe3e5a6ac8213fd598c80e1412b7692df4ed
Valery Teplyakov <Valeriy.Teplyakov@jetbrains.com>
2019-10-27 02:44:18 +0300

    Convert LiveTemplateProvider to kotlin
    GitOrigin-RevId: 4148285c1d62b9aac49922b5d3e55e498f4d58e7

---
+++ src/org/jetbrains/r/editor/LiveTemplateProvider.kt
@@ -0,0 +1,25 @@
+// Copyright (c) 2017, Holger Brandl, Ekaterina Tuzova
+
+
+/*
+ * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.r.editor
+
+import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider
+
+class LiveTemplateProvider : DefaultLiveTemplatesProvider {
+
+  override fun getDefaultLiveTemplateFiles(): Array<String> {
+    return DEFAULT_TEMPLATES
+  }
+
+  override fun getHiddenLiveTemplateFiles(): Array<String>? {
+    return null
+  }
+
+  companion object {
+    private val DEFAULT_TEMPLATES = arrayOf("/liveTemplates/rtemplates", "/liveTemplates/surround")
+  }
+}
