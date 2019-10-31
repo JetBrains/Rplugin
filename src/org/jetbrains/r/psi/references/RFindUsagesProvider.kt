12c22be8c320fcdb726e5ea4f572117fffa92abc
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-10-31 12:39:21 +0300

    Convert Find Usages Provider to Kotlin
    GitOrigin-RevId: 6f63a871af5f66a4a183c2afc42d824d76e18491

--- src/org/jetbrains/r/psi/references/RFindUsagesProvider.kt
+++ src/org/jetbrains/r/psi/references/RFindUsagesProvider.kt
@@ -3,77 +3,66 @@
  * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
-package org.jetbrains.r.psi.references;
+package org.jetbrains.r.psi.references
 
-import com.intellij.lang.cacheBuilder.DefaultWordsScanner;
-import com.intellij.lang.cacheBuilder.WordsScanner;
-import com.intellij.lang.findUsages.FindUsagesProvider;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiNamedElement;
-import com.intellij.psi.tree.TokenSet;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.r.lexer.RLexer;
-import org.jetbrains.r.parsing.RElementTypes;
-import org.jetbrains.r.parsing.RParserDefinition;
-import org.jetbrains.r.psi.api.*;
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner
+import com.intellij.lang.cacheBuilder.WordsScanner
+import com.intellij.lang.findUsages.FindUsagesProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.tree.TokenSet
+import org.jetbrains.r.lexer.RLexer
+import org.jetbrains.r.parsing.RElementTypes
+import org.jetbrains.r.parsing.RParserDefinition
+import org.jetbrains.r.psi.api.RAssignmentStatement
+import org.jetbrains.r.psi.api.RFunctionExpression
+import org.jetbrains.r.psi.api.RIdentifierExpression
+import org.jetbrains.r.psi.api.RParameter
 
-public class RFindUsagesProvider implements FindUsagesProvider {
-    @Nullable
-    @Override
-    public WordsScanner getWordsScanner() {
-        return new DefaultWordsScanner(new RLexer(), TokenSet.create(RElementTypes.R_IDENTIFIER),
-                TokenSet.create(RParserDefinition.END_OF_LINE_COMMENT),
-                TokenSet.create(RElementTypes.R_STRING_LITERAL_EXPRESSION));
+class RFindUsagesProvider : FindUsagesProvider {
+  override fun getWordsScanner(): WordsScanner? {
+    return DefaultWordsScanner(RLexer(), TokenSet.create(RElementTypes.R_IDENTIFIER),
+                               TokenSet.create(RParserDefinition.END_OF_LINE_COMMENT),
+                               TokenSet.create(RElementTypes.R_STRING_LITERAL_EXPRESSION))
+  }
+
+
+  override fun canFindUsagesFor(psiElement: PsiElement): Boolean {
+    //        isLibraryFile(psiElement.getContainingFile())
+    return psiElement is PsiNamedElement || psiElement is RIdentifierExpression
+  }
+
+
+  override fun getHelpId(psiElement: PsiElement): String? {
+    return null
+  }
+
+
+  override fun getType(element: PsiElement): String {
+    if (element is RAssignmentStatement) {
+      val assignedValue = element.assignedValue
+      if (assignedValue is RFunctionExpression) {
+        return "function"
+      }
     }
 
+    return if (element is RParameter) "function parameter" else "variable"
 
-    @Override
-    public boolean canFindUsagesFor(@NotNull PsiElement psiElement) {
-//        isLibraryFile(psiElement.getContainingFile())
-        return psiElement instanceof PsiNamedElement || psiElement instanceof RIdentifierExpression;
+  }
+
+
+  override fun getDescriptiveName(element: PsiElement): String {
+    if (element is RAssignmentStatement) {
+      val assignee = element.assignee
+      return assignee!!.text
     }
 
-
-    @Nullable
-    @Override
-    public String getHelpId(@NotNull PsiElement psiElement) {
-        return null;
-    }
+    // this will be used e.g. when renaming function parameters
+    return element.text
+  }
 
 
-    @NotNull
-    @Override
-    public String getType(@NotNull PsiElement element) {
-        if (element instanceof RAssignmentStatement) {
-            RPsiElement assignedValue = ((RAssignmentStatement) element).getAssignedValue();
-            if (assignedValue instanceof RFunctionExpression) {
-                return "function";
-            }
-        }
-
-        if (element instanceof RParameter) return "function parameter";
-
-        return "variable";
-    }
-
-
-    @NotNull
-    @Override
-    public String getDescriptiveName(@NotNull PsiElement element) {
-        if (element instanceof RAssignmentStatement) {
-            PsiElement assignee = ((RAssignmentStatement) element).getAssignee();
-            return assignee.getText();
-        }
-
-        // this will be used e.g. when renaming function parameters
-        return element.getText();
-    }
-
-
-    @NotNull
-    @Override
-    public String getNodeText(@NotNull PsiElement element, boolean useFullName) {
-        return element.getText();
-    }
+  override fun getNodeText(element: PsiElement, useFullName: Boolean): String {
+    return element.text
+  }
 }
