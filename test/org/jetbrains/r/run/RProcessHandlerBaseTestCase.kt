305c9759391cb7ffd6b3cfc1331ebe407a71ded4
Mikhail Putilin <mikhail.putilin@jetbrains.com>
2019-10-31 16:31:47 +0300

    Cache RInterop between tests
    GitOrigin-RevId: cfb937bb33cac4eebaa13b227324d5a8198efc8b

--- test/org/jetbrains/r/run/RProcessHandlerBaseTestCase.kt
+++ test/org/jetbrains/r/run/RProcessHandlerBaseTestCase.kt
@@ -4,11 +4,16 @@
 
 package org.jetbrains.r.run
 
+import com.intellij.execution.process.ProcessEvent
+import com.intellij.execution.process.ProcessListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.Key
 import org.jetbrains.r.RUsefulTestCase
 import org.jetbrains.r.rinterop.RInterop
 import org.jetbrains.r.rinterop.RInteropUtil
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeoutException
 
 abstract class RProcessHandlerBaseTestCase : RUsefulTestCase() {
   protected lateinit var rInterop: RInterop
@@ -16,20 +21,51 @@ abstract class RProcessHandlerBaseTestCase : RUsefulTestCase() {
   override fun setUp() {
     super.setUp()
     rInterop = getRInterop(project)
-    // we want be sure that the interpreter is initialized
-    rInterop.executeCode("1")
-  }
-
-  override fun tearDown() {
-    if (!Disposer.isDisposed(rInterop)) {
-      Disposer.dispose(rInterop)
-    }
-    super.tearDown()
   }
 
   companion object {
+    @Volatile private var cachedInterop: RInterop? = null
+
     private fun getRInterop(project: Project): RInterop {
-      return RInteropUtil.runRWrapperAndInterop(project).blockingGet(3000)!!
+      var rInterop = cachedInterop
+      if (rInterop == null || !validateRInterop(rInterop)) {
+        if (rInterop != null && !Disposer.isDisposed(rInterop)) {
+          Disposer.dispose(rInterop)
+        }
+        rInterop = RInteropUtil.runRWrapperAndInterop(project).blockingGet(3000)!!
+        cachedInterop = rInterop
+        rInterop.processHandler.addProcessListener(object : ProcessListener {
+          override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
+          }
+
+          override fun processTerminated(event: ProcessEvent) {
+            cachedInterop = null
+          }
+
+          override fun processWillTerminate(event: ProcessEvent, willBeDestroyed: Boolean) {
+          }
+
+          override fun startNotified(event: ProcessEvent) {
+          }
+        })
+      } else {
+        rInterop.clearEnvironment(rInterop.globalEnvRef)
+      }
+      return rInterop
+    }
+
+    private fun validateRInterop(rInterop: RInterop): Boolean {
+      return try {
+        val output = StringBuilder()
+        rInterop.executeCodeAsync("cat(1)") { it, _ ->
+          output.append(it)
+        }.blockingGet(1000)
+        output.toString() == "1"
+      } catch (e: ExecutionException) {
+        false
+      } catch (e: TimeoutException) {
+        false
+      }
     }
   }
 }
