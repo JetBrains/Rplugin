8c8983d5520f05e9673a74832711c905c66bc286
Aleksandr Rasnyuk <aleksandr.rasnyuk@jetbrains.com>
2019-11-11 13:15:57 +0300

    Check version of saved graphics settings
    GitOrigin-RevId: c2a6293176d1451bfd69c6f0d18f18dd7270680a

--- src/org/jetbrains/r/settings/RGraphicsSettings.kt
+++ src/org/jetbrains/r/settings/RGraphicsSettings.kt
@@ -12,27 +12,29 @@ import java.awt.Dimension
 @State(name = "RGraphicsSettings", storages = [Storage("rGraphicsSettings.xml")])
 class RGraphicsSettings : SimplePersistentStateComponent<RGraphicsSettingsState>(RGraphicsSettingsState()) {
   companion object {
+    private const val CURRENT_VERSION = 1
+
     fun getInstance(project: Project) = project.service<RGraphicsSettings>()
 
     fun getScreenParameters(project: Project): RGraphicsUtils.ScreenParameters {
-      val state = getInstance(project).state
-      val width = state.width
-      val height = state.height
-      val resolution = state.resolution
-      return if (width == 0 || height == 0 || resolution == 0) {
-        RGraphicsUtils.getDefaultScreenParameters(false).also {
-          setScreenParameters(project, it)
+      return with(getInstance(project).state) {
+        if (width == 0 || height == 0 || resolution == 0 || version != CURRENT_VERSION) {
+          RGraphicsUtils.getDefaultScreenParameters(false).also {
+            setScreenParameters(project, it)
+          }
+        } else {
+          RGraphicsUtils.ScreenParameters(Dimension(width, height), resolution)
         }
-      } else {
-        RGraphicsUtils.ScreenParameters(Dimension(width, height), resolution)
       }
     }
 
     fun setScreenParameters(project: Project, parameters: RGraphicsUtils.ScreenParameters) {
-      val state = getInstance(project).state
-      state.width = parameters.width
-      state.height = parameters.height
-      state.resolution = parameters.resolution ?: 0
+      getInstance(project).state.apply {
+        width = parameters.width
+        height = parameters.height
+        resolution = parameters.resolution ?: 0
+        version = CURRENT_VERSION
+      }
     }
   }
 }
@@ -41,4 +43,5 @@ class RGraphicsSettingsState : BaseState() {
   var width: Int by property(0)
   var height: Int by property(0)
   var resolution: Int by property(0)
+  var version: Int by property(0)
 }
