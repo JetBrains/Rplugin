15155dd7fbf0335726a7f758882ef0555e4c500f
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2019-11-14 00:18:04 +0300

    RLightVirtualFileManager: use Document for updating file content
    R-352
    
    GitOrigin-RevId: 82889a8a936a1247af5b6f2440229742eefaca72

--- src/org/jetbrains/r/editor/RLightVirtualFileManager.kt
+++ src/org/jetbrains/r/editor/RLightVirtualFileManager.kt
@@ -4,10 +4,14 @@
 
 package org.jetbrains.r.editor
 
+import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.testFramework.LightVirtualFile
 import com.intellij.util.containers.ContainerUtil
 import org.jetbrains.r.RLanguage
@@ -16,18 +20,26 @@ class RLightVirtualFileManager(private val project: Project) {
   private val cache = ContainerUtil.createConcurrentSoftValueMap<String, LightVirtualFile>()
 
   fun openLightFileWithContent(fqn: String, name: String, methodContent: CharSequence): VirtualFile {
-    val virtualFile = getOrCreateLightFile(fqn, name, methodContent)
-    FileEditorManager.getInstance(project).openFile(virtualFile, true, true)
+    val virtualFile = getOrCreateLightFile(fqn, name)
+    val openFile = FileEditorManager.getInstance(project).openFile(virtualFile, true, true)
+    invokeAndWaitIfNeeded { runWriteAction {
+      try {
+        virtualFile.isWritable = true
+        val first = openFile.first()
+        if (first is TextEditor) {
+          val document = first.editor.document
+          document.setText(methodContent)
+          PsiDocumentManager.getInstance(project).commitDocument(document)
+        }
+      } finally {
+        virtualFile.isWritable = false
+      }
+    } }
     return virtualFile
   }
 
-  private fun getOrCreateLightFile(fqn: String, name: String, methodContent: CharSequence): LightVirtualFile {
-    val virtualFile = cache.computeIfAbsent(fqn) { LightVirtualFile(name, RLanguage.INSTANCE, "") }
-    virtualFile.isWritable = true
-    virtualFile.setContent(this, methodContent, false)
-    virtualFile.isWritable = false
-    return virtualFile
-  }
+  private fun getOrCreateLightFile(fqn: String, name: String): LightVirtualFile =
+    cache.computeIfAbsent(fqn) { LightVirtualFile(name, RLanguage.INSTANCE, "") }
 
   companion object {
     fun getInstance(project: Project) : RLightVirtualFileManager = project.service<RLightVirtualFileManager>()
