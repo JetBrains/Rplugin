43a924ab0f7bcc5e81432ae3b5c0de43cd537a9b
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-11-15 21:08:53 +0300

    Convert file to Kotlin and move it
    GitOrigin-RevId: 7a40c1276657ecc3f091c6564c09d9c6820cd214

--- src/org/jetbrains/r/editor/RBraceMatcher.kt
+++ src/org/jetbrains/r/editor/RBraceMatcher.kt
@@ -1,40 +1,28 @@
-// Copyright (c) 2017, Holger Brandl, Ekaterina Tuzova
 /*
  * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
-package org.jetbrains.r.highlighting;
+// Copyright (c) 2017, Holger Brandl, Ekaterina Tuzova
+package org.jetbrains.r.editor
 
-import com.intellij.lang.BracePair;
-import com.intellij.lang.PairedBraceMatcher;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.tree.IElementType;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.r.parsing.RElementTypes;
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import org.jetbrains.r.parsing.RElementTypes
 
-public class RBraceMatcher implements PairedBraceMatcher {
-    private final BracePair[] PAIRS = new BracePair[]{
-            new BracePair(RElementTypes.R_LPAR, RElementTypes.R_RPAR, false),
-            new BracePair(RElementTypes.R_LBRACKET, RElementTypes.R_RBRACKET, false),
-            new BracePair(RElementTypes.R_LDBRACKET, RElementTypes.R_RDBRACKET, false),
-            new BracePair(RElementTypes.R_LBRACE, RElementTypes.R_RBRACE, false)};
+private val bracePairs = arrayOf(
+  BracePair(RElementTypes.R_LPAR, RElementTypes.R_RPAR, false),
+  BracePair(RElementTypes.R_LBRACKET, RElementTypes.R_RBRACKET, false),
+  BracePair(RElementTypes.R_LDBRACKET, RElementTypes.R_RDBRACKET, false),
+  BracePair(RElementTypes.R_LBRACE, RElementTypes.R_RBRACE, false))
 
+class RBraceMatcher : PairedBraceMatcher {
+  override fun getPairs(): Array<BracePair> = bracePairs
 
-    @Override
-    public BracePair[] getPairs() {
-        return PAIRS;
-    }
+  override fun getPairs(): Array<BracePair> = pairs
 
+  override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?): Boolean = true
 
-    @Override
-    public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType lbraceType, @Nullable IElementType contextType) {
-        return true;
-    }
-
-
-    @Override
-    public int getCodeConstructStart(final PsiFile file, int openingBraceOffset) {
-        return openingBraceOffset;
-    }
+  override fun getCodeConstructStart(file: PsiFile, openingBraceOffset: Int): Int = openingBraceOffset
 }
