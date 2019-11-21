513fc814c066de4bd24aa8f7b837a12fb7c230f7
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-11-21 16:40:38 +0300

    Cleanup utility file
    GitOrigin-RevId: b30b42d2b61d77fe97f2f7816fe9baa8de85a3c2

--- src/org/jetbrains/r/psi/RPsiUtil.kt
+++ src/org/jetbrains/r/psi/RPsiUtil.kt
@@ -5,29 +5,20 @@
 package org.jetbrains.r.psi
 
 import com.google.common.collect.Lists
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
-import com.intellij.psi.search.ProjectAndLibrariesScope
-import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.tree.TokenSet
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.util.containers.Predicate
 import org.jetbrains.r.packages.RSkeletonUtil
 import org.jetbrains.r.parsing.RElementTypes.*
 import org.jetbrains.r.psi.api.*
 import org.jetbrains.r.psi.impl.RAssignmentStatementImpl
-import org.jetbrains.r.psi.references.RReferenceBase
-import org.jetbrains.r.psi.stubs.RAssignmentNameIndex
 import org.jetbrains.r.psi.stubs.RParameterStub
 import org.jetbrains.r.skeleton.psi.RSkeletonBase
 
 
 object RPsiUtil {
 
-  val BUILTIN_CONSTANTS = TokenSet.create(
-    R_NA_LITERAL, R_NA_LITERAL, R_BOUNDARY_LITERAL, R_BOOLEAN_LITERAL, R_NULL_LITERAL)
   val RESERVED_WORDS = TokenSet.create(
     R_IF, R_ELSE, R_REPEAT,
     R_WHILE, R_FUNCTION, R_FOR,
@@ -39,35 +30,7 @@ object RPsiUtil {
     R_LE, R_AND, R_ANDAND, R_OR, R_OROR,
     R_LEFT_ASSIGN, R_RIGHT_ASSIGN, R_LIST_SUBSET, R_AT)
 
-  private val LOCAL_ASSIGNMENT = TokenSet.create(
-    R_LEFT_ASSIGN, R_RIGHT_ASSIGN)
-
-  private val LOG = Logger.getInstance(RPsiUtil::class.java)
-
-  val MINUTE = 60 * 1000
   private val PACKAGE_IMPORT_METHODS = Lists.newArrayList("require", "library")
-
-
-  fun getParametersExpressions(parameters: List<RParameter>): List<RExpression> {
-    return parameters.mapNotNull { it.variable }
-  }
-
-
-  fun getAssignmentStatement(parameter: RParameter): RAssignmentStatement? {
-    val functionExpression = getFunction(parameter) ?: return null
-    val assignmentStatement = functionExpression.parent
-    return if (assignmentStatement == null || assignmentStatement !is RAssignmentStatement) {
-      null
-    }
-    else assignmentStatement
-  }
-
-
-  fun getFunction(parameter: RParameter): RFunctionExpression? {
-    //TODO: check some conditions when we should stop
-    return PsiTreeUtil.getParentOfType(parameter, RFunctionExpression::class.java)
-  }
-
 
   /**
    * Returns true if `element` is the LHS of named argument in a function call.
@@ -77,20 +40,6 @@ object RPsiUtil {
     return parent is RNamedArgument && parent.identifyingElement == element
   }
 
-  /**
-   * Returns true if `element` is the LHS of named argument in a function call.
-   */
-  fun isVarDeclaration(element: RIdentifierExpression): Boolean {
-    val parent = element.parent
-
-    return if (parent !is RAssignmentStatement) {
-      false
-    }
-    else parent.assignee == element
-
-  }
-
-
   fun getFunction(callExpression: RCallExpression): RFunctionExpression? {
     val expression = callExpression.expression
     return if (expression is RIdentifierExpression) {
@@ -98,7 +47,6 @@ object RPsiUtil {
     }
     else null
   }
-
 
   private fun getFunctionFromReference(reference: PsiReference?): RFunctionExpression? {
     if (reference == null) {
@@ -112,79 +60,15 @@ object RPsiUtil {
     return PsiTreeUtil.getChildOfType(assignmentStatement, RFunctionExpression::class.java)
   }
 
-
-  fun getFunction(binaryExpression: ROperatorExpression): RFunctionExpression? {
-    val operator = PsiTreeUtil.getChildOfType(binaryExpression, org.jetbrains.r.psi.api.ROperator::class.java)
-    return if (operator != null) {
-      getFunctionFromReference(operator.reference)
-    }
-    else null
-  }
-
-
-  fun getAssignmentStatement(expression: RFunctionExpression): RAssignmentStatement? {
-    val assignmentStatement = expression.parent
-    return if (assignmentStatement is RAssignmentStatement) {
-      assignmentStatement
-    }
-    else null
-  }
-
-
   fun isReturn(expression: PsiElement): Boolean {
     return expression is RCallExpression && expression.expression.text in listOf("return", "base::return", "base:::return")
   }
-
-
-  fun findCall(project: Project, functionName: String, predicate: Predicate<RCallExpression>): RCallExpression? {
-    val scope = ProjectAndLibrariesScope(project)
-    val possibleDefinitions = RAssignmentNameIndex.find(functionName, project, scope)
-    var functionDefinition: RAssignmentStatement? = null
-    for (assignment in possibleDefinitions) {
-      if (assignment.assignedValue is RFunctionExpression) {
-        functionDefinition = assignment
-        break
-      }
-    }
-    if (functionDefinition == null) {
-      return null
-    }
-    for (reference in ReferencesSearch.search(functionDefinition, scope)) {
-      val referenceFrom = reference.element
-      val parent = referenceFrom.parent as? RCallExpression ?: continue
-      if (predicate.apply(parent)) {
-        return parent
-      }
-    }
-    return null
-  }
-
-
-  fun isNamespacePrefix(element: RIdentifierExpression): Boolean {
-    return element.parent is RNamespaceAccessExpression
-  }
-
 
   fun isReturnValue(o: PsiElement): Boolean {
     // the recursion is needed to resolve final blocks
     //        return PsiTreeUtil.nextVisibleLeaf(o) == null && isReturnValue(o.getParent());
     return PsiTreeUtil.getNextSiblingOfType(o, RExpression::class.java) == null
            && (o.parent is RFunctionExpression || o.parent != null && isReturnValue(o.parent))
-  }
-
-  /**
-   * Returns method name if `functionExpression` is not an anonymous function definition.
-   */
-  fun getName(functionExpression: RFunctionExpression): String? {
-    return if (functionExpression.parent is RAssignmentStatement) {
-      (functionExpression.parent as RAssignmentStatementImpl).name
-    }
-    else null
-
-  }
-
-  fun isLocalAssignment(rAssignmentStatement: RAssignmentStatement): Boolean {
-    return rAssignmentStatement.node.findChildByType(LOCAL_ASSIGNMENT) != null
   }
 
   fun isLibraryElement(element: PsiElement): Boolean {
@@ -212,10 +96,6 @@ object RPsiUtil {
 
   fun resolveCall(call: RCallExpression, isIncomplete: Boolean = true): List<RAssignmentStatement> {
     call.expression.reference?.let { reference ->
-      if (reference !is RReferenceBase<*>) {
-        return emptyList()
-      }
-
       val multiResolve = reference.multiResolve(isIncomplete)
       return multiResolve.map { it.element }.filterIsInstance<RAssignmentStatement>()
     }
@@ -252,8 +132,8 @@ fun RAssignmentStatement.getParameters(): List<RParameter> {
   if (!isFunctionDeclaration) {
     return emptyList()
   }
-  (this as? RAssignmentStatementImpl)?.greenStub?.let {
-    return it.childrenStubs.filterIsInstance(RParameterStub::class.java).map { it.psi }
+  (this as? RAssignmentStatementImpl)?.greenStub?.let { stub ->
+    return stub.childrenStubs.filterIsInstance(RParameterStub::class.java).map { it.psi }
   }
   // TODO: handle somehow binary skeleton case. Ticket R-257
   return (assignedValue as? RFunctionExpression)?.parameterList?.parameterList ?: emptyList()
