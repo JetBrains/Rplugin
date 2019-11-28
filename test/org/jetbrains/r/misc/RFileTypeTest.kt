83e7db45153c39092382001f2152525cd5950ed5
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2019-11-28 13:02:32 +0300

    add Rprofile as an extension for R file type R-225
    GitOrigin-RevId: e70ab6dad7334a674675d87136d01ca9e32d8c95

---
+++ test/org/jetbrains/r/misc/RFileTypeTest.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.r.misc
+
+import com.intellij.openapi.fileTypes.FileTypeManager
+import junit.framework.TestCase
+import org.jetbrains.r.RFileType
+import org.jetbrains.r.RUsefulTestCase
+
+class RFileTypeTest : RUsefulTestCase() {
+
+  fun testRFileTypes() {
+    TestCase.assertEquals(RFileType, FileTypeManager.getInstance ().getFileTypeByFileName("Foo.R"))
+    TestCase.assertEquals(RFileType, FileTypeManager.getInstance ().getFileTypeByFileName(".Rprofile"))
+    TestCase.assertEquals(RFileType, FileTypeManager.getInstance ().getFileTypeByFileName("Foo.r"))
+    TestCase.assertEquals(RFileType, FileTypeManager.getInstance ().getFileTypeByFileName(".rprofile"))
+  }
+}
