451e465635a89b74432a71d4a595434ac60e0865
Aleksandr Rasnyuk <aleksandr.rasnyuk@jetbrains.com>
2019-11-29 17:13:21 +0300

    R-381 Fix RMarkdown inline toolbar positioning when soft wrap is on
    GitOrigin-RevId: ca69d1f77fc3765273f60253c662a399568e547c

--- visualisation/src/org/intellij/datavis/ui/InlineToolbar.kt
+++ visualisation/src/org/intellij/datavis/ui/InlineToolbar.kt
@@ -8,6 +8,8 @@ import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ex.EditorGutterComponentEx
+import com.intellij.openapi.editor.ex.SoftWrapChangeListener
+import com.intellij.openapi.editor.impl.SoftWrapModelImpl
 import com.intellij.psi.PsiElement
 import java.awt.*
 import javax.swing.JPanel
@@ -17,6 +19,7 @@ class InlineToolbar(val cell: PsiElement,
                     private val editor: Editor,
                     toolbarActions: ActionGroup) : JPanel(BorderLayout()) {
 
+  private var numSoftWraps = 0
   private var couldUpdateBound = false
   private val actionToolBar = ActionManager.getInstance().createActionToolbar("InlineToolbar", toolbarActions, true).apply {
     setTargetComponent(editor.contentComponent)
@@ -30,6 +33,28 @@ class InlineToolbar(val cell: PsiElement,
     component.background = Color(0, 0, 0, 0)
     component.cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
     add(component)
+    addSoftWrapListener()
+  }
+
+  private fun addSoftWrapListener() {
+    val softWrapModel = editor.softWrapModel as SoftWrapModelImpl
+    softWrapModel.addSoftWrapChangeListener(object : SoftWrapChangeListener {
+      override fun softWrapsChanged() {
+        if (!softWrapModel.isSoftWrappingEnabled && numSoftWraps > 0) {
+          numSoftWraps = 0
+          updateBounds()
+        }
+      }
+
+      override fun recalculationEnds() {
+        val newNumSoftWraps = softWrapModel.registeredSoftWraps.size
+        if (softWrapModel.isSoftWrappingEnabled && numSoftWraps != newNumSoftWraps) {
+          numSoftWraps = newNumSoftWraps
+          updateBounds()
+        }
+      }
+    })
+    numSoftWraps = if (softWrapModel.isSoftWrappingEnabled) softWrapModel.registeredSoftWraps.size else 0
   }
 
   override fun paint(g: Graphics) {
@@ -50,7 +75,8 @@ class InlineToolbar(val cell: PsiElement,
     val editorBounds = editor.component.bounds
     val deltaY = (toolbarHeight - editor.lineHeight) / 2
     val newToolbarX = editorBounds.x + editorBounds.width - toolbarWidth - gutterWidth
-    val newToolbarY = editor.visualLineToY(editor.document.getLineNumber(cell.textRange.endOffset)) - deltaY
+    val visual = editor.offsetToVisualPosition(cell.textRange.endOffset - 1)
+    val newToolbarY = editor.visualLineToY(visual.line) - deltaY
     try {
       couldUpdateBound = true
       bounds = Rectangle(newToolbarX, newToolbarY, toolbarWidth, toolbarHeight)
