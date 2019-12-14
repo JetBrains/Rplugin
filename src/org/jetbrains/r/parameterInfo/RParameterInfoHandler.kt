9a123bce3f2f4fb399401e7c85f4de5e5911b9aa
Valery Teplyakov <Valeriy.Teplyakov@jetbrains.com>
2019-12-14 21:30:28 +0300

    R-455 Parameter hinting
    GitOrigin-RevId: 132a21e7eac87b8d5b96aa7eeb8883383a6fd6cd

---
+++ src/org/jetbrains/r/parameterInfo/RParameterInfoHandler.kt
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.r.parameterInfo
+
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.lang.parameterInfo.*
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.elementType
+import icons.org.jetbrains.r.RBundle
+import org.jetbrains.r.parsing.RElementTypes
+import org.jetbrains.r.psi.RElementFactory
+import org.jetbrains.r.psi.RPsiUtil
+import org.jetbrains.r.psi.api.RArgumentList
+import org.jetbrains.r.psi.api.RCallExpression
+import org.jetbrains.r.psi.api.RFunctionExpression
+import org.jetbrains.r.psi.api.RNamedArgument
+
+class RParameterInfoHandler : ParameterInfoHandler<RArgumentList, RParameterInfoHandler.RParameterInfoArgumentList> {
+
+  override fun couldShowInLookup(): Boolean = true
+  override fun getParametersForLookup(item: LookupElement?, context: ParameterInfoContext?) = emptyArray<Any>()
+
+  override fun findElementForUpdatingParameterInfo(context: UpdateParameterInfoContext): RArgumentList? {
+    return findArgumentList(context, context.parameterListStart)
+  }
+
+  override fun findElementForParameterInfo(context: CreateParameterInfoContext): RArgumentList? {
+    val argumentList = findArgumentList(context) ?: return null
+    context.itemsToShow = RPsiUtil.resolveCall(argumentList.parent as RCallExpression).map { assignment ->
+      val args = (RElementFactory.createRPsiElementFromText(context.project,
+                                                            "function${assignment.functionParameters}") as RFunctionExpression).parameterList.parameterList
+      val names = args.map { it.name }
+      val values = args.map { it.defaultValue?.text }
+      RParameterInfoArgumentList(names, values, names.indices.toList())
+    }.toTypedArray()
+
+    return argumentList
+  }
+
+  override fun showParameterInfo(element: RArgumentList, context: CreateParameterInfoContext) {
+    context.showHint(element, element.textOffset, this)
+  }
+
+  override fun updateParameterInfo(parameterOwner: RArgumentList, context: UpdateParameterInfoContext) {
+    val carriageOffset = context.offset
+    if (!parameterOwner.textRange.contains(carriageOffset)) {
+      context.removeHint()
+      return
+    }
+
+    val expressions = parameterOwner.expressionList
+    val argumentNames = parameterOwner.namedArgumentList.map { it.name }
+    context.objectsToView.map { it as RParameterInfoArgumentList }.forEach {
+      var curArgIndex = 0
+      val skipNames = mutableSetOf<String>()
+      val probableNewPermutation = mutableListOf<Int>()
+      val names = it.names
+      it.currentArgumentIndex = 0
+      it.isDisabled = false
+
+      for (i in expressions.indices) {
+        val arg = expressions[i]
+        if (arg is RNamedArgument) {
+          if (arg.name in skipNames) {
+            // Multiple named argument with same name
+            it.isDisabled = true
+            break
+          }
+
+          skipNames.add(arg.name)
+          var ind = names.indexOf(arg.name)
+          if (ind == -1) {
+            ind = names.indexOf(DOTS)
+            if (ind == -1) {
+              // Unused argument
+              it.isDisabled = true
+              break
+            }
+          }
+
+          if (probableNewPermutation.lastOrNull() != ind) probableNewPermutation.add(ind)
+        } else {
+          while (curArgIndex < names.size && (names[curArgIndex] in skipNames || names[curArgIndex] in argumentNames)) ++curArgIndex
+          if (curArgIndex == names.size) {
+            // Too many arguments
+            it.isDisabled = true
+            break
+          }
+          if (names[curArgIndex] != DOTS) skipNames.add(names[curArgIndex])
+          if (probableNewPermutation.lastOrNull() != curArgIndex) probableNewPermutation.add(curArgIndex)
+        }
+
+        val stOffset = findPrevCommaOffset(arg) + 1
+        val fnOffset = findNextCommaOffset(arg)
+        if (carriageOffset in stOffset..fnOffset) it.currentArgumentIndex = probableNewPermutation.lastIndex
+      }
+
+      if (it.isDisabled) {
+        it.permutation = names.indices.toList()
+      } else {
+        names.forEachIndexed { ind, name ->
+          if (name != DOTS && name !in skipNames || name == DOTS && ind !in probableNewPermutation) probableNewPermutation.add(ind)
+        }
+        it.permutation = probableNewPermutation
+      }
+    }
+  }
+
+  override fun updateUI(argumentList: RParameterInfoArgumentList, context: ParameterInfoUIContext) {
+    var highlightOffsetStart = -1
+    var highlightOffsetEnd = 0
+    val isDisabled = argumentList.isDisabled
+    var text = buildString {
+      val (names, defaultValues, permutation, currentArgumentIndex) = argumentList
+      for (i in permutation.indices) {
+        if (i != 0) append(", ")
+        if (!isDisabled && currentArgumentIndex == i) highlightOffsetStart = length
+
+        val argumentInd = permutation[i]
+        if (argumentInd != i) append("[")
+        append(names[argumentInd])
+        defaultValues[argumentInd]?.let {
+          val renderedValue = when {
+            it.length <= MAX_DEFAULT_VALUE_LEN -> it
+            it.startsWith("\"") -> "\"$DOTS\""
+            it.startsWith("'") -> "'$DOTS'"
+            else -> DOTS
+          }
+          append(" = $renderedValue")
+        }
+        if (argumentInd != i) append("]")
+        if (!isDisabled && currentArgumentIndex == i) highlightOffsetEnd = length
+      }
+    }
+
+    if (text.isEmpty()) text = RBundle.message("parameter.info.no.parameters")
+    context.setupUIComponentPresentation(text, highlightOffsetStart, highlightOffsetEnd, isDisabled,
+                                         false, false, context.getDefaultParameterColor())
+  }
+
+  private fun findArgumentList(context: ParameterInfoContext, parameterListStart: Int = -1): RArgumentList? {
+    var argumentList = ParameterInfoUtils.findParentOfType(context.file, context.offset - 1, RArgumentList::class.java)
+    if (argumentList == null || parameterListStart < 0) return argumentList
+
+    while (parameterListStart != argumentList!!.textRange.startOffset) {
+      argumentList = PsiTreeUtil.getParentOfType(argumentList, RArgumentList::class.java) ?: return null
+      if (parameterListStart > argumentList.textRange.startOffset) return null
+    }
+
+    return argumentList
+  }
+
+  private fun findPrevCommaOffset(arg: PsiElement): Int {
+    var child = arg.prevSibling
+    while (child != null) {
+      if (child.elementType == RElementTypes.R_COMMA) return child.textOffset
+      child = child.prevSibling
+    }
+
+    return Int.MIN_VALUE
+  }
+
+  private fun findNextCommaOffset(arg: PsiElement): Int {
+    var child = arg.nextSibling
+    while (child != null) {
+      if (child.elementType == RElementTypes.R_COMMA) return child.textOffset
+      child = child.nextSibling
+    }
+
+    return Int.MAX_VALUE
+  }
+
+  data class RParameterInfoArgumentList(var names: List<String>,
+                                        val defaultValues: List<String?>,
+                                        var permutation: List<Int>,
+                                        var currentArgumentIndex: Int = -1,
+                                        var isDisabled: Boolean = false)
+
+  companion object {
+    const val DOTS = "..."
+    const val MAX_DEFAULT_VALUE_LEN = 32
+  }
+}
---
+++ test/org/jetbrains/r/parameterInfo/RParameterInfoHandlerTest.kt
@@ -0,0 +1,241 @@
+/*
+ * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.r.parameterInfo
+
+import com.intellij.openapi.actionSystem.IdeActions
+import com.intellij.testFramework.fixtures.EditorHintFixture
+import com.intellij.util.ui.UIUtil
+import icons.org.jetbrains.r.RBundle
+import org.jetbrains.r.RLightCodeInsightFixtureTestCase
+
+class RParameterInfoHandlerTest : RLightCodeInsightFixtureTestCase() {
+
+  override fun setUp() {
+    super.setUp()
+    addLibraries()
+  }
+
+  fun testEmptyList() {
+    doTest("print(<caret>)", "<x>, ...")
+  }
+
+  fun testLocalFunction() {
+    doTest("""
+      foo <- function(fst, snd, thd) { 42 }
+      
+      foo(40, 4<caret>1, 43)
+    """.trimIndent(), "fst, <snd>, thd")
+  }
+
+  fun testDefaultValues() {
+    doTest("""
+      foo <- function(fst = 42, snd, thd = f(call(42, "aba"), "daba"), fth = to_long_call(to_long_call(to_long_call(50)))) { 42 }
+      
+      foo(40, 41, 4<caret>3, 44)
+    """.trimIndent(), "fst = 42, snd, <thd = f(call(42, \"aba\"), \"daba\")>, fth = ...")
+
+    doTest("""
+      foo <- function(aa = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', bb = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") { 42 }
+      
+      foo(<caret>, "ba")
+    """.trimIndent(), "<aa = '...'>, bb = \"...\"")
+  }
+
+  fun testArgsPermutation() {
+    doTest("""
+      foo <- function(fst, snd, thd = 42, fth = foo(1, 2, 3, 4)) { 42 }
+      
+      foo(40, thd = 4<caret>1, snd = 43, 42)
+    """.trimIndent(), "fst, <[thd = 42]>, [snd], fth = foo(1, 2, 3, 4)")
+
+    doTest("""
+      foo <- function(fst, snd) { 42 }
+      
+      foo(40, fst = 4<caret>1)
+    """.trimIndent(), "[snd], <[fst]>")
+  }
+
+  fun testCaretAroundComma() {
+    val fooDeclaration = """
+      foo <- function(fst, snd) { 42 }
+    """.trimIndent()
+    doTest("""
+      $fooDeclaration
+      foo(41<caret>, 43)
+    """.trimIndent(), "<fst>, snd")
+
+    doTest("""
+      $fooDeclaration
+      foo(41,<caret> 43)
+    """.trimIndent(), "fst, <snd>")
+
+    doTest("""
+      $fooDeclaration
+      foo(41  $t $n $n   $t<caret>  $t , $t  $n  $t    43)
+    """.trimIndent(), "<fst>, snd")
+
+    doTest("""
+      $fooDeclaration
+      foo(41 $t   $n $n $t  ,  $t $n $n $t<caret>  $n $n $n $t  43)
+    """.trimIndent(), "fst, <snd>")
+  }
+
+  fun testCommaAroundBoundaries() {
+    val fooDeclaration = """
+      foo <- function(fst, snd) { 42 }
+    """.trimIndent()
+    doTest("""
+      $fooDeclaration
+      foo(<caret>41, 43)
+    """.trimIndent(), "<fst>, snd")
+
+    doTest("""
+      $fooDeclaration
+      foo(41,43<caret>)
+    """.trimIndent(), "fst, <snd>")
+
+    doTest("""
+      $fooDeclaration
+      foo($n   $n $t   $t<caret>$t $t $n   41 $t   $n $n $t  ,  $t $n $n $t   43 $n $t $t   )
+    """.trimIndent(), "<fst>, snd")
+
+    doTest("""
+      $fooDeclaration
+      foo($n   $n $t   $t$t $t $n   41 $t   $n $n $t  ,  $t $n $n $t   43 $n $t <caret> $t   )
+    """.trimIndent(), "fst, <snd>")
+  }
+
+  fun testNoParameters() {
+    doTest("""
+      foo <- function() { 42 }
+      
+      foo(<caret>)
+    """.trimIndent(), RBundle.message("parameter.info.no.parameters").replace("<", "&lt;").replace(">", "&gt;"))
+  }
+
+  fun testDots() {
+    val fooDeclaration = """
+      foo <- function(fst, snd, ..., thd) { 42 }
+    """.trimIndent()
+
+    doTest("""
+      $fooDeclaration
+      foo(41, 42, 4<caret>3, 44, 45, thd = 46)
+    """.trimIndent(), "fst, snd, <...>, thd")
+
+    doTest("""
+      $fooDeclaration
+      foo(41, 42, 43, 44, 4<caret>5, thd = 46)
+    """.trimIndent(), "fst, snd, <...>, thd")
+
+    doTest("""
+      $fooDeclaration
+      foo(41, 42, 43, 44, 45, thd =<caret> 46)
+    """.trimIndent(), "fst, snd, ..., <thd>")
+  }
+
+  fun testDotsWithPermutation() {
+    val fooDeclaration = """
+      foo <- function(fst, snd, ..., thd) { 42 }
+    """.trimIndent()
+
+    doTest("""
+      $fooDeclaration
+      foo(41, 42, 4<caret>3, thd = 46, 44)
+    """.trimIndent(), "fst, snd, <...>, thd, [...]")
+
+    doTest("""
+      $fooDeclaration
+      foo(41, 42, 43, thd = 4<caret>6, 44)
+    """.trimIndent(), "fst, snd, ..., <thd>, [...]")
+
+    doTest("""
+      $fooDeclaration
+      foo(41, 42, 43, thd = 46, 4<caret>4)
+    """.trimIndent(), "fst, snd, ..., thd, <[...]>")
+  }
+
+  fun testCallInCall() {
+    val fooBarDeclaration = """
+      foo <- function(fst, snd, thd) { 42 }
+      bar <- function(aaa, bbb, ccc) { 44 }
+    """.trimIndent()
+
+    doTest("""
+      $fooBarDeclaration
+      foo(13, bar(1<caret>0, 9, 8), "aba")
+    """.trimIndent(), "<aaa>, bbb, ccc")
+
+    doTest("""
+      $fooBarDeclaration
+      foo(13, bar(10, 9, 8), "a<caret>ba")
+    """.trimIndent(), "fst, snd, <thd>")
+
+    doTest("""
+      $fooBarDeclaration
+      foo(13, b<caret>ar(10, 9, 8), "aba")
+    """.trimIndent(), "fst, <snd>, thd")
+  }
+
+  fun testNotCompletedValue() {
+    doTest("""
+      foo <- function(fst, snd, thd) { 42 }
+      foo(12, thd = <caret>)
+    """.trimIndent(), "fst, <[thd]>, [snd]")
+  }
+
+  fun testMultiVariant() {
+    doTest("""
+      filter(10, 15, 43<caret>)
+    """.trimIndent(),
+           "x, filter, <method = c(\"convolution\", \"recursive\")>, sides = 2, circular = FALSE, init = NULL",
+           ".data, <...>, .preserve = FALSE")
+  }
+
+  fun testDisable() {
+    val fooDeclaration = "foo <- function(aa, bb) { 42 }"
+    val result = "aa, bb"
+
+    doTest("""
+      $fooDeclaration
+      foo(<caret>aa = 10, aa = 20)
+    """.trimIndent(), result, isDisabled = true)
+
+    doTest("""
+      $fooDeclaration
+      foo(aa = 10, cc = 20<caret>)
+    """.trimIndent(), result, isDisabled = true)
+
+    doTest("""
+      $fooDeclaration
+      foo(aa = 10, <caret>20, 30)
+    """.trimIndent(), result, isDisabled = true)
+
+  }
+
+  fun doTest(text: String, vararg expectedResults: String, isDisabled: Boolean = false) {
+    val hintFixture = EditorHintFixture(myFixture.testRootDisposable)
+
+    myFixture.configureByText("a.R", text)
+    myFixture.performEditorAction(IdeActions.ACTION_EDITOR_SHOW_PARAMETER_INFO)
+    UIUtil.dispatchAllInvocationEvents()
+
+    assertEquals(wrap(*expectedResults, isDisabled = isDisabled), hintFixture.currentHintText)
+  }
+
+  companion object {
+    const val t = "\t"
+    const val n = "\n"
+
+    fun wrap(vararg results: String, isDisabled: Boolean = false): String {
+      return results.joinToString("\n-\n") { result ->
+        val replacedQuote = result.replace("\"", "&quot;")
+        val replacedHighlight = if (isDisabled) "<font color=a8a8a8>$replacedQuote</font>"
+        else Regex("<(.+)>").replace(replacedQuote) { "<b>${it.groupValues[1]}</b>" }
+        "<html>$replacedHighlight</html>"
+      }
+    }
+  }
+}
