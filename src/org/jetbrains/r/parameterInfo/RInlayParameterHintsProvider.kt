d3abed3dfe70847c02f71cdb0cd7a7207cff5c5e
Valery Teplyakov <Valeriy.Teplyakov@jetbrains.com>
2019-12-15 22:30:18 +0300

    R-475 Inlay parameter hints
    GitOrigin-RevId: 1b8f9a90aaac0e5d15bee94ddbde705d7c43738a

---
+++ src/org/jetbrains/r/parameterInfo/RInlayParameterHintsProvider.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.r.parameterInfo
+
+import com.intellij.codeInsight.hints.HintInfo
+import com.intellij.codeInsight.hints.InlayInfo
+import com.intellij.codeInsight.hints.InlayParameterHintsProvider
+import com.intellij.codeInsight.hints.Option
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import icons.org.jetbrains.r.RBundle
+import org.jetbrains.r.packages.RSkeletonUtil
+import org.jetbrains.r.psi.RPsiUtil
+import org.jetbrains.r.psi.api.*
+import org.jetbrains.r.skeleton.psi.RSkeletonAssignmentStatement
+
+@Suppress("UnstableApiUsage")
+class RInlayParameterHintsProvider : InlayParameterHintsProvider {
+
+  override fun getSupportedOptions(): List<Option> = listOf(PARAMETER_HINT_OPTION, WRAP_DOTS_OPTION)
+
+  override fun getDefaultBlackList(): Set<String> = setOf("(...)", "(x, ...)")
+
+  override fun getParameterHints(element: PsiElement?): List<InlayInfo> {
+    if (element !is RArgumentList || !PARAMETER_HINT_OPTION.isEnabled()) return emptyList()
+
+    val isWrapDots = WRAP_DOTS_OPTION.isEnabled()
+    val assignment = RPsiUtil.resolveCall(element.parent as RCallExpression).singleOrNull() ?: return emptyList()
+    val result = mutableListOf<InlayInfo>()
+
+    val parameterNames = assignment.parameterNameList
+    val dotsArgIndex = parameterNames.indexOf(DOTS)
+    var isLastDots = false
+    var lastDotsArgEndOffset = -1
+
+    fun wrapDotsIfNeed() {
+      if (!isWrapDots) return
+      val lastIndex = result.lastIndex
+      val prevOffset = result[lastIndex].offset
+      result[lastIndex] = InlayInfo("$DOTS(", prevOffset)
+      result.add(InlayInfo(")", lastDotsArgEndOffset))
+    }
+
+    val permutation = RParameterInfoUtil.getArgumentsPermutation(parameterNames, element).first
+    val expressions = element.expressionList
+    for (i in expressions.indices) {
+      val parameterIndex = permutation[i]
+      if (parameterIndex == -1) {
+        if (isLastDots) {
+          wrapDotsIfNeed()
+          isLastDots = false
+        }
+        continue
+      }
+
+      val arg = expressions[i]
+      if (parameterIndex == dotsArgIndex) {
+        lastDotsArgEndOffset = arg.textRange.endOffset
+        if (isLastDots) continue
+
+        result.add(InlayInfo(DOTS, arg.textOffset))
+        isLastDots = true
+      }
+      else {
+        if (isMustProvideNameHint(arg)) {
+          result.add(InlayInfo(parameterNames[parameterIndex], arg.textOffset))
+        }
+        else if (isLastDots) {
+          wrapDotsIfNeed()
+        }
+        isLastDots = false
+      }
+    }
+
+    return result
+  }
+
+  override fun getHintInfo(element: PsiElement): HintInfo? {
+    if (element !is RArgumentList) return null
+    val assignment = RPsiUtil.resolveCall(element.parent as RCallExpression).firstOrNull() ?: return null
+    val fullName = if (assignment is RSkeletonAssignmentStatement) {
+      RSkeletonUtil.parsePackageAndVersionFromSkeletonFilename(assignment.containingFile.name)!!.first + "::" + assignment.name
+    }
+    else assignment.name
+    return HintInfo.MethodInfo(fullName, assignment.parameterNameList)
+  }
+
+  override fun getInlayPresentation(inlayText: String): String {
+    return if (inlayText in listOf("...", "...(", ")")) inlayText else super.getInlayPresentation(inlayText)
+  }
+
+  override fun getBlacklistExplanationHTML(): String {
+    return RBundle.message("inlay.hints.blacklist.pattern.explanation")
+  }
+
+  companion object {
+    private val WRAP_DOTS_OPTION = Option("R_HINT_OPTION_WRAP_DOTS", RBundle.message("inlay.hints.wrap.dots.option.description"), true)
+    private val PARAMETER_HINT_OPTION = Option("R_HINT_OPTION_PARAMETER_HINT", RBundle.message("inlay.hints.parameter.hint.description"), true)
+
+    private const val DOTS = "..."
+
+    private fun isMustProvideNameHint(arg: PsiElement): Boolean {
+      return PsiTreeUtil.instanceOf(arg, RStringLiteralExpression::class.java, RNumericLiteralExpression::class.java,
+                                    RBooleanLiteral::class.java, RNullLiteral::class.java, RNaLiteral::class.java,
+                                    RBoundaryLiteral::class.java)
+             || arg is RIdentifierExpression && arg.name in listOf("T", "F")
+    }
+  }
+}
---
+++ test/org/jetbrains/r/parameterInfo/RInlayParameterHintsProviderTest.kt
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.r.parameterInfo
+
+import com.intellij.codeInsight.hints.getLanguageForSettingKey
+import com.intellij.codeInsight.hints.settings.ParameterNameHintsSettings
+import org.jetbrains.r.RLanguage
+import org.jetbrains.r.RLightCodeInsightFixtureTestCase
+
+@Suppress("UnstableApiUsage")
+class RInlayParameterHintsProviderTest : RLightCodeInsightFixtureTestCase() {
+
+  override fun setUp() {
+    super.setUp()
+    addLibraries()
+  }
+
+  fun testParameterNames() {
+    doParameterNameTest("""
+      foo <- function(a, b, c) { a + length(b) + c }
+      
+      foo(<hint text="a:"/>42, <hint text="b:"/>"abacaba", <hint text="c:"/>15)
+    """.trimIndent())
+  }
+
+  fun testNamedArguments() {
+    doParameterNameTest("""
+      foo <- function(a, b, c) { a + length(b) + c }
+      
+      foo(<hint text="a:"/>42, b = "abacaba", <hint text="c:"/>15)
+    """.trimIndent())
+  }
+
+  fun testConstants() {
+    doParameterNameTest("""
+      foo <- function(a, b, c, d, e, f, g, h, i, j, k, l1, l2, l3, l4, l5) { }
+      
+      foo(<hint text="a:"/>"abacaba", <hint text="b:"/>42, <hint text="c:"/>TRUE, <hint text="d:"/>F, <hint text="e:"/>NULL,
+          <hint text="f:"/>NA, <hint text="g:"/>NA_complex_, <hint text="h:"/>Inf, <hint text="i:"/>NA, <hint text="j:"/>NaN, 
+          function(a, b) { }, callll(), 1:100, 5 + 5, l5)
+    """.trimIndent())
+  }
+
+  fun testPermutation() {
+    doParameterNameTest("""
+      foo <- function(a, b, c, d) { length(a) + b + c + d }
+      
+      foo(c = 42, <hint text="a:"/>"abacaba", b = 43, <hint text="d:"/>15)
+    """.trimIndent())
+  }
+
+  fun testDots() {
+    doParameterNameTest("""
+      foo <- function(a, b, ..., d) { a + b + d }
+      
+      foo(<hint text="a:"/>42, d = 40, <hint text="b:"/>33, <hint text="..."/>bb = 43, aa = 20, aa = 15, 15, 20)
+    """.trimIndent())
+  }
+
+  fun testWrappedDots() {
+    doParameterNameTest("""
+      foo <- function(a, b, ..., d) { a + b + d }
+      
+      foo(<hint text="a:"/>42, <hint text="b:"/>33, <hint text="...("/>bb = 43, aa = 20, aa = 15, 15, 20<hint text=")"/>, d = 40)
+    """.trimIndent())
+  }
+
+  fun testUnwrappedDots() {
+    doParameterNameWithoutDotsWrapTest("""
+      foo <- function(a, b, ..., d) { a + b + d }
+      
+      foo(<hint text="a:"/>42, <hint text="b:"/>33, <hint text="..."/>bb = 43, aa = 20, aa = 15, 15, 20, d = 40)
+    """.trimIndent())
+  }
+
+  fun testManyDotsBlocks() {
+    doParameterNameTest("""
+      foo <- function(a, b, ..., d) { a + b + d }
+      
+      foo(<hint text="...("/>bb = 43, aa = 20<hint text=")"/>, a = 42, <hint text="...("/>aa = 15<hint text=")"/>, b = 40, d = 40, <hint text="..."/>15, 20)
+    """.trimIndent())
+  }
+
+  fun testLines() {
+    doParameterNameTest("""
+      foo <- function(a, foo, ..., d) { a + b + d }
+      bar <- function(mode, len) { 100500 }
+      
+      foo(<hint text="a:"/>20,
+          function() {
+            42 + 43 + 44 + bar(<hint text="mode:"/>"normal", <hint text="len:"/>500)
+          },
+          <hint text="...("/>aa = 15, bb = 20, cc = 60<hint text=")"/>,
+          d = "wiwi")
+    """.trimIndent())
+  }
+
+  fun testLibraryFunction() {
+    doParameterNameTest("""
+      data.table::data.table(<hint text="...("/>aa = 100<hint text=")"/>, keep.rownames = FALSE, <hint text="..."/>aa = 20)
+      stats::filter(1:100, rep(1, 3), <hint text="method:"/>"convolution", <hint text="sides:"/>2, <hint text="circular:"/>FALSE)
+    """.trimIndent())
+  }
+
+  fun testIgnoreNamespace() {
+    addIgnorePatterns("stats::*")
+
+    doParameterNameTest("""
+      stats::binomial("logit")
+      stats::filter(1:100, rep(1, 3), "convolution", 2, FALSE)
+      dplyr::filter(dplyr::tibble(), <hint text="..."/>aa == 12, bb == 13)
+    """.trimIndent())
+  }
+
+  fun testIgnoreFunctionFromNamespace() {
+    addIgnorePatterns("stats::filter")
+
+    doParameterNameTest("""
+      stats::binomial(<hint text="link:"/>"logit")
+      stats::filter(1:100, rep(1, 3), "convolution", 2, FALSE)
+      dplyr::filter(dplyr::tibble(), <hint text="..."/>aa == 12, bb == 13)
+    """.trimIndent())
+  }
+
+  fun testIgnoreFunction() {
+    addIgnorePatterns("*filter")
+
+    doParameterNameTest("""
+      stats::binomial(<hint text="link:"/>"logit")
+      stats::filter(1:100, rep(1, 3), "convolution", 2, FALSE)
+      dplyr::filter(dplyr::tibble(), aa == 12, bb == 13)
+      dplyr::tally(dplyr::tibble(), <hint text="wt:"/>NULL)
+    """.trimIndent())
+  }
+
+  fun testIgnoreFunctionsWithParameters() {
+    addIgnorePatterns("(x, y)")
+
+    doParameterNameTest("""
+      foo <- function(x, y) { }
+      bar <- function(x, y, z) { }
+      
+      foo(10, 20)
+      bar(<hint text="x:"/>10, <hint text="y:"/>20, <hint text="z:"/>30)
+    """.trimIndent())
+  }
+
+  private fun doParameterNameTest(text: String) {
+    enableHints("R_HINT_OPTION_PARAMETER_HINT", "R_HINT_OPTION_WRAP_DOTS")
+    doTest(text)
+  }
+
+  private fun doParameterNameWithoutDotsWrapTest(text: String) {
+    enableHints("R_HINT_OPTION_PARAMETER_HINT")
+    doTest(text)
+  }
+
+  private fun enableHints(vararg names: String) {
+    val allHints = RInlayParameterHintsProvider().supportedOptions
+    allHints.forEach { if (it.id in names) it.set(true) else it.set(false) }
+  }
+
+  private fun addIgnorePatterns(vararg patterns: String) {
+    patterns.forEach {
+      ParameterNameHintsSettings.getInstance().addIgnorePattern(getLanguageForSettingKey(RLanguage.INSTANCE), it)
+    }
+  }
+
+  private fun doTest(text: String) {
+    myFixture.configureByText("a.R", text)
+    myFixture.testInlays()
+  }
+}
