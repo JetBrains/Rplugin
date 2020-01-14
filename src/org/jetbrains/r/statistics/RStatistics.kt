9cc6abf50acde6bb393419f02f2beae840c53de9
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-01-14 17:25:18 +0300

    Add available interpreter list to R statistics
    GitOrigin-RevId: fc3ce8b13f08013566ec066127bec2860938078e

--- src/org/jetbrains/r/statistics/RStatistics.kt
+++ src/org/jetbrains/r/statistics/RStatistics.kt
@@ -7,6 +7,8 @@ package org.jetbrains.r.statistics
 import com.intellij.internal.statistic.eventLog.FeatureUsageData
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
 import org.jetbrains.r.interpreter.RInterpreter
+import org.jetbrains.r.interpreter.RInterpreterInfo
+import org.jetbrains.r.interpreter.RInterpreterUtil
 import org.jetbrains.r.rinterop.RCondaUtil
 import java.io.File
 
@@ -24,10 +26,31 @@ object RStatistics {
   }
 
   fun logSetupInterpreter(interpreter: RInterpreter) {
+    val suggestedInterpreters = collectFoundInterpreters(interpreter)
+    val isConda = isConda(interpreter.interpreterPath)
     logEvent(RStatisticsEvent.SETUP_INTERPRETER) {
       addData("version", interpreter.version.toCompactString())
-      val isConda = RCondaUtil.findCondaByRInterpreter(File(interpreter.interpreterPath)) != null
       addData("is.conda", isConda)
+      addData("suggested", suggestedInterpreters)
     }
   }
+
+  private fun isConda(path: String) =
+    RCondaUtil.findCondaByRInterpreter(File(path)) != null
+
+  private fun collectFoundInterpreters(selected: RInterpreter): List<String> {
+    val allInterpreters = RInterpreterUtil.suggestAllInterpreters(false)
+    val selectedInfo: RInterpreterInfo? = allInterpreters.find { it.interpreterPath == selected.interpreterPath }
+    return if (selectedInfo == null) {
+      // WTF actually???
+      allInterpreters
+    }
+    else {
+      val others = allInterpreters - selectedInfo
+      (listOf(selectedInfo) + others)
+    }.map { infoToString(it) }
+  }
+
+  private fun infoToString(info: RInterpreterInfo): String =
+    """${info.version.toCompactString()}_${isConda(info.interpreterPath)}"""
 }
