5b4ad475b0e27a7c1c1a88f63d3312099d455721
Aleksandr Rasnyuk <aleksandr.rasnyuk@jetbrains.com>
2020-01-23 13:20:52 +0300

    Implement `RPackageVersion` class with functionality for version comparison
    GitOrigin-RevId: 8557310f5382c567424b1124cdcd2b239511c0b4

---
+++ src/org/jetbrains/r/packages/RPackageVersion.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.r.packages
+
+import kotlin.math.min
+
+data class RPackageVersion(val numbers: List<Int>) : Comparable<RPackageVersion> {
+  override fun compareTo(other: RPackageVersion): Int {
+    val minSize = min(numbers.size, other.numbers.size)
+    for (i in 0 until minSize) {
+      when {
+        numbers[i] > other.numbers[i] -> return GREATER
+        numbers[i] < other.numbers[i] -> return LESS
+      }
+    }
+    return when {
+      numbers.size > other.numbers.size -> GREATER
+      numbers.size < other.numbers.size -> LESS
+      else -> EQUAL
+    }
+  }
+
+  companion object {
+    private const val GREATER = 1
+    private const val EQUAL = 0
+    private const val LESS = -1
+
+    /**
+     * **Note:** blank version string is considered correct and yields [RPackageVersion] with empty [RPackageVersion.numbers] list
+     */
+    fun from(version: String): RPackageVersion? {
+      return if (version.isNotBlank()) {
+        val tokens = version.trim().split('.', '-')
+        val numbers = tokens.mapNotNull { it.toIntOrNull() }
+        if (numbers.size == tokens.size) RPackageVersion(numbers) else null
+      } else {
+        RPackageVersion(emptyList())
+      }
+    }
+
+    /**
+     * Compare two packages' version strings in *almost* (see notes) the same manner
+     * as the R function `compareVersion`.
+     * **Note:** `null`s are treated as empty strings.
+     * **Note:** two `null`s are considered equal (contrary to `compareVersion`)
+     * @return negative for less, positive for greater, zero for equal
+     * and `null` if version strings are invalid
+     */
+    fun compare(aVersion: String?, bVersion: String?): Int? {
+      return from(aVersion ?: "")?.let { a ->
+        from(bVersion ?: "")?.let { b ->
+          a.compareTo(b)
+        }
+      }
+    }
+  }
+}
---
+++ test/org/jetbrains/r/packages/RPackageVersionTest.kt
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.r.packages
+
+import junit.framework.TestCase
+import org.jetbrains.r.RUsefulTestCase
+
+class RPackageVersionTest : RUsefulTestCase() {
+  fun testParseValidVersionString() {
+    val string2numbers = mapOf(
+      "" to emptyList<Int>(),
+      "1" to listOf(1),
+      "123" to listOf(123),
+      "1.2" to listOf(1, 2),
+      "12.34" to listOf(12, 34),
+      "1.2.3" to listOf(1, 2, 3),
+      "1.23-4" to listOf(1, 23, 4),
+      "1.2.3.4" to listOf(1, 2, 3, 4),
+      "1.23-45.6" to listOf(1, 23, 45, 6),
+      "12-345.6789-12345" to listOf(12, 345, 6789, 12345)
+    )
+    for ((string, numbers) in string2numbers) {
+      val version = RPackageVersion.from(string)
+      TestCase.assertNotNull("Cannot parse version string '$string'", version)
+      TestCase.assertEquals("Incorrect version numbers for string '$string'", numbers, version!!.numbers)
+    }
+  }
+
+  fun testParseInvalidVersionString() {
+    val strings = listOf(
+      ".",
+      "-",
+      ".-",
+      "1..2",
+      "12.-34",
+      "1...2",
+      "1.-.23",
+      "1.2.e"
+    )
+    for (string in strings) {
+      val version = RPackageVersion.from(string)
+      TestCase.assertNull("Incorrectly treated '$string' as a valid version string", version)
+    }
+  }
+
+  fun testCompareEqualValidVersionStrings() {
+    val samples = listOf(
+      Sample("", "", Order.EQUAL),
+      Sample(null, "", Order.EQUAL),
+      Sample(null, null, Order.EQUAL),
+      Sample("1.2-3", "1.2-3", Order.EQUAL),
+      Sample("1.2.3.4", "1.2-3.4", Order.EQUAL)  // Note: dot instead of hyphen
+    )
+    checkSamplesWithInversion(samples)
+  }
+
+  fun testCompareUnequalValidVersionStrings() {
+    val samples = listOf(
+      Sample(null, "1.2-3", Order.LESS),
+      Sample("1.2", "1.2.3", Order.LESS),
+      Sample("1.2.3", "1.2.4", Order.LESS),
+      Sample("1.9", "1.10.0", Order.LESS),
+      Sample("1.9.9", "1.10.0", Order.LESS),
+      Sample("1.9.9.9", "1.10.0", Order.LESS),
+      Sample("1.27", "2.1", Order.LESS)
+    )
+    checkSamplesWithInversion(samples)
+  }
+
+  fun testCompareInvalidVersionStrings() {
+    val samples = listOf(
+      Sample("", ".", null),
+      Sample(null, ".", null),
+      Sample(null, "1...2", null),
+      Sample("1.2.3", "1.2.e", null)
+    )
+    checkSamplesWithInversion(samples)
+  }
+
+  private fun checkSamplesWithInversion(samples: List<Sample>) {
+    checkSamples(samples.map { it.inverted })
+    checkSamples(samples)
+  }
+
+  private fun checkSamples(samples: List<Sample>) {
+    for (sample in samples) {
+      val actualOrder = RPackageVersion.compare(sample.aVersion, sample.bVersion).toOrder()
+      TestCase.assertEquals("Incorrect comparison of '${sample.aVersion}' and '${sample.bVersion}'", sample.expectedOrder, actualOrder)
+    }
+  }
+
+  private fun Int?.toOrder(): Order? {
+    return this?.let { value ->
+      when {
+        value > 0 -> Order.GREATER
+        value < 0 -> Order.LESS
+        else -> Order.EQUAL
+      }
+    }
+  }
+
+  private val Sample.inverted: Sample
+    get() = Sample(bVersion, aVersion, expectedOrder.inverted)
+
+  private val Order?.inverted: Order?
+    get() = this?.let { order ->
+      when (order) {
+        Order.LESS -> Order.GREATER
+        Order.GREATER -> Order.LESS
+        else -> Order.EQUAL
+      }
+    }
+
+  private data class Sample(val aVersion: String?, val bVersion: String?, val expectedOrder: Order?)
+
+  private enum class Order {
+    GREATER,
+    EQUAL,
+    LESS,
+  }
+}
