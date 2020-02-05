636720ac91179a02554c34303ab3eddbb8d3be83
Mikhail.Putilin <Mikhail.Putilin@jetbrains.com>
2020-02-05 17:14:52 +0300

    Add missing translateToUTF8 in variable loader
    GitOrigin-RevId: a898c69208dbd64fac0dba5d17d82bf3c27fc6a7

--- test/org/jetbrains/r/rinterop/RInteropUnicodeTest.kt
+++ test/org/jetbrains/r/rinterop/RInteropUnicodeTest.kt
@@ -10,30 +10,28 @@ import org.jetbrains.r.run.RProcessHandlerBaseTestCase
 class RInteropUnicodeTest : RProcessHandlerBaseTestCase() {
   fun testUnicode() {
     val s = "abc\u0448\u79c1 \u2203 x"
-    val name = "\u79c2"
-    rInterop.executeCode("`$name` <- '$s'")
-    TestCase.assertEquals("[1] \"$s\"", rInterop.executeCode("`$name`").stdout.trim())
-    TestCase.assertEquals(s, rInterop.executeCode("cat(`$name`)").stdout.trim())
-    TestCase.assertTrue(s in (RRef.expressionRef("`$name`", rInterop).getValueInfo() as RValueSimple).text)
-    TestCase.assertTrue(rInterop.globalEnvLoader.variables.any { it.name == name })
+    rInterop.executeCode("var <- '$s'")
+    TestCase.assertEquals("[1] \"$s\"", rInterop.executeCode("var").stdout.trim())
+    TestCase.assertEquals(s, rInterop.executeCode("cat(var)").stdout.trim())
+    TestCase.assertTrue(s in (RRef.expressionRef("var", rInterop).getValueInfo() as RValueSimple).text)
+    TestCase.assertTrue(rInterop.globalEnvLoader.variables.any { it.name == "var" })
   }
 
   fun testUnicodeEscape() {
     val s = "abc\u0448\u79c1 \u2203 x"
     val escaped = "abc\\u0448\\u79c1 \\u2203 x"
-    val name = "\u79c2"
-    rInterop.executeCode("`$name` <- '$escaped'")
-    TestCase.assertEquals("[1] \"$s\"", rInterop.executeCode("`$name`").stdout.trim())
-    TestCase.assertEquals(s, rInterop.executeCode("cat(`$name`)").stdout.trim())
-    TestCase.assertTrue(s in (RRef.expressionRef("`$name`", rInterop).getValueInfo() as RValueSimple).text)
-    TestCase.assertTrue(rInterop.globalEnvLoader.variables.any { it.name == name })
+    rInterop.executeCode("var <- '$escaped'")
+    TestCase.assertEquals("[1] \"$s\"", rInterop.executeCode("var").stdout.trim())
+    TestCase.assertEquals(s, rInterop.executeCode("cat(var)").stdout.trim())
+    TestCase.assertTrue(s in (RRef.expressionRef("var", rInterop).getValueInfo() as RValueSimple).text)
+    TestCase.assertTrue(rInterop.globalEnvLoader.variables.any { it.name == "var" })
   }
 
   fun testDataTable() {
     addLibraries()
     val s1 = "column \u79c1"
     val s2 = "value \u79c2"
-    val table = rInterop.dataFrameGetViewer(RRef.expressionRef("dplyr::tibble(`$s1` = '$s2')", rInterop)).blockingGet(DEFAULT_TIMEOUT)!!
+    val table = rInterop.dataFrameGetViewer(RRef.expressionRef("dplyr::tibble('$s1' = '$s2')", rInterop)).blockingGet(DEFAULT_TIMEOUT)!!
     TestCase.assertEquals(s1, table.getColumnName(1))
     TestCase.assertEquals(s2, table.getValueAt(0, 1))
   }
