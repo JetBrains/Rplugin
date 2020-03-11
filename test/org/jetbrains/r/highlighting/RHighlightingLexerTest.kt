64b52b5742bf0666287ff50f7221b72e8ba91f07
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2020-03-11 11:33:49 +0300

    [R] move RHighlightingLexerTest.kt to highlighting package
    GitOrigin-RevId: bf48231ab983f745502011b436c434c7a566e8df

--- test/org/jetbrains/r/highlighting/RHighlightingLexerTest.kt
+++ test/org/jetbrains/r/highlighting/RHighlightingLexerTest.kt
@@ -1,12 +1,13 @@
-// Copyright (c) 2017, Holger Brandl, Ekaterina Tuzova
 /*
- * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
-package org.jetbrains.r.lexer
+// Copyright (c) 2017, Holger Brandl, Ekaterina Tuzova
+package org.jetbrains.r.highlighting
 
 import com.intellij.lexer.Lexer
 import junit.framework.TestCase
+import org.jetbrains.r.lexer.RLexer
 
 class RHighlightingLexerTest : TestCase() {
 
