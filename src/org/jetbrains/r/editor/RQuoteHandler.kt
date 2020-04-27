7b07d807fdf6327e27baf54a3097391e8ec2eb4b
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-04-27 21:17:29 +0300

    Convert quote handler to Kotlin
    GitOrigin-RevId: 4ddb0fdd8bd2398a084aceac94c1e418cc3f45a7

--- src/org/jetbrains/r/editor/RQuoteHandler.kt
+++ src/org/jetbrains/r/editor/RQuoteHandler.kt
@@ -2,38 +2,32 @@
 /*
  * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
+package org.jetbrains.r.editor
 
-package org.jetbrains.r.editor;
+import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.highlighter.HighlighterIterator
+import org.jetbrains.r.parsing.RElementTypes
 
-import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.highlighter.HighlighterIterator;
-import org.jetbrains.r.parsing.RElementTypes;
-
-public class RQuoteHandler extends SimpleTokenSetQuoteHandler {
-    public RQuoteHandler() {
-        super(RElementTypes.R_STRING);
-    }
-
-
-    @Override
-    public boolean hasNonClosedLiteral(Editor editor, HighlighterIterator iterator, int offset) {
-        final Document document = editor.getDocument();
-        int lineEndOffset = document.getLineEndOffset(document.getLineNumber(offset));
-        if (offset < lineEndOffset) {
-            final CharSequence charSequence = document.getCharsSequence();
-            final char openQuote = charSequence.charAt(offset);
-            final int nextCharOffset = offset + 1;
-            if (nextCharOffset < lineEndOffset && charSequence.charAt(nextCharOffset) == openQuote) {
-                return true;
-            }
-            for (int i = nextCharOffset + 1; i < lineEndOffset; i++) {
-                if (charSequence.charAt(i) == openQuote) {
-                    return false;
-                }
-            }
+class RQuoteHandler : SimpleTokenSetQuoteHandler(RElementTypes.R_STRING) {
+  override fun hasNonClosedLiteral(editor: Editor,
+                                   iterator: HighlighterIterator,
+                                   offset: Int): Boolean {
+    val document = editor.document
+    val lineEndOffset = document.getLineEndOffset(document.getLineNumber(offset))
+    if (offset < lineEndOffset) {
+      val charSequence = document.charsSequence
+      val openQuote = charSequence[offset]
+      val nextCharOffset = offset + 1
+      if (nextCharOffset < lineEndOffset && charSequence[nextCharOffset] == openQuote) {
+        return true
+      }
+      for (i in nextCharOffset + 1 until lineEndOffset) {
+        if (charSequence[i] == openQuote) {
+          return false
         }
-        return true;
+      }
     }
+    return true
+  }
 }
