6ed7b0f98ac23292b936fcd388714917371fdd1a
Aleksandr Rasnyuk <aleksandr.rasnyuk@jetbrains.com>
2020-06-10 13:54:39 +0300

    [Inlays] A better way of removing dialog's border
    GitOrigin-RevId: 1195ac5085dcd7a4b04cb9ee08f08803d6749ac8

--- visualisation/src/org/intellij/datavis/r/inlays/components/BorderlessDialogWrapper.kt
+++ visualisation/src/org/intellij/datavis/r/inlays/components/BorderlessDialogWrapper.kt
@@ -7,10 +7,9 @@ package org.intellij.datavis.r.inlays.components
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.util.SystemInfo
-import com.intellij.util.ui.JBUI
 import javax.swing.Action
 import javax.swing.JComponent
-import javax.swing.JPanel
+import javax.swing.border.Border
 
 abstract class BorderlessDialogWrapper(project: Project, private val dialogTitle: String, ideModalityType: IdeModalityType) :
   DialogWrapper(project, null, true, ideModalityType, false)
@@ -18,21 +17,18 @@ abstract class BorderlessDialogWrapper(project: Project, private val dialogTitle
   override fun init() {
     super.init()
     title = dialogTitle
-    removeMarginsIfPossible()
   }
 
   override fun createActions(): Array<Action> {
     return if (SystemInfo.isMac) arrayOf(cancelAction, okAction) else arrayOf(okAction, cancelAction)
   }
 
+  override fun createContentPaneBorder(): Border? {
+    return null
+  }
+
   protected fun createOkCancelPanel(): JComponent {
     val buttons = createActions().map { createJButtonForAction(it) }
     return createButtonsPanel(buttons)
-  }
-
-  private fun removeMarginsIfPossible() {
-    (rootPane.contentPane as? JPanel)?.let { panel ->
-      panel.border = JBUI.Borders.empty()
-    }
   }
 }
