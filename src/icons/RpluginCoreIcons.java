809e91567ea5c383514eb9e319b740aa12578172
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2020-06-11 21:36:50 +0300

    IDEA-CR-63640: [R] add R plugin Core Icons
    GitOrigin-RevId: b6c5b3fbd55a4ea7f8f45a82a33c439462f9800c

---
+++ src/icons/RpluginCoreIcons.java
@@ -0,0 +1,76 @@
+package icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class RpluginCoreIcons {
+  private static @NotNull Icon load(@NotNull String path) {
+    return IconManager.getInstance().getIcon(path, RpluginCoreIcons.class);
+  }
+
+  /** 16x16 */ public static final @NotNull Icon ChangeWorkingDirectory = load("/icons/changeWorkingDirectory.svg");
+
+  public final static class Chart {
+    /** 16x16 */ public static final @NotNull Icon ChartAreaRange = load("/icons/chart/chartAreaRange.svg");
+    /** 16x16 */ public static final @NotNull Icon ChartBar = load("/icons/chart/chartBar.svg");
+    /** 16x16 */ public static final @NotNull Icon ChartBubble = load("/icons/chart/chartBubble.svg");
+    /** 16x16 */ public static final @NotNull Icon ChartLine = load("/icons/chart/chartLine.svg");
+    /** 16x16 */ public static final @NotNull Icon ChartPie = load("/icons/chart/chartPie.svg");
+    /** 16x16 */ public static final @NotNull Icon ChartScatter = load("/icons/chart/chartScatter.svg");
+    /** 16x16 */ public static final @NotNull Icon ChartStock = load("/icons/chart/chartStock.svg");
+
+  }
+
+  public final static class FileTypes {
+    /** 16x16 */ public static final @NotNull Icon HtmlNotebook = load("/icons/fileTypes/htmlNotebook.svg");
+    /** 16x16 */ public static final @NotNull Icon Presentation = load("/icons/fileTypes/presentation.svg");
+    /** 16x16 */ public static final @NotNull Icon RMarkdown = load("/icons/fileTypes/rMarkdown.svg");
+    /** 16x16 */ public static final @NotNull Icon Shiny = load("/icons/fileTypes/shiny.svg");
+
+  }
+
+  public final static class Graphics {
+    /** 16x16 */ public static final @NotNull Icon ConstraintProportions = load("/icons/graphics/constraintProportions.svg");
+
+  }
+
+  public final static class Packages {
+    /** 16x16 */ public static final @NotNull Icon UpgradeAll = load("/icons/packages/upgradeAll.svg");
+
+  }
+  /** 16x16 */ public static final @NotNull Icon R = load("/icons/r.svg");
+  /** 16x16 */ public static final @NotNull Icon R_logo_16 = load("/icons/r_logo_16.svg");
+  /** 16x16 */ public static final @NotNull Icon Render = load("/icons/render.svg");
+  /** 16x16 */ public static final @NotNull Icon Render_dark_1 = load("/icons/render_dark-1.svg");
+  /** 16x16 */ public static final @NotNull Icon RMarkdown = load("/icons/rMarkdown.svg");
+
+  public final static class Run {
+    /** 16x16 */ public static final @NotNull Icon DebugCurrent = load("/icons/run/debugCurrent.svg");
+    /** 16x16 */ public static final @NotNull Icon RestartJob = load("/icons/run/restartJob.svg");
+    /** 16x16 */ public static final @NotNull Icon RunAbove = load("/icons/run/runAbove.svg");
+    /** 16x16 */ public static final @NotNull Icon RunBelow = load("/icons/run/runBelow.svg");
+    /** 16x16 */ public static final @NotNull Icon RunCurrent = load("/icons/run/runCurrent.svg");
+    /** 16x16 */ public static final @NotNull Icon RunJob = load("/icons/run/runJob.svg");
+
+  }
+
+  public final static class Table {
+    /** 16x16 */ public static final @NotNull Icon Pagination = load("/icons/table/pagination.svg");
+
+  }
+
+  public final static class ToolWindow {
+    /** 13x13 */ public static final @NotNull Icon RConsole = load("/icons/toolWindow/RConsole.svg");
+    /** 13x13 */ public static final @NotNull Icon RGraph = load("/icons/toolWindow/RGraph.svg");
+    /** 13x13 */ public static final @NotNull Icon RHtml = load("/icons/toolWindow/RHtml.svg");
+    /** 13x13 */ public static final @NotNull Icon RPackages = load("/icons/toolWindow/RPackages.svg");
+    /** 13x13 */ public static final @NotNull Icon RTable = load("/icons/toolWindow/RTable.svg");
+
+  }
+}
