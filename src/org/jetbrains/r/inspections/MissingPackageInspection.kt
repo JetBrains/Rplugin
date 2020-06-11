08a27741c51aacc236804a04775a3c21985174da
Aleksandr Rasnyuk <aleksandr.rasnyuk@jetbrains.com>
2020-06-11 19:51:36 +0300

    [R] Suggest installing 'gifski' for 'gganimate'
    GitOrigin-RevId: 89e80ef642f705d35c66b3c3436c871fae458d3e

--- src/org/jetbrains/r/inspections/MissingPackageInspection.kt
+++ src/org/jetbrains/r/inspections/MissingPackageInspection.kt
@@ -7,15 +7,20 @@ package org.jetbrains.r.inspections
 
 import com.intellij.codeInspection.InspectionManager
 import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import org.jetbrains.annotations.Nls
 import org.jetbrains.r.RBundle
 import org.jetbrains.r.hints.parameterInfo.RParameterInfoUtil
 import org.jetbrains.r.intentions.InstallAllFileLibraryFix
+import org.jetbrains.r.intentions.InstallLibrariesFix
 import org.jetbrains.r.intentions.InstallLibraryFix
 import org.jetbrains.r.interpreter.RInterpreterManager
+import org.jetbrains.r.packages.RequiredPackage
+import org.jetbrains.r.packages.RequiredPackageInstaller
 import org.jetbrains.r.psi.RPsiUtil
 import org.jetbrains.r.psi.RRecursiveElementVisitor
 import org.jetbrains.r.psi.api.*
@@ -88,6 +93,23 @@ class MissingPackageInspection : RInspection() {
       val descriptionTemplate = RBundle.message("inspection.missingPackage.description", packageName)
       problemsHolder.registerProblem(elementForReporting, descriptionTemplate,
                                      InstallLibraryFix(packageName), InstallAllFileLibraryFix())
+    } else {
+      checkImplicitDependenciesFor(packageName, elementForReporting, problemsHolder)
+    }
+  }
+
+  private fun checkImplicitDependenciesFor(packageName: String, element: PsiElement, problemsHolder: ProblemsHolder) {
+    findImplicitDependenciesFor(packageName, element.project)?.let { dependencies ->
+      val description = createImplicitDependenciesDescription(packageName, dependencies)
+      problemsHolder.registerProblem(element, description, ProblemHighlightType.WARNING, InstallLibrariesFix(dependencies))
+    }
+  }
+
+  private fun findImplicitDependenciesFor(packageName: String, project: Project): List<RequiredPackage>? {
+    return packageName2Dependencies[packageName]?.let { dependencies ->
+      val installer = RequiredPackageInstaller.getInstance(project)
+      val missing = installer.getMissingPackages(dependencies)
+      missing.takeIf { it.isNotEmpty() }
     }
   }
 
@@ -116,5 +138,18 @@ class MissingPackageInspection : RInspection() {
   // http://stackoverflow.com/questions/41298164/how-to-remove-single-and-double-quotes-at-both-ends-of-a-string
   private fun unquote(text: String): String {
     return text.replace("^['\"]*".toRegex(), "").replace("['\"]*$".toRegex(), "")
+  }
+
+  companion object {
+    private val animateDependencies = listOf("gifski", "png").map { RequiredPackage(it) }
+
+    private val packageName2Dependencies = mapOf(
+      "gganimate" to animateDependencies
+    )
+
+    private fun createImplicitDependenciesDescription(packageName: String, dependencies: List<RequiredPackage>): String {
+      val dependencyString = dependencies.joinToString(", ") { it.toFormat(true) }
+      return RBundle.message("inspection.missingImplicitDependencies.description", packageName, dependencyString)
+    }
   }
 }
