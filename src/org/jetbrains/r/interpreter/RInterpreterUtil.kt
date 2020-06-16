f97c42273328108a1f657be068051c0be32776fe
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2020-06-16 10:57:23 +0300

    IDEA-CR-63695: [R] bump minimum supported version of R to 3.4; use the same rwrapper for all R versions
    GitOrigin-RevId: 41648264ae65fbef4ede1b4770f919ee47171b4f

--- src/org/jetbrains/r/interpreter/RInterpreterUtil.kt
+++ src/org/jetbrains/r/interpreter/RInterpreterUtil.kt
@@ -110,7 +110,7 @@ object RInterpreterUtil {
   }
 
   fun isSupportedVersion(version: Version?): Boolean {
-    return version != null && version.isOrGreaterThan(3, 3) && version.lessThan(4, 2)
+    return version != null && version.isOrGreaterThan(3, 4) && version.lessThan(4, 2)
   }
 
   fun suggestAllInterpreters(enabledOnly: Boolean): List<RInterpreterInfo> {
