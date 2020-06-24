d829696c16e5bf3797c828bfcb66c2b23a3079de
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-06-24 15:36:12 +0200

    prefer JDK collections
    GitOrigin-RevId: 699d98435b9b1378d584868e780458e06f9cf56d

--- src/org/jetbrains/r/psi/stubs/RStubElementType.java
+++ src/org/jetbrains/r/psi/stubs/RStubElementType.java
@@ -2,7 +2,6 @@
 /*
  * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
-
 package org.jetbrains.r.psi.stubs;
 
 import com.intellij.lang.ASTNode;
@@ -15,23 +14,21 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.r.RLanguage;
 import org.jetbrains.r.psi.api.RPsiElement;
 
-public abstract class RStubElementType<StubT extends StubElement, PsiT extends RPsiElement> extends IStubElementType<StubT, PsiT> {
-    public RStubElementType(@NonNls final String debugName) {
-        super(debugName, RLanguage.INSTANCE);
-    }
+public abstract class RStubElementType<StubT extends StubElement<?>, PsiT extends RPsiElement> extends IStubElementType<StubT, PsiT> {
+  public RStubElementType(@NonNls final String debugName) {
+    super(debugName, RLanguage.INSTANCE);
+  }
+
+  public abstract PsiElement createElement(@NotNull final ASTNode node);
 
 
-    public abstract PsiElement createElement(@NotNull final ASTNode node);
+  @Override
+  public void indexStub(@NotNull final StubT stub, @NotNull final IndexSink sink) {
+  }
 
-
-    @Override
-    public void indexStub(@NotNull final StubT stub, @NotNull final IndexSink sink) {
-    }
-
-
-    @Override
-    @NotNull
-    public String getExternalId() {
-        return "r." + super.toString();
-    }
+  @Override
+  @NotNull
+  public String getExternalId() {
+    return "r." + super.toString();
+  }
 }
