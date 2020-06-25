10e8d3a95e498f2fe29c53b6dc05bebf5a97126d
Mikhail Putilin <mikhail.putilin@jetbrains.com>
2020-06-25 15:33:30 +0300

    IDEA-CR-2218 [R] Rework job dialog for remote interpreters
    (cherry picked from commit 15c327445db42403c5e098146a720885f8126318)
    
    GitOrigin-RevId: 39b48ed59eccd6d3caf741cb93aa7292344160f8

--- src/org/jetbrains/r/console/jobs/RRunJobDialog.kt
+++ src/org/jetbrains/r/console/jobs/RRunJobDialog.kt
@@ -9,30 +9,51 @@ import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.DocumentAdapter
 import com.intellij.ui.layout.*
 import org.jetbrains.concurrency.runAsync
 import org.jetbrains.r.RBundle
 import org.jetbrains.r.interpreter.RInterpreter
+import org.jetbrains.r.interpreter.isLocal
 import javax.swing.DefaultComboBoxModel
 import javax.swing.JComponent
 import javax.swing.event.DocumentEvent
+import kotlin.reflect.KMutableProperty0
 
 class RRunJobDialog(private val interpreter: RInterpreter, private val defaultScript: VirtualFile?,
                     private val defaultWorkingDirectory: String) :
   DialogWrapper(interpreter.project, null, true, IdeModalityType.IDE) {
   private val project = interpreter.project
   private val panel: DialogPanel
-  private lateinit var scriptField: TextFieldWithBrowseButton
-  private lateinit var workingDirField: TextFieldWithBrowseButton
 
-  val scriptPath get() = scriptField.text
-  val workingDirectory get() = workingDirField.text
-  var runWithGlobalEnvironment: Boolean = false
-  var exportGlobalEnvPolicy: ExportGlobalEnvPolicy = ExportGlobalEnvPolicy.DO_NO_EXPORT
+  enum class PathType {
+    LOCAL {
+      override fun toString() = "Local R Script"
+    },
+    REMOTE {
+      override fun toString() = "Remote R Script"
+    }
+  }
+  private var scriptPathType = PathType.LOCAL
+  private var scriptPathLocal: String = ""
+  private var scriptPathRemote: String = ""
+
+  private var workingDirectory: String = defaultWorkingDirectory
+  private var runWithGlobalEnvironment: Boolean = false
+  private var exportGlobalEnvPolicy: ExportGlobalEnvPolicy = ExportGlobalEnvPolicy.DO_NO_EXPORT
 
   init {
+    if (defaultScript?.isInLocalFileSystem == true) {
+      scriptPathLocal = defaultScript.path
+    } else if (!interpreter.isLocal() && defaultScript != null) {
+      interpreter.getFilePathAtHost(defaultScript)?.let {
+        scriptPathType = PathType.REMOTE
+        scriptPathRemote = it
+      }
+    }
+
     panel = createDialogPanel()
     setTitle(RBundle.message("jobs.dialog.title"))
     setOKButtonText(RBundle.message("jobs.dialog.ok.button.text"))
@@ -46,16 +67,30 @@ class RRunJobDialog(private val interpreter: RInterpreter, private val defaultSc
       row { label(RBundle.message("jobs.dialog.label.script.path")) }
       row {
         cell {
-          label(RBundle.message("jobs.dialog.label.r.script")).withLargeLeftGap()
-          component(interpreter.createFileChooserForHost(defaultScript?.let { interpreter.getFilePathAtHost(it) }.orEmpty() )
-                      .also { scriptField = it }).addTextChangedListener()
+          if (interpreter.isLocal()) {
+            label(RBundle.message("jobs.dialog.label.r.script")).withLargeLeftGap()
+            textFieldWithBrowseButton().addTextChangedListener(::scriptPathLocal)
+          } else {
+            val comboBox = comboBox(DefaultComboBoxModel(arrayOf(PathType.LOCAL, PathType.REMOTE)), ::scriptPathType).component
+            val localField = textFieldWithBrowseButton().addTextChangedListener(::scriptPathLocal).component
+            val remoteField = component(interpreter.createFileChooserForHost()).addTextChangedListener(::scriptPathRemote)
+              .withLeftGap(0).component
+            val listener = {
+              localField.isVisible = comboBox.item == PathType.LOCAL
+              remoteField.isVisible = comboBox.item == PathType.REMOTE
+            }
+            comboBox.addActionListener {
+              listener()
+              updateOkAction()
+            }
+            listener()
+          }
         }
       }
       row {
         cell {
           label(RBundle.message("jobs.dialog.label.working.directory")).withLargeLeftGap()
-          component(interpreter.createFileChooserForHost(defaultWorkingDirectory, true)
-                      .also { workingDirField = it }).addTextChangedListener()
+          component(interpreter.createFileChooserForHost(defaultWorkingDirectory, true)).addTextChangedListener(::workingDirectory)
         }
       }
       row { label(RBundle.message("jobs.dialog.label.environments")) }
@@ -73,19 +108,27 @@ class RRunJobDialog(private val interpreter: RInterpreter, private val defaultSc
       }
     }
 
-  private fun CellBuilder<TextFieldWithBrowseButton>.addTextChangedListener(): CellBuilder<TextFieldWithBrowseButton> =
-    this.apply {
-      component.textField.document.addDocumentListener(object : DocumentAdapter() {
-        override fun textChanged(e: DocumentEvent) {
-          updateOkAction()
-        }
-      })
+  private fun CellBuilder<TextFieldWithBrowseButton>.addTextChangedListener(prop: KMutableProperty0<String>? = null):
+    CellBuilder<TextFieldWithBrowseButton> {
+    if (prop != null) {
+      component.text = prop.get()
     }
+    component.textField.document.addDocumentListener(object : DocumentAdapter() {
+      override fun textChanged(e: DocumentEvent) {
+        prop?.set(component.text)
+        updateOkAction()
+      }
+    })
+    return this
+  }
 
   override fun doOKAction() {
     panel.apply()
     runAsync {
-      val script = interpreter.findFileByPathAtHost(scriptPath)
+      val (script, scriptPath) = when (scriptPathType) {
+        PathType.LOCAL -> LocalFileSystem.getInstance().findFileByPath(scriptPathLocal) to scriptPathLocal
+        PathType.REMOTE -> interpreter.findFileByPathAtHost(scriptPathRemote) to scriptPathRemote
+      }
       if (script == null) {
         invokeLater {
           Messages.showErrorDialog(project, RBundle.message("jobs.dialog.file.not.found.message", scriptPath),
@@ -101,6 +144,9 @@ class RRunJobDialog(private val interpreter: RInterpreter, private val defaultSc
 
   private fun updateOkAction() {
     panel.apply()
-    isOKActionEnabled = scriptPath.isNotEmpty() && workingDirectory.isNotEmpty()
+    isOKActionEnabled = workingDirectory.isNotBlank() && when (scriptPathType) {
+      PathType.LOCAL -> scriptPathLocal.isNotBlank()
+      PathType.REMOTE -> scriptPathRemote.isNotBlank()
+    }
   }
 }
