08f09ea8643b72008a7086ab0a3652c61e73c8c7
Mikhail Putilin <mikhail.putilin@jetbrains.com>
2020-06-25 17:10:21 +0300

    [R] Small changes in remote virtual files
    GitOrigin-RevId: 374dd70e86b754b463248a7a331f7265f151efea

--- remote/src/org/jetbrains/r/remote/filesystem/RRemoteFilesystemUtil.kt
+++ remote/src/org/jetbrains/r/remote/filesystem/RRemoteFilesystemUtil.kt
@@ -6,24 +6,30 @@ package org.jetbrains.r.remote.filesystem
 
 import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.fileEditor.OpenFileDescriptor
+import com.intellij.openapi.fileTypes.INativeFileType
+import com.intellij.openapi.fileTypes.ex.FileTypeChooser
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.util.PathUtilRt
+import com.jetbrains.plugins.webDeployment.WDBundle
+import com.jetbrains.plugins.webDeployment.remoteEdit.EditRemoteFileAction
 import org.jetbrains.r.remote.RRemoteBundle
 import org.jetbrains.r.remote.host.RRemoteHost
 
 object RRemoteFilesystemUtil {
   fun editRemoteFile(project: Project, remoteHost: RRemoteHost, path: String) {
-    ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Opening '${PathUtilRt.getFileName(path)}'") {
+    val name = PathUtilRt.getFileName(path)
+    ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Opening '$name'") {
       override fun run(indicator: ProgressIndicator) {
         val virtualFile = RRemoteVFS.instance.findFileByPath(remoteHost, path) ?: return
         invokeAndWaitIfNeeded {
+          FileTypeChooser.getKnownFileTypeOrAssociate(name)
           val opened = OpenFileDescriptor(project, virtualFile).navigateInEditor(project, true)
           if (!opened) {
-            Messages.showInfoMessage(project, RRemoteBundle.message("remote.host.view.cannot.open.file", virtualFile.name), this.title)
+            Messages.showInfoMessage(project, RRemoteBundle.message("remote.host.view.cannot.open.file", name), this.title)
           }
         }
       }
