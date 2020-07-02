f3e1623f0a9e087cabfb344f48c8f815a2ab8fb1
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2020-07-02 19:01:46 +0700

    [R] fix RIndexableFilesFilter condition: include subdirectories
    (cherry picked from commit 314bc80d49d0d7ac2bc3508999c0c535f0154ad0)
    (cherry picked from commit 5ad7377cc8fc1c399a394bf61b77ef4d2301ee19)
    (cherry picked from commit 3d3f914e17cdf32903698e4975f346a3b998d492)
    (cherry picked from commit 45d98773c21f55552371d2e0dd3ac134b370da22)
    (cherry picked from commit a4d71b0044c36a7795d2b5f10cadfefcc558d890)
    
    GitOrigin-RevId: b5e36e184fd65ba17591a80a4a73f2a4d9ded8bf

--- src/org/jetbrains/r/settings/RIndexableFilesFilter.kt
+++ src/org/jetbrains/r/settings/RIndexableFilesFilter.kt
@@ -3,8 +3,9 @@ package org.jetbrains.r.settings
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.indexing.IndexableFilesFilter
-import org.jetbrains.r.skeleton.RSkeletonFileType
+import org.jetbrains.r.packages.RSkeletonUtil
 
 class RIndexableFilesFilter : IndexableFilesFilter {
-  override fun shouldIndex(file: VirtualFile): Boolean = !Registry.`is`("pycharm.lazy.indexing", false) || file.fileType == RSkeletonFileType
+  override fun shouldIndex(file: VirtualFile): Boolean = !Registry.`is`("pycharm.lazy.indexing", false) ||
+  file.path.contains(RSkeletonUtil.SKELETON_DIR_NAME)
 }
