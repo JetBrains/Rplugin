1128fc207d0e9fcb96ce19818ab34766aa0a02f5
Mikhail Putilin <mikhail.putilin@jetbrains.com>
2020-07-27 14:55:47 +0300

    [R-847] Fix showing debugger toolbar
    GitOrigin-RevId: b066b2bea7ccbfc0f2fca4134efbd80b74ceeaf8

--- src/org/jetbrains/r/console/RConsoleExecuteActionHandler.kt
+++ src/org/jetbrains/r/console/RConsoleExecuteActionHandler.kt
@@ -402,10 +402,8 @@ class RConsoleExecuteActionHandler(private val consoleView: RConsoleView)
 
   @Synchronized
   fun fireBusy() {
-    if (state != State.BUSY) {
-      state = State.BUSY
-      listeners.forEach { it.onBusy() }
-    }
+    state = State.BUSY
+    listeners.forEach { it.onBusy() }
   }
 
   interface Listener {
