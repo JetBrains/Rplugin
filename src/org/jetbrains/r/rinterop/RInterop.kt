fff69f71630afdff608dedd164c8473a8d08b64f
Mikhail Putilin <mikhail.putilin@jetbrains.com>
2020-07-31 18:47:31 +0300

    [R] Fix race condition in debugger tests
    GitOrigin-RevId: 61021b6f7f45d62ee8a6490de30c53d0980765e8

--- src/org/jetbrains/r/rinterop/RInterop.kt
+++ src/org/jetbrains/r/rinterop/RInterop.kt
@@ -436,7 +436,7 @@ class RInterop(val interpreter: RInterpreter, val processHandler: ProcessHandler
   fun debugAddBreakpoint(file: VirtualFile, line: Int,
                          suspend: Boolean = true,
                          evaluateAndLog: String? = null,
-                         condition: String? = null) = executeTask {
+                         condition: String? = null) {
     val position = SourcePosition.newBuilder()
       .setFileId(sourceFileManager.getFileId(file)).setLine(line).build()
     execute(asyncStub::debugAddBreakpoint, DebugAddBreakpointRequest.newBuilder()
@@ -448,7 +448,7 @@ class RInterop(val interpreter: RInterpreter, val processHandler: ProcessHandler
     )
   }
 
-  fun debugRemoveBreakpoint(file: VirtualFile, line: Int) = executeTask {
+  fun debugRemoveBreakpoint(file: VirtualFile, line: Int) {
     execute(asyncStub::debugRemoveBreakpoint, SourcePosition.newBuilder()
       .setFileId(sourceFileManager.getFileId(file)).setLine(line).build())
   }
