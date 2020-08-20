d43d9438f5513d9c69053332c96baab10cd570c3
Maria Filipanova <Maria.Filipanova@jetbrains.com>
2020-08-20 14:09:05 +0300

    R-900: return an aliased path from getEditorContext and add tests
    (cherry picked from commit 6172e2c15461b2b4ff24b85a20190a965342f496)
    
    GitOrigin-RevId: 6515c295977064795f21e233e8320c8b794433bc

--- test/org/jetbrains/r/rinterop/RStudioApiUtilsTest.kt
+++ test/org/jetbrains/r/rinterop/RStudioApiUtilsTest.kt
@@ -230,7 +230,53 @@ class RStudioApiUtilsTest : RConsoleBaseTestCase() {
       |cntx <- rstudioapi::getSourceEditorContext()
       |rstudioapi::setSelectionRanges(list(c(1,1,1,2), c(1,3,1,5)), cntx${"$"}id)
     """.trimMargin()).blockingGetAndDispatchEvents(DEFAULT_TIMEOUT)
-    TestCase.assertEquals(myFixture.editor.caretModel.allCarets.filter { it.hasSelection() }.map { it.selectionStart to it.selectionEnd },
-                          listOf(0 to 1, 2 to 4))
+    TestCase.assertEquals(listOf(0 to 1, 2 to 4),
+                          myFixture.editor.caretModel.allCarets.filter { it.hasSelection() }.map { it.selectionStart to it.selectionEnd })
+  }
+
+  fun testBasic_setSelectionRangesWithRepeatedCalls() {
+    myFixture.configureByText("foo.R", """
+      a <- 3
+      # b
+    """.trimIndent())
+    console.executeText("""
+      |cntx <- rstudioapi::getSourceEditorContext()
+      |rstudioapi::setSelectionRanges(c(1,1,1,3), cntx${"$"}id)
+      |rstudioapi::setSelectionRanges(list(c(1,1,1,2), c(1,3,1,5)), cntx${"$"}id)
+    """.trimMargin()).blockingGetAndDispatchEvents(DEFAULT_TIMEOUT)
+    TestCase.assertEquals(listOf(0 to 1, 2 to 4),
+                          myFixture.editor.caretModel.allCarets.filter { it.hasSelection() }.map { it.selectionStart to it.selectionEnd })
+  }
+
+  fun testBasic_jobRunScript() {
+    rInterop.setWorkingDir(myFixture.testDataPath)
+    console.executeText("""
+      |c <- 5
+      |rstudioapi::jobRunScript("rstudioapi/testJobs.R", importEnv = TRUE, exportEnv = "R_GlobalEnv")
+    """.trimMargin()).blockingGetAndDispatchEvents(DEFAULT_TIMEOUT)
+    TestCase.assertEquals("[1] 3", (console.consoleRuntimeInfo.variables["a"] as RValueSimple).text)
+    TestCase.assertEquals("[1] 8", (console.consoleRuntimeInfo.variables["b"] as RValueSimple).text)
+  }
+
+  fun testBasic_jobRunScriptWithoutExport() {
+    rInterop.setWorkingDir(myFixture.testDataPath)
+    console.executeText("""
+      |c <- 5
+      |rstudioapi::jobRunScript("rstudioapi/testJobs.R")
+    """.trimMargin()).blockingGetAndDispatchEvents(DEFAULT_TIMEOUT)
+    TestCase.assertEquals(null, console.consoleRuntimeInfo.variables["a"])
+  }
+
+  fun testBasic_jobRunScriptWithoutImport() {
+    rInterop.setWorkingDir(myFixture.testDataPath)
+    console.executeText("""
+      |c <- 5
+      |id <- rstudioapi::jobRunScript("rstudioapi/testJobs2.R", exportEnv = "R_GlobalEnv")
+      |rstudioapi::jobRunScript("rstudioapi/testJobs.R", exportEnv = "R_GlobalEnv")
+    """.trimMargin()).blockingGetAndDispatchEvents(DEFAULT_TIMEOUT)
+    TestCase.assertEquals(null, console.consoleRuntimeInfo.variables["a"])
+    TestCase.assertEquals(null, console.consoleRuntimeInfo.variables["b"])
+    TestCase.assertEquals("[1] 3", (console.consoleRuntimeInfo.variables["e"] as RValueSimple).text)
+    TestCase.assertEquals("[1] 4", (console.consoleRuntimeInfo.variables["d"] as RValueSimple).text)
   }
 }
