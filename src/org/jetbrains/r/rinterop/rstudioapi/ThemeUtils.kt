2063bc5c7aa93afdd556d7cb0046b8e6121de1f0
Maria Filipanova <Maria.Filipanova@jetbrains.com>
2020-08-30 09:36:37 +0300

    R-900: return more params from getThemeInfo
    (cherry picked from commit 80b436d2df897400ff9c6baaebf694d1418e0434)
    
    GitOrigin-RevId: 70bcb2e1cd9424c26619eb2e0d33cc1bc5e66604

--- src/org/jetbrains/r/rinterop/rstudioapi/ThemeUtils.kt
+++ src/org/jetbrains/r/rinterop/rstudioapi/ThemeUtils.kt
@@ -1,7 +1,9 @@
 package org.jetbrains.r.rinterop.rstudioapi
 
+import com.intellij.ui.JBColor
 import com.intellij.util.ui.UIUtil
 import org.jetbrains.r.rinterop.RObject
+import java.awt.Color
 import javax.swing.UIManager
 
 fun getThemeInfo(): RObject {
@@ -9,9 +11,17 @@ fun getThemeInfo(): RObject {
   return RObject.newBuilder()
     .setNamedList(RObject.NamedList.newBuilder()
                     .addRObjects(0, RObject.KeyValue.newBuilder().setKey("editor").setValue(UIManager.getLookAndFeel().name.toRString()))
-                    .addRObjects(1, RObject.KeyValue.newBuilder().setKey("global").setValue(getRNull()))
+                    .addRObjects(1, RObject.KeyValue.newBuilder().setKey("global").setValue("Modern".toRString()))
                     .addRObjects(2, RObject.KeyValue.newBuilder().setKey("dark").setValue(UIUtil.isUnderDarcula().toRBoolean()))
-                    .addRObjects(3, RObject.KeyValue.newBuilder().setKey("foreground").setValue(getRNull()))
-                    .addRObjects(4, RObject.KeyValue.newBuilder().setKey("background").setValue(getRNull()))
+                    .addRObjects(3, RObject.KeyValue.newBuilder().setKey("foreground").setValue(
+                     colorHelper(JBColor.foreground()).toRString())
+                    )
+                    .addRObjects(4, RObject.KeyValue.newBuilder().setKey("background").setValue(
+                      colorHelper(JBColor.background()).toRString())
+                    )
     ).build()
+}
+
+private fun colorHelper(color: Color): String {
+  return "rgb(${color.red}, ${color.green}, ${color.blue})"
 }
