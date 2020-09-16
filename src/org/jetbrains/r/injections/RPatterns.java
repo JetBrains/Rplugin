5d32cb66813cbaa1a6b339a4bdb5d2333d3da601
Andrey Vokin <andrey.vokin@jetbrains.com>
2020-09-16 12:24:35 +0200

    [R] Shiny: CSS selectors highlighting and ID completion for indertUI/removeUI
    R-997 [Shiny] CSS selectors highlighting and ID completion for indertUI/removeUI
    
    GitOrigin-RevId: a91cb4b493c587c0febb00c8eaaa1d3e0416a19c

--- src/org/jetbrains/r/injections/RPatterns.java
+++ src/org/jetbrains/r/injections/RPatterns.java
@@ -4,9 +4,9 @@ import com.intellij.patterns.PatternCondition;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.patterns.PsiElementPattern;
 import com.intellij.psi.PsiElement;
+import com.intellij.util.ArrayUtil;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.r.hints.parameterInfo.RArgumentInfo;
 import org.jetbrains.r.hints.parameterInfo.RParameterInfoUtil;
 import org.jetbrains.r.psi.api.*;
 
@@ -14,9 +14,9 @@ public class RPatterns {
   /**
    * Used in rplugin/resources/injections/rInjections.xml
    */
-  public static @NotNull PsiElementPattern.Capture<RStringLiteralExpression> callArgumentPattern(@NotNull String methodName,
+  public static @NotNull PsiElementPattern.Capture<RStringLiteralExpression> callArgumentPattern(@NotNull String methodExpression,
                                                                                                  @NotNull String argumentName) {
-    String debugPatternName = String.format("%s(%s)", methodName, argumentName);
+    String debugPatternName = String.format("%s(%s)", methodExpression, argumentName);
     return PlatformPatterns.psiElement(RStringLiteralExpression.class).with(new PatternCondition<>(debugPatternName) {
       @Override
       public boolean accepts(@NotNull final RStringLiteralExpression stringLiteral, final ProcessingContext context) {
@@ -29,13 +29,12 @@ public class RPatterns {
 
           RCallExpression call = (RCallExpression)argumentList.getParent();
           RExpression receiver = call.getExpression();
-          if (!(receiver instanceof RIdentifierExpression) || !methodName.equals(receiver.getName())) {
+          String[] methodNames = methodExpression.split("\\|");
+          if (!(receiver instanceof RIdentifierExpression) || !ArrayUtil.contains(receiver.getName(), methodNames)) {
             return false;
           }
-          RArgumentInfo argumentInfo = RParameterInfoUtil.INSTANCE.getArgumentInfo(call);
-          if (argumentInfo != null) {
-            return argumentInfo.getArgumentPassedToParameter(argumentName) == stringLiteral;
-          }
+          RExpression argumentValue = RParameterInfoUtil.INSTANCE.getArgumentByName(call, argumentName);
+          return argumentValue == stringLiteral;
         }
         return false;
       }
--- test/org/jetbrains/r/injections/RInjectionsTest.kt
+++ test/org/jetbrains/r/injections/RInjectionsTest.kt
@@ -1,10 +1,10 @@
 package org.jetbrains.r.injections
 
 import org.jetbrains.r.RLanguage
-import org.jetbrains.r.run.RProcessHandlerBaseTestCase
+import org.jetbrains.r.RUsefulTestCase
 import org.junit.Assert.assertNotEquals
 
-class RInjectionsTest : RProcessHandlerBaseTestCase() {
+class RInjectionsTest : RUsefulTestCase() {
   fun testInjectionInShinyMarkdownCall() {
     doTest(
       """
@@ -31,7 +31,16 @@ class RInjectionsTest : RProcessHandlerBaseTestCase() {
     doTest("htmlTemplate(text_ = \"<div st<caret>yle='color: red'>Hello</div>\")")
   }
 
+  fun testInjectionInShinyInsertUICall() {
+    doTest("insertUI(selector = \"#my_<caret>id\")")
+  }
+
+  fun testInjectionInShinyRemoveUICall() {
+    doTest("removeUI(selector = \"#my_<caret>id\")")
+  }
+
   private fun doTest(code: String) {
+    addLibraries()
     myFixture.configureByText("test.r", code)
     val element = myFixture.file.findElementAt(myFixture.caretOffset)
     assertNotEquals(element!!.language, RLanguage.INSTANCE)
