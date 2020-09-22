30f2a44026d1ab6e9bcdbb1ee4aec2f714312781
Dmitrii.Orekhov <dmitrii.orekhov@jetbrains.com>
2020-09-22 17:52:39 +0300

    ML completion settings added to Preference | Editor | General | CodeCompletion pane: enable checkbox, host and port options
    GitOrigin-RevId: 496dac6b40e71205f1756d2a9764d282d45d2196

---
+++ src/org/jetbrains/r/settings/MLCompletionConfigurable.kt
@@ -0,0 +1,41 @@
+package org.jetbrains.r.settings
+
+import com.intellij.openapi.options.BoundConfigurable
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.layout.*
+import org.jetbrains.r.RBundle
+
+class MLCompletionConfigurable : BoundConfigurable(RBundle.message("project.settings.ml.completion.name")) {
+
+  companion object {
+    private const val PORT_FIELD_WIDTH = 5
+  }
+
+  private val settings = MLCompletionSettings.getInstance()
+
+  override fun createPanel(): DialogPanel {
+    return panel {
+      titledRow(displayName) {
+        row {
+          val enableCompletionCheckbox = checkBox(RBundle.message("project.settings.ml.completion.checkbox"),
+                                                  settings.state::isEnabled)
+          row {
+            label(RBundle.message("project.settings.ml.completion.server.label"))
+          }
+          row {
+            cell {
+              label(RBundle.message("project.settings.ml.completion.host.label"))
+              textField({ settings.state.host ?: "" }, settings.state::host.setter)
+                .enableIf(enableCompletionCheckbox.selected)
+              label(RBundle.message("project.settings.ml.completion.port.label"))
+              intTextField(settings.state::port, columns=PORT_FIELD_WIDTH)
+                .enableIf(enableCompletionCheckbox.selected)
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
