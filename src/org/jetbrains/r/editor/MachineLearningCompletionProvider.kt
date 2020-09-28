b54f4550aa0a6f79a9c08e30776de1cfcc3c2e8c
Artem Popov <artem.popov@jetbrains.com>
2020-09-28 15:19:56 +0300

    Add id to the beggining of the message. Id helps to understand if we are at the middle of the token.
    GitOrigin-RevId: ad4e99c680c4361effb0e210c234f9979f35e303

--- src/org/jetbrains/r/editor/MachineLearningCompletionProvider.kt
+++ src/org/jetbrains/r/editor/MachineLearningCompletionProvider.kt
@@ -7,6 +7,7 @@ package org.jetbrains.r.editor
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.CompletionInitializationContext
 import com.intellij.codeInsight.completion.PrioritizedLookupElement
 import com.intellij.icons.AllIcons
 import com.intellij.util.ProcessingContext
@@ -15,20 +16,31 @@ import org.jetbrains.r.settings.MachineLearningCompletionSettings
 import java.net.Socket
 
 internal class MachineLearningCompletionProvider : CompletionProvider<CompletionParameters>() {
-
   private val settings = MachineLearningCompletionSettings.getInstance()
+  private val dummyIdentifier = CompletionInitializationContext.DUMMY_IDENTIFIER_TRIMMED
+
+  private fun constructInputMessage(parameters: CompletionParameters) : ByteArray {
+    val previousText = parameters.originalFile.text.subSequence(0, parameters.offset)
+    val localTextContent = parameters.position.text
+
+    // if True then we are at the start of the new token
+    val newTokenStartIdentifier = if (localTextContent == dummyIdentifier) "0|" else "1|"
+
+    return (newTokenStartIdentifier + previousText).toByteArray()
+  }
 
   override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
     if (!settings.state.isEnabled) {
       return
     }
-    val offset = parameters.offset
-    val pre_text = parameters.originalFile.text.subSequence(0, offset)
-    var answer = ""
+    val inputMessage = constructInputMessage(parameters)
+    var answer: String
+
     Socket(settings.state.host, settings.state.port).use {
-      it.getOutputStream().write((pre_text as String).toByteArray())
+      it.getOutputStream().write(inputMessage)
       answer = it.getInputStream().readAllBytes().toString(Charsets.UTF_8)
     }
+
     var currentCompletionText = ""
     var isEven = true
     for (text in answer.split("\n")) {
@@ -40,7 +52,7 @@ internal class MachineLearningCompletionProvider : CompletionProvider<Completion
         val score = text.toDouble()
         result.addElement(
           PrioritizedLookupElement.withPriority(
-            RLookupElement(currentCompletionText, true, AllIcons.Nodes.Favorite, tailText = " " + text),
+            RLookupElement(currentCompletionText, true, AllIcons.Nodes.Favorite, tailText = " $text"),
             score
           )
         )
