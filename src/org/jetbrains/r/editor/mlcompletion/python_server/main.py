7c57691ca453de79bb0462711f410afdb25e901e
Dmitrii Orekhov <dmitrii.orekhov@jetbrains.com>
2020-10-05 20:30:34 +0300

    http python server
    Host and port as command line arguments for server app
    
    GitOrigin-RevId: 688c652e666006a1ac8bdb351160734e95402cae

--- src/org/jetbrains/r/editor/mlcompletion/python_server/main.py
+++ src/org/jetbrains/r/editor/mlcompletion/python_server/main.py
@@ -1,13 +1,26 @@
-import socketserver
 import numpy as np
-from utils import bind_to_free_port
+import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+ENCODING = 'utf-8'
 
 
-class RequestHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        self.data = self.request.recv(4096).strip()
+class RequestHandler(BaseHTTPRequestHandler):
+
+    # keep-alive
+    protocol_version = 'HTTP/1.1'
+
+    def do_POST(self):
+        print(self.headers) # Debug purposes
+        try:
+            content_length = int(self.headers.get('Content-Length'))
+        except TypeError:
+            # Content-Length header is missing
+            self.send_response(400)
+            return
+        input_message = self.rfile.read(content_length)
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        print(input_message.decode('utf-8'))
         completion_variants = ["1",
                                "something",
                                "good completion",
@@ -21,16 +34,23 @@ class RequestHandler(socketserver.BaseRequestHandler):
         answer = []
         for completion, score in zip(completion_variants, map(lambda x: "%0.2f" % x, scores)):
             answer.append(completion.encode('utf-8') + b"\n" + score.encode('utf-8'))
-        answer = b"\n".join(answer)
         print(answer)
-        self.request.send(answer)
+        answer_as_bytes = b"\n".join(answer)
+        self.send_response(200)
+        self.__make_header(content_length=len(answer_as_bytes),
+                           content_type='text/plain; charset=UTF-8')
+        self.wfile.write(answer_as_bytes)
+
+    def __make_header(self, content_length, content_type='application/json'):
+        self.send_header("Content-Type", content_type)
+        self.send_header("Content-Length", content_length)
+        self.end_headers()
 
 
-host, port = "localhost", 7337
-with bind_to_free_port(
-    server_class=socketserver.TCPServer,
-    host=host,
-    start_port=port,
-    handler=RequestHandler
-) as server:
-    server.serve_forever()
+host, port = sys.argv[1], int(sys.argv[2])
+with HTTPServer((host, port), RequestHandler) as server:
+    try:
+        print("Serving")
+        server.serve_forever()
+    finally:
+        server.shutdown()
