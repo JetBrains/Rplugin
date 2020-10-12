28df47526011c55239d4643f55f7d7a326bc19b0
Mikhail Putilin <mikhail.putilin@jetbrains.com>
2020-10-12 16:15:41 +0300

    [R-809] Move datavis icons from rplugin to visualisation
    GitOrigin-RevId: 608a7074d493d448fe89e25dc8ea0e6d26a25aca

--- visualisation/src/org/intellij/datavis/r/VisualizationIcons.kt
+++ visualisation/src/org/intellij/datavis/r/VisualizationIcons.kt
@@ -9,17 +9,19 @@ import com.intellij.openapi.util.IconLoader
 object VisualizationIcons {
 
   // Chart.
-  val BAR_CHART = IconLoader.getIcon("/icons/chart/chartBar.svg")
-  val LINE_CHART = IconLoader.getIcon("/icons/chart/chartLine.svg")
-  val SCATTER_CHART = IconLoader.getIcon("/icons/chart/chartScatter.svg")
-  val PIE_CHART = IconLoader.getIcon("/icons/chart/chartPie.svg")
-  val AREA_CHART = IconLoader.getIcon("/icons/chart/chartAreaRange.svg")
-  val STOCK_CHART = IconLoader.getIcon("/icons/chart/chartStock.svg")
-  val BUBBLE_CHART = IconLoader.getIcon("/icons/chart/chartBubble.svg")
+  val BAR_CHART = getIcon("/icons/chart/chartBar.svg")
+  val LINE_CHART = getIcon("/icons/chart/chartLine.svg")
+  val SCATTER_CHART = getIcon("/icons/chart/chartScatter.svg")
+  val PIE_CHART = getIcon("/icons/chart/chartPie.svg")
+  val AREA_CHART = getIcon("/icons/chart/chartAreaRange.svg")
+  val STOCK_CHART = getIcon("/icons/chart/chartStock.svg")
+  val BUBBLE_CHART = getIcon("/icons/chart/chartBubble.svg")
 
   // Table.
-  val TABLE_PAGINATION = IconLoader.getIcon("/icons/table/pagination.svg")
+  val TABLE_PAGINATION = getIcon("/icons/table/pagination.svg")
 
   // Graphics.
-  val CONSTRAIN_IMAGE_PROPORTIONS = IconLoader.getIcon("/icons/graphics/constraintProportions.svg")
+  val CONSTRAIN_IMAGE_PROPORTIONS = getIcon("/icons/graphics/constraintProportions.svg")
+
+  private fun getIcon(path: String) = IconLoader.getIcon(path, VisualizationIcons::class.java)
 }
