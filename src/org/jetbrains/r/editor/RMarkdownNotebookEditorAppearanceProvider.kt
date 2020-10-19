6b74f641fd49a8a624ae86366d75d0a510c08221
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2020-10-20 02:49:42 +0300

    refactor NotebookEditorAppearance
    GitOrigin-RevId: ef8167633466507d0891d7f1fae134878b0e7564

--- src/org/jetbrains/r/editor/RMarkdownNotebookEditorAppearanceProvider.kt
+++ src/org/jetbrains/r/editor/RMarkdownNotebookEditorAppearanceProvider.kt
@@ -18,21 +18,11 @@ class RMarkdownNotebookEditorAppearanceProvider: NotebookEditorAppearanceProvide
   }
 }
 
-//TODO add keys
 object RMarkdownNotebookEditorAppearance: NotebookEditorAppearance, NotebookEditorAppearanceSizes by DefaultNotebookEditorAppearanceSizes {
   // TODO Sort everything lexicographically.
 
   private val RMARKDOWN_CHUNK = TextAttributesKey.createTextAttributesKey("RMARKDOWN_CHUNK")
   override fun getCodeCellBackground(scheme: EditorColorsScheme): Color? = scheme.getAttributes(RMARKDOWN_CHUNK).backgroundColor
-
-  override val GUTTER_INPUT_EXECUTION_COUNT = TextAttributesKey.createTextAttributesKey("")
-  override val GUTTER_OUTPUT_EXECUTION_COUNT = TextAttributesKey.createTextAttributesKey("")
-  override val PROGRESS_STATUS_RUNNING_COLOR = ColorKey.createColorKey("")
-  override val CELL_UNDER_CARET_COMMAND_MODE_STRIPE_COLOR = ColorKey.createColorKey("")
-  override val CELL_UNDER_CARET_EDITOR_MODE_STRIPE_COLOR = ColorKey.createColorKey("")
-  override val SAUSAGE_BUTTON_APPEARANCE = TextAttributesKey.createTextAttributesKey("")
-  override val SAUSAGE_BUTTON_SHORTCUT_COLOR = ColorKey.createColorKey("")
-  override val SAUSAGE_BUTTON_BORDER_COLOR = ColorKey.createColorKey("")
 
   /**
    * Takes lines of the cell and returns a color for the stripe that will be drawn behind the folding markers.
