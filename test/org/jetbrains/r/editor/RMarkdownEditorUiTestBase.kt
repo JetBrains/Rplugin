b2625a495962c158983eedb8242e2c6ec98d6615
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2020-10-23 12:37:41 +0300

    add RMarkdownCellLinesTest
    (cherry picked from commit b6f39ba8a0f02623dc7b78a3bb76d1e256130aa6)
    
    IJ-CR-3640
    
    GitOrigin-RevId: f4e9cc6ea1bf5e984c1c83edf46055021eb659c5

---
+++ test/org/jetbrains/r/editor/RMarkdownEditorUiTestBase.kt
@@ -0,0 +1,58 @@
+package org.jetbrains.r.editor
+
+import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiManager
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
+import com.intellij.testFramework.fixtures.impl.TempDirTestFixtureImpl
+import org.assertj.core.internal.bytebuddy.utility.RandomString
+import org.jetbrains.plugins.notebooks.editor.extractTextAndCaretOffset
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+abstract class RMarkdownEditorUiTestBase {
+  lateinit var fixture: CodeInsightTestFixture
+
+  @Before
+  fun fixtureSetUp() {
+    fixture = with(IdeaTestFixtureFactory.getFixtureFactory()) {
+      createCodeInsightFixture(
+        createFixtureBuilder(RMarkdownEditorUiTestBase::class.java.name, true).fixture,
+        TempDirTestFixtureImpl())
+    }
+    fixture.testDataPath = "${PathManager.getHomePath()}/rplugin/testData/editor"
+    fixture.setUp()
+  }
+
+  @After
+  fun fixtureTearDown() {
+    fixture.tearDown()
+  }
+}
+
+internal fun CodeInsightTestFixture.openNotebookTextInEditor(text: String) {
+  val (textWithoutCaret, caretOffset) = extractTextAndCaretOffset(text)
+
+  val localFile = runWriteAction {
+    tempDirFixture.createFile("notebook_${RandomString.make()}.rmd")
+  }
+
+  val psiFile = PsiManager.getInstance(project).findFile(localFile)!!
+  val document = PsiDocumentManager.getInstance(project).getDocument(psiFile)!!
+
+  runWriteAction {
+    document.setText(textWithoutCaret)
+    PsiDocumentManager.getInstance(project).commitDocument(document)
+    FileDocumentManager.getInstance().saveDocument(document)
+  }
+
+  openFileInEditor(localFile)
+
+  caretOffset?.let { editor.caretModel.moveToOffset(it) }
+}
