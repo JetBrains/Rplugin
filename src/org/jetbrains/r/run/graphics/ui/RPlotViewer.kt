232791cd2e891d551564108b437eb8ad8694e6de
Aleksandr Rasnyuk <aleksandr.rasnyuk@jetbrains.com>
2020-10-27 18:46:39 +0300

    [R] Standalone GE: buffer rendered plot to increase performance
    (cherry picked from commit 91943f5edfd9a9952574f0a413d465c86605571c)
    
    IDEA-CR-68212
    
    GitOrigin-RevId: 6f088692cf03f59db227c436cd449648ebc7feb4

--- src/org/jetbrains/r/run/graphics/ui/RPlotViewer.kt
+++ src/org/jetbrains/r/run/graphics/ui/RPlotViewer.kt
@@ -1,7 +1,11 @@
 package org.jetbrains.r.run.graphics.ui
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.editor.colors.EditorColorsListener
+import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.project.Project
+import com.intellij.reference.SoftReference
+import com.intellij.util.ui.ImageUtil
 import org.jetbrains.r.run.graphics.RCanvasPlotterProvider
 import org.jetbrains.r.run.graphics.RGraphicsUtils
 import org.jetbrains.r.run.graphics.RPlot
@@ -10,14 +14,17 @@ import org.jetbrains.r.settings.RGraphicsSettings
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.RenderingHints
+import java.awt.image.BufferedImage
 import javax.swing.JComponent
 
 class RPlotViewer(project: Project, parent: Disposable) : JComponent() {
+  private var cachedImage = SoftReference<BufferedImage>(null)
+
   private var darkMode = RGraphicsSettings.isDarkModeEnabled(project)
     set(value) {
       if (field != value) {
         field = value
-        repaint()
+        refresh()
       }
     }
 
@@ -25,7 +32,7 @@ class RPlotViewer(project: Project, parent: Disposable) : JComponent() {
     set(value) {
       if (field !== value) {
         field = value
-        repaint()
+        refresh()
       }
     }
 
@@ -33,7 +40,7 @@ class RPlotViewer(project: Project, parent: Disposable) : JComponent() {
     set(value) {
       if (field != value) {
         field = value
-        repaint()
+        refresh()
       }
     }
 
@@ -41,20 +48,46 @@ class RPlotViewer(project: Project, parent: Disposable) : JComponent() {
     get() = RGraphicsUtils.ScreenParameters(size, resolution)
 
   init {
+    project.messageBus.connect(parent).subscribe(EditorColorsManager.TOPIC, EditorColorsListener {
+      refresh()
+    })
     RGraphicsSettings.addDarkModeListener(project, parent) { isEnabled ->
       darkMode = isEnabled
     }
   }
 
+  private fun refresh() {
+    cachedImage.clear()
+    repaint()
+  }
+
   override fun paintComponent(g: Graphics) {
     super.paintComponent(g)
     plot?.let { plot ->
-      if (g is Graphics2D) {
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-        g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE)
-        val provider = RCanvasPlotterProvider(parameters, g)
-        RPlotUtil.replay(plot, provider, darkMode)
-      }
+      val image = getOrDrawImage(plot)
+      g.drawImage(image, 0, 0, width, height, null)
+    }
+  }
+
+  private fun getOrDrawImage(plot: RPlot): BufferedImage {
+    return cachedImage.getIfSameSize() ?: drawImage(plot).also { image ->
+      cachedImage = SoftReference(image)
+    }
+  }
+
+  private fun drawImage(plot: RPlot): BufferedImage {
+    return ImageUtil.createImage(width, height, BufferedImage.TYPE_INT_ARGB).also { image ->
+      val g = image.graphics as Graphics2D
+      g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+      g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE)
+      val provider = RCanvasPlotterProvider(parameters, g)
+      RPlotUtil.replay(plot, provider, darkMode)
+    }
+  }
+
+  private fun SoftReference<BufferedImage>.getIfSameSize(): BufferedImage? {
+    return get()?.takeIf { image ->
+      ImageUtil.getUserWidth(image) == width && ImageUtil.getUserHeight(image) == height
     }
   }
 }
