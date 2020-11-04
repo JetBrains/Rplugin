ed2b4a61a4702cc36269dab38261c250d023c2fe
Aleksandr Rasnyuk <aleksandr.rasnyuk@jetbrains.com>
2020-11-04 15:38:16 +0300

    [R] Remove code duplication from RPlotViewer
    GitOrigin-RevId: c1dae4cda5ee0b9b3762f75fd7d953a5b70aa0be

--- src/org/jetbrains/r/run/graphics/ui/RPlotViewer.kt
+++ src/org/jetbrains/r/run/graphics/ui/RPlotViewer.kt
@@ -6,14 +6,11 @@ import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.project.Project
 import com.intellij.reference.SoftReference
 import com.intellij.util.ui.ImageUtil
-import org.jetbrains.r.run.graphics.RCanvasPlotterProvider
 import org.jetbrains.r.run.graphics.RGraphicsUtils
 import org.jetbrains.r.run.graphics.RPlot
 import org.jetbrains.r.run.graphics.RPlotUtil
 import org.jetbrains.r.settings.RGraphicsSettings
 import java.awt.Graphics
-import java.awt.Graphics2D
-import java.awt.RenderingHints
 import java.awt.image.BufferedImage
 import javax.swing.JComponent
 
@@ -47,6 +44,11 @@ class RPlotViewer(project: Project, parent: Disposable) : JComponent() {
   val parameters: RGraphicsUtils.ScreenParameters
     get() = RGraphicsUtils.ScreenParameters(size, resolution)
 
+  val image: BufferedImage?
+    get() = plot?.let { plot ->
+      getOrCreateImage(plot)
+    }
+
   init {
     project.messageBus.connect(parent).subscribe(EditorColorsManager.TOPIC, EditorColorsListener {
       refresh()
@@ -63,25 +65,14 @@ class RPlotViewer(project: Project, parent: Disposable) : JComponent() {
 
   override fun paintComponent(g: Graphics) {
     super.paintComponent(g)
-    plot?.let { plot ->
-      val image = getOrDrawImage(plot)
+    image?.let { image ->
       g.drawImage(image, 0, 0, width, height, null)
     }
   }
 
-  private fun getOrDrawImage(plot: RPlot): BufferedImage {
-    return cachedImage.getIfSameSize() ?: drawImage(plot).also { image ->
+  private fun getOrCreateImage(plot: RPlot): BufferedImage {
+    return cachedImage.getIfSameSize() ?: RPlotUtil.createImage(plot, parameters, darkMode).also { image ->
       cachedImage = SoftReference(image)
-    }
-  }
-
-  private fun drawImage(plot: RPlot): BufferedImage {
-    return ImageUtil.createImage(width, height, BufferedImage.TYPE_INT_ARGB).also { image ->
-      val g = image.graphics as Graphics2D
-      g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-      g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE)
-      val provider = RCanvasPlotterProvider(parameters, g)
-      RPlotUtil.replay(plot, provider, darkMode)
     }
   }
 
