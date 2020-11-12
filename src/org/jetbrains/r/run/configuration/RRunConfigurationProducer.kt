0b797e51b178923aa8adf79cf7f13cae228c7662
Andrey Vokin <andrey.vokin@jetbrains.com>
2020-11-12 09:55:50 +0100

    [R] started run configurations
    R-1138 Support run configuration for R
    R-1137 Run configurations
    
    GitOrigin-RevId: 2b18979a19c6653fd5831f88b8254c93391924cd

---
+++ src/org/jetbrains/r/run/configuration/RRunConfigurationProducer.kt
@@ -0,0 +1,43 @@
+package org.jetbrains.r.run.configuration
+
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.LazyRunConfigurationProducer
+import com.intellij.execution.configurations.runConfigurationType
+import com.intellij.openapi.project.guessModuleDir
+import com.intellij.openapi.util.Ref
+import com.intellij.psi.PsiElement
+import org.jetbrains.r.psi.api.RFile
+
+class RRunConfigurationProducer : LazyRunConfigurationProducer<RRunConfiguration>() {
+  override fun setupConfigurationFromContext(configuration: RRunConfiguration,
+                                             context: ConfigurationContext,
+                                             sourceElement: Ref<PsiElement>): Boolean {
+    val location = context.location ?: return false
+    val element = location.psiElement
+    val psiFile = element.containingFile ?: return false
+    if (psiFile !is RFile) {
+      return false
+    }
+
+    configuration.name = psiFile.name
+    configuration.filePath = psiFile.virtualFile.path
+
+    val moduleDirectory = context.module.guessModuleDir()
+    configuration.workingDirectory = moduleDirectory?.path ?: psiFile.virtualFile.parent.path
+
+    return true
+  }
+
+  override fun isConfigurationFromContext(configuration: RRunConfiguration, context: ConfigurationContext): Boolean {
+    val location = context.location ?: return false
+    val element = location.psiElement
+    val psiFile = element.containingFile ?: return false
+    if (psiFile !is RFile) {
+      return false
+    }
+
+    return configuration.filePath == psiFile.virtualFile.path
+  }
+
+  override fun getConfigurationFactory() = runConfigurationType<RRunConfigurationType>()
+}
---
+++ test/org/jetbrains/r/run/RRunConfigurationProducerTest.kt
@@ -0,0 +1,46 @@
+package org.jetbrains.r.run
+
+import com.intellij.execution.Location
+import com.intellij.execution.PsiLocation
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.module.ModuleUtilCore
+import com.intellij.psi.PsiElement
+import com.intellij.testFramework.MapDataContext
+import org.jetbrains.r.RLightCodeInsightFixtureTestCase
+import org.jetbrains.r.run.configuration.RRunConfiguration
+import org.jetbrains.r.run.configuration.RRunConfigurationProducer
+
+class RRunConfigurationProducerTest : RLightCodeInsightFixtureTestCase() {
+  fun testCreateRunConfiguration() {
+    myFixture.configureByText(
+      "my_file.R",
+      """
+        a <- c(1, 2, 3)
+      """.trimIndent()
+    )
+
+    val templateConfigurationContext = getConfigurationContext(myFixture.file)
+
+    val configurationContext = RRunConfigurationProducer().createConfigurationFromContext(templateConfigurationContext)
+    val runConfiguration = configurationContext?.configurationSettings?.configuration
+    assertNotNull(runConfiguration)
+
+    if (runConfiguration !is RRunConfiguration) {
+      fail("RRunConfiguration expected")
+    }
+    assertEquals("/src", (runConfiguration as RRunConfiguration).workingDirectory)
+    assertEquals("/src/my_file.R", runConfiguration.filePath)
+    assertEquals("", runConfiguration.arguments)
+  }
+
+  private fun getConfigurationContext(element: PsiElement): ConfigurationContext {
+    val dataContext = MapDataContext()
+    dataContext.put(CommonDataKeys.PROJECT, myFixture.project)
+    dataContext.put(LangDataKeys.MODULE, ModuleUtilCore.findModuleForPsiElement(element))
+    val location = PsiLocation.fromPsiElement(element)
+    dataContext.put(Location.DATA_KEY, location)
+    return ConfigurationContext.getFromContext(dataContext)
+  }
+}
