f829a1dc50956b57fcd856312ab38ccd5c7c005f
Dmitrii Orekhov <dmitrii.orekhov@jetbrains.com>
2020-11-27 22:58:12 +0300

    Launch server on application startup
    GitOrigin-RevId: e382527b7ba9614c543205d0b785b9fd0f2104f1

---
+++ src/org/jetbrains/r/editor/mlcompletion/MachineLearningCompletionServerInitializerListener.kt
@@ -0,0 +1,9 @@
+package org.jetbrains.r.editor.mlcompletion
+
+import com.intellij.ide.AppLifecycleListener
+
+class MachineLearningCompletionServerInitializerListener: AppLifecycleListener {
+  override fun appStarted() {
+    MachineLearningCompletionServerService.getInstance()
+  }
+}
