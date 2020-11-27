c78c751b3f5dab4b0addacbdc608b5cbeb75ab28
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2020-11-27 18:30:21 +0300

    move method to RMarkdownCellControllersUtils
    (cherry picked from commit 64c7a4a4ad2cda83ca8cf5294e82ac8e44cf2b17)
    
    IJ-CR-4363
    
    GitOrigin-RevId: 01f3d2541ec9ef8aee4ef3e54038344393799885

--- src/org/jetbrains/r/editor/ui/RMarkdownCellControllersUtils.kt
+++ src/org/jetbrains/r/editor/ui/RMarkdownCellControllersUtils.kt
@@ -1,7 +1,10 @@
 package org.jetbrains.r.editor.ui
 
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.Inlay
+import com.intellij.openapi.util.Disposer
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -24,3 +27,19 @@ internal fun getFenceLangByOffset(psiFile: PsiFile, offset: Int): PsiElement? =
   psiFile.viewProvider
     .let { it.findElementAt(offset, it.baseLanguage) }
     ?.let { it.parent.children.find { it.elementType == MarkdownTokenTypes.FENCE_LANG } }
+
+internal fun performForCommittedPsi(editor: Editor, inlay: Inlay<*>, processPsiElement: (PsiElement) -> Unit) {
+  PsiDocumentManager.getInstance(editor.project!!).performForCommittedDocument(editor.document) {
+    val psiElement = getFenceLangByOffset(editor.psiFile!!, inlay.offset)
+    if (psiElement != null) {
+      processPsiElement(psiElement)
+    }
+    else {
+      if (editor.document.textLength > 0) {
+        Logger.getInstance("#org.jetbrains.r.editor.ui.performOnCommittedPsi")
+          .error("psi structure and RMarkdownMergingLangLexer result don't match")
+      }
+      Disposer.dispose(inlay)
+    }
+  }
+}
