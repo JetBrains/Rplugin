ca9acdc1cf847487386467f5662fc52aae87bd56
Vladimir Koshelev <Vladimir.Koshelev@jetbrains.com>
2020-12-01 20:56:59 +0100

    [visualization] fix issue in VisualizationBundle
    GitOrigin-RevId: 48625ab22286b6dbe1542304629afb54d56f55e7

--- visualisation/src/org/intellij/datavis/r/VisualizationBundle.kt
+++ visualisation/src/org/intellij/datavis/r/VisualizationBundle.kt
@@ -5,10 +5,14 @@
 package org.intellij.datavis.r
 
 import com.intellij.DynamicBundle
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
+@NonNls
 private const val BUNDLE = "messages.VisualizationBundle"
 
 object VisualizationBundle : DynamicBundle(BUNDLE) {
+  @Nls
   fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, params)
 }
