32938ff369adbda783a9e9658e2b9af50d7d042e
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2020-12-07 15:26:33 +0300

    hide implementation details of NotebookCellLinesLexer
    GitOrigin-RevId: 91c49189be6624376839c86707926099b1428a71

--- visualisation/src/org/jetbrains/plugins/notebooks/editor/NotebookCellLinesLexer.kt
+++ visualisation/src/org/jetbrains/plugins/notebooks/editor/NotebookCellLinesLexer.kt
@@ -6,28 +6,32 @@ import com.intellij.psi.tree.IElementType
 interface NotebookCellLinesLexer {
   val longestTokenLength: Int
 
-  fun createNotebookCellTypeAwareLexer(): Lexer
-
-  fun getCellType(tokenType: IElementType): NotebookCellLines.CellType?
-
   fun shouldParseWholeFile(): Boolean = false
 
-  fun markerSequence(chars: CharSequence, ordinalIncrement: Int, offsetIncrement: Int): Sequence<NotebookCellLines.Marker> = sequence {
-    val lexer = createNotebookCellTypeAwareLexer()
-    lexer.start(chars, 0, chars.length)
-    var ordinal = 0
-    while (true) {
-      val tokenType = lexer.tokenType ?: break
-      val cellType = getCellType(tokenType)
-      if (cellType != null) {
-        yield(NotebookCellLines.Marker(
-          ordinal = ordinal++ + ordinalIncrement,
-          type = cellType,
-          offset = lexer.currentPosition.offset + offsetIncrement,
-          length = lexer.tokenText.length,
-        ))
+  fun markerSequence(chars: CharSequence, ordinalIncrement: Int, offsetIncrement: Int): Sequence<NotebookCellLines.Marker>
+
+  companion object {
+    fun defaultMarkerSequence(underlyingLexerFactory: () -> Lexer,
+                              tokenToCellType: (IElementType) -> NotebookCellLines.CellType?,
+                              chars: CharSequence,
+                              ordinalIncrement: Int,
+                              offsetIncrement: Int): Sequence<NotebookCellLines.Marker> = sequence{
+      val lexer = underlyingLexerFactory()
+      lexer.start(chars, 0, chars.length)
+      var ordinal = 0
+      while (true) {
+        val tokenType = lexer.tokenType ?: break
+        val cellType = tokenToCellType(tokenType)
+        if (cellType != null) {
+          yield(NotebookCellLines.Marker(
+            ordinal = ordinal++ + ordinalIncrement,
+            type = cellType,
+            offset = lexer.currentPosition.offset + offsetIncrement,
+            length = lexer.tokenText.length,
+          ))
+        }
+        lexer.advance()
       }
-      lexer.advance()
     }
   }
 }
