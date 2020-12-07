39e62eb3e3cb57b73951a81ffc7e8e0233125ee9
Vladimir Koshelev <Vladimir.Koshelev@jetbrains.com>
2020-12-07 08:56:59 +0100

    [R] fix messages in RAnnotatorTest
    GitOrigin-RevId: b906948f8cf945ee53f16f97907c52839beafaef

--- testData/annotator/sourceAndLinks.R
+++ testData/annotator/sourceAndLinks.R
@@ -1,9 +1,9 @@
 # Tooltip for file argument
-<info descr="R_FUNCTION_CALL">source</info>("<info descr="Open file in editor (ACTION_GOTO_DECLARATION)">R/A.R</info>")
-<info descr="R_FUNCTION_CALL">source</info>("<info descr="Open file in editor (ACTION_GOTO_DECLARATION)">/home/user/R/B.R</info>")
+<info descr="R_FUNCTION_CALL">source</info>("<info descr="Open the file in editor (ACTION_GOTO_DECLARATION)">R/A.R</info>")
+<info descr="R_FUNCTION_CALL">source</info>("<info descr="Open the file in editor (ACTION_GOTO_DECLARATION)">/home/user/R/B.R</info>")
 
 # Tooltip for named file argument
-<info descr="R_FUNCTION_CALL">source</info>(<info descr="R_NAMED_ARGUMENT">file</info> = "<info descr="Open file in editor (ACTION_GOTO_DECLARATION)">R/A.R</info>")
+<info descr="R_FUNCTION_CALL">source</info>(<info descr="R_NAMED_ARGUMENT">file</info> = "<info descr="Open the file in editor (ACTION_GOTO_DECLARATION)">R/A.R</info>")
 
 # No tooltip for another arguments & string literals & empty string
 <info descr="R_FUNCTION_CALL">source</info>(<info descr="R_NAMED_ARGUMENT">deparseCtrl</info> = "R/A.R")
