e667ce284b19d75ef8778ce2826656cefef6bf38
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2020-12-17 17:54:31 +0300

    [R-1154] move functions from jupyter NotebookEditorUiUtil to visualization
    GitOrigin-RevId: 17a24b47b06d2824c4ff5eb75a00afeb1b713299

--- visualisation/src/org/jetbrains/plugins/notebooks/editor/NotebookVisualizationUiUtil.kt
+++ visualisation/src/org/jetbrains/plugins/notebooks/editor/NotebookVisualizationUiUtil.kt
@@ -1,7 +1,10 @@
 package org.jetbrains.plugins.notebooks.editor
 
+import com.intellij.openapi.editor.Editor
+import java.awt.Color
 import java.awt.Graphics
 import kotlin.math.min
+import java.awt.Rectangle
 
 inline fun <T, G : Graphics> G.use(handler: (g: G) -> T): T =
   try {
@@ -25,4 +28,35 @@ inline fun <T> trimLists(left: List<T>, right: List<T>, comparator: (T, T) -> Bo
   }
 
   return left.run { subList(trimLeft, size - trimRight) } to right.run { subList(trimLeft, size - trimRight) }
+}
+
+inline fun paintNotebookCellBackgroundGutter(
+  editor: Editor,
+  g: Graphics,
+  r: Rectangle,
+  stripe: Color?,
+  top: Int,
+  height: Int,
+  crossinline actionBetweenBackgroundAndStripe: () -> Unit = {}
+) {
+  val appearance = editor.notebookAppearance
+  val borderWidth = appearance.getCellLeftLineWidth() + appearance.CODE_CELL_LEFT_LINE_PADDING
+  g.color = appearance.getCodeCellBackground(editor.colorsScheme)
+  g.fillRect(r.width - borderWidth, top, borderWidth, height)
+  actionBetweenBackgroundAndStripe()
+  if (stripe != null) {
+    appearance.paintCellStripe(g, r, stripe, top, height)
+  }
+}
+
+fun NotebookEditorAppearance.paintCellStripe(
+  g: Graphics,
+  r: Rectangle,
+  stripe: Color,
+  top: Int,
+  height: Int,
+) {
+  val borderWidth = getCellLeftLineWidth() + CODE_CELL_LEFT_LINE_PADDING
+  g.color = stripe
+  g.fillRect(r.width - borderWidth, top, getCellLeftLineWidth(), height)
 }
