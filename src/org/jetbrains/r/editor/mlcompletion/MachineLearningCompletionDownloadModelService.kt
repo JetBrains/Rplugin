1c9f9fbbea524a6d5cbd2d4423f5c950c6c6b6e5
Dmitrii Orekhov <dmitrii.orekhov@jetbrains.com>
2020-12-29 05:23:52 +0300

    Added ArtifactKind.ZIP Enum instance to download zip artifacts + some WIP changes to DownloadModelService
    GitOrigin-RevId: 373c680f5845ce07f35da63fe68bd500ea164060

--- src/org/jetbrains/r/editor/mlcompletion/MachineLearningCompletionDownloadModelService.kt
+++ src/org/jetbrains/r/editor/mlcompletion/MachineLearningCompletionDownloadModelService.kt
@@ -6,37 +6,55 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import org.jetbrains.idea.maven.aether.ArtifactKind
 import org.jetbrains.jps.model.library.JpsMavenRepositoryLibraryDescriptor
+import java.nio.file.Paths
+import java.util.concurrent.atomic.AtomicBoolean
 
 
 class MachineLearningCompletionDownloadModelService {
 
   companion object {
     fun getInstance() = service<MachineLearningCompletionDownloadModelService>()
+    val isBeingDownloaded = AtomicBoolean(false)
   }
 
   fun checkForUpdatesAndDownloadIfNeeded(project: Project) {
-    if (!newVersionIsAvailable()) {
+    // Check that project is with RPlugin (or maybe even in listener via separate service)
+    if (!newVersionIsAvailable(project)) {
       return
     }
-    downloadModel(project)
+    askForUpdate(project)
   }
 
-  private fun newVersionIsAvailable() : Boolean {
+  private fun newVersionIsAvailable(project: Project): Boolean {
     return true
   }
 
-  private fun downloadModel(project: Project) {
+  fun downloadModel(project: Project): Boolean {
+    if (!isBeingDownloaded.compareAndSet(false, true)) {
+      return false
+    }
+    try {
+      download(project)
+      return true
+    } // Exception processing would be nice, rethrow if progress canceled, notify of something gone wrong
+    finally {
+      isBeingDownloaded.set(false)
+    }
+  }
+
+  private fun download(project: Project) {
     val repositoryDescriptor =
       RemoteRepositoryDescription("rcompletion-models", "Models for R Machine Learning Completion",
                                   "https://packages.jetbrains.team/maven/p/mlrcc/rcompletion-models")
     val modelDescriptor =
       JpsMavenRepositoryLibraryDescriptor("org.jetbrains.r.deps.mlcompletion", "bundled_python_server", "1.0",
                                           "zip", false, emptyList())
-    val artifactKinds = setOf(ArtifactKind.ARTIFACT)
-    val roots = JarRepositoryManager.resolveAndDownload(project,
-                                                        modelDescriptor,
-                                                        artifactKinds,
-                                                        System.getProperty("user.home"),
-                                                        listOf(repositoryDescriptor))
+    val artifactKinds = setOf(ArtifactKind.ZIP_ARCHIVE)
+    JarRepositoryManager.resolveAndDownload(project,
+                                            modelDescriptor,
+                                            artifactKinds,
+                                            Paths.get(System.getProperty("user.home"), "test-maven-download").toString(),
+                                            listOf(repositoryDescriptor))
   }
+
 }
