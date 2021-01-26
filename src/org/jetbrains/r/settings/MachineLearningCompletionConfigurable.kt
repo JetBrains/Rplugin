8b968061efbc1749b08d07707d2e0719c7226598
Dmitrii Orekhov <dmitrii.orekhov@jetbrains.com>
2021-01-26 03:27:06 +0300

    WIP: check for updates button
    GitOrigin-RevId: 1cca808ba63d3ae29a2e5cfe056019d2182f9b9e

--- src/org/jetbrains/r/settings/MachineLearningCompletionConfigurable.kt
+++ src/org/jetbrains/r/settings/MachineLearningCompletionConfigurable.kt
@@ -1,10 +1,14 @@
 package org.jetbrains.r.settings
 
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.options.BoundConfigurable
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.components.dialog
 import com.intellij.ui.layout.*
 import org.jetbrains.r.RBundle
+import org.jetbrains.r.editor.mlcompletion.model.updater.MachineLearningCompletionDownloadModelService
+import javax.swing.JPanel
 
 class MachineLearningCompletionConfigurable : BoundConfigurable(RBundle.message("project.settings.ml.completion.name")) {
 
@@ -13,9 +17,14 @@ class MachineLearningCompletionConfigurable : BoundConfigurable(RBundle.message(
     private val settings = MachineLearningCompletionSettings.getInstance()
   }
 
+  private val MODALITY = ModalityState.current()
+
   override fun createPanel(): DialogPanel {
     return panel {
       titledRow(displayName) {
+        row {
+          createCheckForUpdatesButton()
+        }
         row {
           val enableCompletionCheckbox = checkBox(RBundle.message("project.settings.ml.completion.checkbox"),
                                                   settings.state::isEnabled)
@@ -28,12 +37,40 @@ class MachineLearningCompletionConfigurable : BoundConfigurable(RBundle.message(
               textField({ settings.state.host ?: "" }, settings.state::host.setter)
                 .enableIf(enableCompletionCheckbox.selected)
               label(RBundle.message("project.settings.ml.completion.port.label"))
-              intTextField(settings.state::port, columns=PORT_FIELD_WIDTH)
+              intTextField(settings.state::port, columns = PORT_FIELD_WIDTH)
                 .enableIf(enableCompletionCheckbox.selected)
             }
           }
         }
       }
+    }
+  }
+
+  private fun Cell.createCheckForUpdatesButton() = button("Check for update") {
+    // TODO: remove duplicating code with ...ProjectOpenListener
+    val modelDownloaderService = MachineLearningCompletionDownloadModelService.getInstance()
+    modelDownloaderService.getArtifactsToDownloadDescriptorsAsync { artifactsToUpdate ->
+      val size = modelDownloaderService.getArtifactsSize(artifactsToUpdate)
+      if (artifactsToUpdate.isNotEmpty()) {
+        ApplicationManager.getApplication().invokeLater( { dialog("Update title", createCheckUpdatesDialog()).show() }, MODALITY)
+      }
+      else {
+        MachineLearningCompletionDownloadModelService.isBeingDownloaded.set(false)
+      }
+    }
+  }.enableIf(object : ComponentPredicate() {
+    override fun invoke(): Boolean =
+      !MachineLearningCompletionDownloadModelService.isBeingDownloaded.get()
+
+    override fun addListener(listener: (Boolean) -> Unit) =
+      MachineLearningCompletionDownloadModelService.isBeingDownloaded.afterChange { newValue ->
+        listener(!newValue)
+      }
+  })
+
+  private fun createCheckUpdatesDialog(): JPanel = panel {
+    row {
+      label("Hello")
     }
   }
 
