463f851116e1803486e1fd96768b7442c1bad8b5
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2021-02-01 11:03:31 +0300

    remove CharsetHint
    GitOrigin-RevId: 54980004989c304c5f4df19ed206e1fa3a12f8fc

--- src/org/jetbrains/r/rmarkdown/RMarkdownFileType.kt
+++ src/org/jetbrains/r/rmarkdown/RMarkdownFileType.kt
@@ -4,22 +4,18 @@
 
 package org.jetbrains.r.rmarkdown
 
-import com.intellij.openapi.fileTypes.FileType
-import com.intellij.openapi.fileTypes.FileType.CharsetHint.*
 import com.intellij.openapi.fileTypes.LanguageFileType
 import com.intellij.openapi.vfs.CharsetToolkit
 import com.intellij.openapi.vfs.VirtualFile
 import icons.RIcons
 import org.jetbrains.annotations.NonNls
-import java.nio.charset.StandardCharsets
 import javax.swing.Icon
 
-object RMarkdownFileType : LanguageFileType(RMarkdownLanguage), FileType {
+object RMarkdownFileType : LanguageFileType(RMarkdownLanguage) {
   override fun getName() = "RMarkdown"
   @NonNls
   override fun getDescription() = "R Markdown"
   override fun getDefaultExtension() = "Rmd"
   override fun getIcon(): Icon = RIcons.RMarkdown
   override fun getCharset(file: VirtualFile, content: ByteArray): String = CharsetToolkit.UTF8
-  override fun getCharsetHint(): FileType.CharsetHint = ForcedCharset(StandardCharsets.UTF_8)
 }
