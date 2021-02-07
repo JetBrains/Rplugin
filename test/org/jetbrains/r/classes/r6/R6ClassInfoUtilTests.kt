61ceab9effa803fab6e5ab51b183be8e4f5d5376
deaglegross <deagle.gross@gmail.com>
2021-02-08 02:24:56 +0300

    add test and full r6 class definition

--- test/org/jetbrains/r/classes/r6/R6ClassInfoUtilTests.kt
+++ test/org/jetbrains/r/classes/r6/R6ClassInfoUtilTests.kt
@@ -4,16 +4,17 @@
 
 package org.jetbrains.r.classes.r6
 
+import junit.framework.TestCase
 import org.jetbrains.r.classes.RClassesUtilTestsBase
 import org.jetbrains.r.psi.api.RAssignmentStatement
 
 class R6ClassInfoUtilTests : RClassesUtilTestsBase() {
-  private val instantiateClassCode = """
+  private val fullClassCodeDefinition = """
       Car <- R6Class("Car", 
         inherit = Vehicle,
-        list(
-          weight,
-          speed,
+        public = list(
+          weight = 0,
+          speed = 0,
           accelerate = function(acc = 1){
             self${"$"}speed <- self${"$"}speed + acc
             invisible(self)
@@ -22,20 +23,37 @@ class R6ClassInfoUtilTests : RClassesUtilTestsBase() {
             self${"$"}speed <- self${"$"}speed - acc
             invisible(self)
           }
-      ))
+        ),
+        private = list(
+          engine_rpm = 1000,
+          maximize = function(rmp = 1000) {
+            self${"$"}engine_rpm <- self${"$"}engine_rpm + rmp
+            invisible(self)
+          }
+        )
+      )
       """.trimIndent()
 
   fun testGetAssociatedClassName(){
-    val rAssignmentStatement = getRootElementOfPsi(instantiateClassCode) as RAssignmentStatement
+    val rAssignmentStatement = getRootElementOfPsi(fullClassCodeDefinition) as RAssignmentStatement
     val rCallExpression = getRCallExpressionFromAssignment(rAssignmentStatement)
     val className = R6ClassInfoUtil.getAssociatedClassName(rCallExpression!!)
     assertEquals("Car", className)
   }
 
   fun testGetAssociatedSuperClassName(){
-    val rAssignmentStatement = getRootElementOfPsi(instantiateClassCode) as RAssignmentStatement
+    val rAssignmentStatement = getRootElementOfPsi(fullClassCodeDefinition) as RAssignmentStatement
     val rCallExpression = getRCallExpressionFromAssignment(rAssignmentStatement)
     val superClassName = R6ClassInfoUtil.getAssociatedSuperClassName(rCallExpression!!)
     assertEquals("Vehicle", superClassName)
+  }
+
+  fun testGetAssociatedFields(){
+    val rAssignmentStatement = getRootElementOfPsi(fullClassCodeDefinition) as RAssignmentStatement
+    val rCallExpression = getRCallExpressionFromAssignment(rAssignmentStatement)
+    val classFields = R6ClassInfoUtil.getAssociatedFields(rCallExpression!!)
+
+    TestCase.assertNotNull(classFields)
+    assertEquals(classFields!!.size, 2)
   }
 }
