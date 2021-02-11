f48ac1f74d7783269d025b2169fcb04aa73a654c
Nikolay Korolev <Nikolai.Korolev@jetbrains.com>
2021-02-11 11:25:50 +0300

    Do not show notification in case ML Completion turned off
    GitOrigin-RevId: fd0535aac2530c099dca7b6a43ff39c21218852c

--- src/org/jetbrains/r/editor/mlcompletion/model/updater/MachineLearningCompletionProjectOpenListener.kt
+++ src/org/jetbrains/r/editor/mlcompletion/model/updater/MachineLearningCompletionProjectOpenListener.kt
@@ -3,6 +3,7 @@ package org.jetbrains.r.editor.mlcompletion.model.updater
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
 import org.jetbrains.r.editor.mlcompletion.model.updater.MachineLearningCompletionNotifications.askForUpdate
+import org.jetbrains.r.settings.MachineLearningCompletionSettings
 import java.util.concurrent.atomic.AtomicBoolean
 
 class MachineLearningCompletionProjectOpenListener : ProjectManagerListener {
@@ -12,6 +13,9 @@ class MachineLearningCompletionProjectOpenListener : ProjectManagerListener {
   }
 
   override fun projectOpened(project: Project) {
+    if (!MachineLearningCompletionSettings.getInstance().state.isEnabled) {
+      return;
+    }
     if (!notifiedAnyProject.compareAndSet(false, true)) {
       return
     }
