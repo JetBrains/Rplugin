f586d490bdc6a04e7b9e41cd745a1919ae9321d5
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-04-26 10:42:50 +0200

    handle BOM and more efficient XML parsing — pass InputStream with UTF-8 encoding to XML reader instead of Reader
    GitOrigin-RevId: 2357b8b01cd0cc0c70e1521d1f11f799963f189d

--- src/org/jetbrains/r/settings/RUpdateComponent.kt
+++ src/org/jetbrains/r/settings/RUpdateComponent.kt
@@ -1,7 +1,6 @@
 /*
  * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
-
 package org.jetbrains.r.settings
 
 import com.intellij.ide.util.PropertiesComponent
@@ -21,9 +20,7 @@ import org.jetbrains.r.RPluginUtil
 import java.net.URLEncoder
 import java.util.concurrent.TimeUnit
 
-
 private const val KEY = "r.last.update.timestamp"
-
 
 class RUpdateComponent : EditorFactoryListener {
   override fun editorCreated(event: EditorFactoryEvent) {
@@ -57,7 +54,7 @@ class RUpdateComponent : EditorFactoryListener {
           PropertiesComponent.getInstance().setValue(KEY, System.currentTimeMillis().toString())
           HttpRequests.request(url).connect<Any> { request ->
             try {
-              JDOMUtil.load(request.reader)
+              JDOMUtil.load(request.inputStream)
             }
             catch (ignore: JDOMException) {
             }
