adfd87cfe47e7a21051dbb864dc9184c0aa8ed7c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-05-02 13:29:39 +0200

    update protobuf from 3.13.0 to 3.15.8
    GitOrigin-RevId: 9c556face4782b695ba2f3f7892c908125cde321

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.PrintWriter
 import java.nio.charset.StandardCharsets
 
 val grpcVersion = "1.31.1"
-val protobufVersion = "3.13.0"
+val protobufVersion = "3.15.8"
 
 val isTeamCity = System.getenv("RPLUGIN_CI") == "TeamCity"
 
