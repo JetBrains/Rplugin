815b8158c17e48b8d8153db00b3be2d97237f6d2
Konstantin Bulenkov <kb@jetbrains.com>
2021-05-10 19:37:14 +0200

    refactor markdown preview to use TextEditorWithPreview
    GitOrigin-RevId: b389275b6b1a53c1cb5d7931594f6dfb9afeccd8

--- src/org/jetbrains/r/editor/WelcomeGuideManager.kt
+++ src/org/jetbrains/r/editor/WelcomeGuideManager.kt
@@ -4,10 +4,9 @@ import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.TextEditorWithPreview
 import com.intellij.openapi.project.Project
 import com.intellij.testFramework.LightVirtualFile
-import org.intellij.plugins.markdown.ui.preview.MarkdownSplitEditor
-import org.intellij.plugins.markdown.ui.split.SplitFileEditor
 import org.jetbrains.r.RPluginUtil
 
 class WelcomeGuideManager(private val project: Project) {
@@ -26,11 +25,8 @@ class WelcomeGuideManager(private val project: Project) {
       val welcomeText = String(javaClass.getResourceAsStream("/fileTemplates/internal/welcome.md").readAllBytes())
       val welcomeFile = LightVirtualFile("R plugin - Welcome.md", welcomeText)
       invokeLater {
-        val openFile = FileEditorManager.getInstance(project).openFile(welcomeFile, true)
-        if (openFile.isNotEmpty() && openFile[0] is MarkdownSplitEditor) {
-          val markdownSplitEditor = openFile[0] as MarkdownSplitEditor
-          markdownSplitEditor.triggerLayoutChange(SplitFileEditor.SplitEditorLayout.SECOND, true)
-        }
+        welcomeFile.putUserData(TextEditorWithPreview.DEFAULT_LAYOUT_FOR_FILE, TextEditorWithPreview.Layout.SHOW_PREVIEW)
+        FileEditorManager.getInstance(project).openFile(welcomeFile, true)
       }
     }
   }
