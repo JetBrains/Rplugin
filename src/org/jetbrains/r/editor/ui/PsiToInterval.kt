d4efdfbcdde014451e577c3fd4ad306ae242d440
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2021-05-21 14:09:30 +0300

    [R-1154] add PsiToInterval mapping class
    GitOrigin-RevId: 83cfa2bb3ae7a9c230fa614bee31219bc0bd0dc5

---
+++ src/org/jetbrains/r/editor/ui/PsiToInterval.kt
@@ -0,0 +1,45 @@
+package org.jetbrains.r.editor.ui
+
+import com.intellij.openapi.editor.event.DocumentEvent
+import com.intellij.openapi.editor.event.DocumentListener
+import com.intellij.openapi.editor.impl.EditorImpl
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.notebooks.editor.NotebookCellLines
+import org.jetbrains.plugins.notebooks.editor.NotebookIntervalPointer
+import org.jetbrains.plugins.notebooks.editor.NotebookIntervalPointerFactory
+
+
+/**
+ * preserves correct mapping between psiElement and NotebookCellLines.Interval even when document is changing
+ */
+class PsiToInterval(project: Project, editor: EditorImpl, extractPsi: (NotebookCellLines.Interval) -> PsiElement?) {
+  private val psiToInterval = LinkedHashMap<PsiElement, NotebookIntervalPointer>()
+
+  init {
+    val pointerFactory = NotebookIntervalPointerFactory.get(editor)
+
+    editor.document.addDocumentListener(object : DocumentListener {
+      override fun documentChanged(event: DocumentEvent) {
+        PsiDocumentManager.getInstance(project).performForCommittedDocument(editor.document) {
+          psiToInterval.clear()
+          if (!Disposer.isDisposed(editor.disposable)) {
+            for (interval in NotebookCellLines.get(editor).intervalsIterator()) {
+              extractPsi(interval)?.let { psi ->
+                psiToInterval[psi] = pointerFactory.create(interval)
+              }
+            }
+          }
+        }
+      }
+    }, editor.disposable)
+  }
+
+  operator fun get(psiElement: PsiElement): NotebookCellLines.Interval? =
+    psiToInterval[psiElement]?.get()
+
+  fun findPsi(interval: NotebookCellLines.Interval): PsiElement? =
+    psiToInterval.entries.find { it.value.get() == interval }?.key
+}
