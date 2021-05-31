9b99a94aa5db6be7d0bb51fa674d76914d80c04f
Andrey Vokin <andrey.vokin@jetbrains.com>
2021-05-31 18:51:49 +0200

    DS-1263 "Run All" changes the cells being displayed earlier on screen
    Navigate to the selected cell after output inlay added/removed/changed
    
    GitOrigin-RevId: 4648a935bb5143ee14d7277736f3034f71b80f29

--- visualisation/src/org/jetbrains/plugins/notebooks/editor/outputs/impl/NotebookOutputInlayControllerImplUtil.kt
+++ visualisation/src/org/jetbrains/plugins/notebooks/editor/outputs/impl/NotebookOutputInlayControllerImplUtil.kt
@@ -1,8 +1,12 @@
 package org.jetbrains.plugins.notebooks.editor.outputs.impl
 
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.ScrollType
 import com.intellij.openapi.editor.ex.EditorGutterComponentEx
+import com.intellij.openapi.editor.impl.ScrollingModelImpl
 import org.jetbrains.annotations.TestOnly
 import org.jetbrains.plugins.notebooks.editor.SwingClientProperty
+import org.jetbrains.plugins.notebooks.editor.cellSelectionModel
 import org.jetbrains.plugins.notebooks.editor.outputs.NotebookOutputInlayController
 import java.awt.BorderLayout
 import javax.swing.JComponent
@@ -22,3 +26,25 @@ internal val NotebookOutputInlayController.collapsingComponents: List<Collapsing
 
 val NotebookOutputInlayController.outputComponents: List<JComponent>
   @TestOnly get() = collapsingComponents.map { it.mainComponent }
+
+fun scrollToSelectedCell(editor: Editor) {
+  if (!editor.caretModel.isUpToDate) {
+    return
+  }
+
+  val cell = editor.cellSelectionModel?.primarySelectedCell
+  if (cell != null) {
+    val lineNumber = cell.lines.first
+    val offset = editor.document.getLineEndOffset(lineNumber)
+    (editor.scrollingModel as ScrollingModelImpl).run {
+      val wasAnimationEnabled = isAnimationEnabled
+      disableAnimation()
+      try {
+        editor.scrollingModel.scrollTo(editor.offsetToLogicalPosition(offset), ScrollType.MAKE_VISIBLE)
+      }
+      finally {
+        if (wasAnimationEnabled) enableAnimation()
+      }
+    }
+  }
+}
