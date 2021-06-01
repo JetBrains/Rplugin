aae62ac0fe72012dda44cbe960a255ce02766069
Andrey Vokin <andrey.vokin@jetbrains.com>
2021-06-01 16:55:57 +0200

    DS-1263 "Run All" changes the cells being displayed earlier on screen
    Move scrolling only if output inlay was changed
    
    GitOrigin-RevId: 806db1db9768d5f02f4b29489aa681252c215aa3

--- visualisation/src/org/jetbrains/plugins/notebooks/editor/outputs/impl/InnerComponent.kt
+++ visualisation/src/org/jetbrains/plugins/notebooks/editor/outputs/impl/InnerComponent.kt
@@ -8,6 +8,7 @@ import java.awt.Component
 import java.awt.Dimension
 import javax.swing.JComponent
 import javax.swing.JPanel
+import javax.swing.SwingUtilities
 import kotlin.math.max
 import kotlin.math.min
 
@@ -50,6 +51,12 @@ internal class InnerComponent(private val editor: EditorImpl) : JPanel() {
     foldSize { maximumSize }
 
   override fun doLayout() {
+    val editorVisibleYTop = editor.scrollingModel.visibleArea.y
+    val editorVisibleYBottom = editor.scrollingModel.visibleArea.run { y + height }
+    val editorRelativeYTop = SwingUtilities.convertPoint(this, 0, 0, editor.contentComponent).y
+    val oldInsets = insets
+    val oldComponentHeights = components.map { it.height }
+
     var totalY = insets.top
     forEveryComponent(Component::getPreferredSize) { component, newWidth, newHeight ->
       component.setBounds(
@@ -61,7 +68,47 @@ internal class InnerComponent(private val editor: EditorImpl) : JPanel() {
       totalY += newHeight
     }
 
-    scrollToSelectedCell(editor)
+    val newComponentHeights = components.map { it.height }
+
+    // When components adjust their sizes, the code below compensates vertical scroll position. The topmost visible line should keep its
+    // visual position.
+    val (diff, animate) = run {
+      var animate = false
+      var diff = insets.top - oldInsets.top
+      var oldTop = editorRelativeYTop
+      var newTop = editorRelativeYTop
+      var oldBottom: Int
+      var newBottom: Int
+
+      for ((oldHeight, newHeight) in oldComponentHeights.zip(newComponentHeights)) {
+        if (editorVisibleYTop < oldTop) break
+
+        oldBottom = oldTop + oldHeight
+        newBottom = newTop + newHeight
+
+        if (
+          newHeight < oldHeight &&
+          oldTop <= editorVisibleYTop &&
+          editorVisibleYBottom <= oldBottom &&
+          editorRelativeYTop + diff <= newTop
+        ) {
+          // No code lines were visible. Centering the component.
+          diff += oldTop - editorVisibleYTop - editor.scrollingModel.visibleArea.height / 2
+          animate = true
+          break
+        }
+
+        diff += newHeight - oldHeight
+
+        oldTop = oldBottom
+        newTop = newBottom
+      }
+      diff to animate
+    }
+
+    if (diff != 0) {
+      scrollToSelectedCell(editor)
+    }
   }
 
   private inline fun foldSize(crossinline handler: Component.() -> Dimension): Dimension {
