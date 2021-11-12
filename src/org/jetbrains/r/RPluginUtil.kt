f02c1304c2d53ed50ff5e094786b98288c0145f5
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-11-12 17:02:46 +0100

    deprecate slow and bad getPluginOrPlatformByClassName API
    GitOrigin-RevId: e4924a2797c561164207c1349fd3cc3f9081a3ba

--- src/org/jetbrains/r/RPluginUtil.kt
+++ src/org/jetbrains/r/RPluginUtil.kt
@@ -1,30 +1,29 @@
 /*
  * Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
-
 package org.jetbrains.r
 
-import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.ide.plugins.cl.PluginAwareClassLoader
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.extensions.PluginDescriptor
 import com.intellij.util.io.exists
-import org.jetbrains.r.settings.RInterpreterSettings
 import java.io.File
-import java.nio.file.Paths
+import java.nio.file.Path
 
 object RPluginUtil {
   const val PLUGIN_ID = "R4Intellij"
   private const val PLUGIN_NAME = "rplugin"
 
-  fun getPlugin(): PluginDescriptor = PluginManagerCore.getPluginDescriptorOrPlatformByClassName(RInterpreterSettings::class.java.name)!!
+  fun getPlugin(): PluginDescriptor = (RPluginUtil::class.java.classLoader as PluginAwareClassLoader).pluginDescriptor
 
-  fun findFileInRHelpers(relativePath: String): File =
-    Paths.get(helpersPath, relativePath).toFile()
+  fun findFileInRHelpers(relativePath: String): File {
+    return Path.of(helpersPath, relativePath).toFile()
+  }
 
   val helpersPath: String
     get() = helperPathOrNull ?: throw IllegalStateException("Cannot find rplugin directory")
 
   val helperPathOrNull: String?
-    get() = Paths.get(PathManager.getPluginsPath(), PLUGIN_NAME).takeIf { it.exists() }?.toString() ?:
-            Paths.get(PathManager.getPreInstalledPluginsPath(), PLUGIN_NAME).takeIf { it.exists() }?.toString()
+    get() = Path.of(PathManager.getPluginsPath(), PLUGIN_NAME).takeIf { it.exists() }?.toString() ?:
+            Path.of(PathManager.getPreInstalledPluginsPath(), PLUGIN_NAME).takeIf { it.exists() }?.toString()
 }
