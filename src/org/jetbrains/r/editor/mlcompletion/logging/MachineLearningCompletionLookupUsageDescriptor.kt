cbdc8a24bc8ac760c9d80d19396b2a55a8bfa310
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2022-03-10 11:49:15 +0100

    FUS: move *CollectorExtension to *LookupUsageDescriptor (IJ-CR-20299)
    GitOrigin-RevId: 09fe0c4a9d1dbffa908da24254755b8be9ec662c

--- src/org/jetbrains/r/editor/mlcompletion/logging/MachineLearningCompletionLookupUsageDescriptor.kt
+++ src/org/jetbrains/r/editor/mlcompletion/logging/MachineLearningCompletionLookupUsageDescriptor.kt
@@ -4,19 +4,23 @@ import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.impl.LookupImpl
 import com.intellij.codeInsight.lookup.impl.LookupResultDescriptor
 import com.intellij.codeInsight.lookup.impl.LookupUsageDescriptor
+import com.intellij.codeInsight.lookup.impl.LookupUsageTracker
+import com.intellij.internal.statistic.eventLog.events.EventField
+import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.eventLog.events.EventPair
+import com.intellij.internal.statistic.service.fus.collectors.FeatureUsageCollectorExtension
 import org.jetbrains.r.editor.mlcompletion.MachineLearningCompletionUtils.isMergedLookupElement
 import org.jetbrains.r.editor.mlcompletion.MachineLearningCompletionUtils.isRLookupElement
 import org.jetbrains.r.editor.mlcompletion.MachineLearningCompletionUtils.isRMachineLearningLookupElement
-import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionCollectorExtension.Companion.contextType
-import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionCollectorExtension.Companion.mlAppVersion
-import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionCollectorExtension.Companion.mlEnabled
-import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionCollectorExtension.Companion.mlModelVersion
-import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionCollectorExtension.Companion.mlNProposedVariants
-import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionCollectorExtension.Companion.mlTimeMs
-import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionCollectorExtension.Companion.lookupElementOrigin
-import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionCollectorExtension.Companion.responseReceived
 import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionLookupStatistics.Companion.rStatistics
+import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionLookupUsageDescriptor.MachineLearningCompletionCollectorExtension.Companion.CONTEXT_TYPE
+import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionLookupUsageDescriptor.MachineLearningCompletionCollectorExtension.Companion.LOOKUP_ELEMENT_ORIGIN
+import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionLookupUsageDescriptor.MachineLearningCompletionCollectorExtension.Companion.ML_APP_VERSION
+import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionLookupUsageDescriptor.MachineLearningCompletionCollectorExtension.Companion.ML_ENABLED
+import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionLookupUsageDescriptor.MachineLearningCompletionCollectorExtension.Companion.ML_MODEL_VERSION
+import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionLookupUsageDescriptor.MachineLearningCompletionCollectorExtension.Companion.ML_N_PROPOSED_VARIANTS
+import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionLookupUsageDescriptor.MachineLearningCompletionCollectorExtension.Companion.ML_TIME_MS
+import org.jetbrains.r.editor.mlcompletion.logging.MachineLearningCompletionLookupUsageDescriptor.MachineLearningCompletionCollectorExtension.Companion.RESPONSE_RECEIVED
 
 class MachineLearningCompletionLookupUsageDescriptor : LookupUsageDescriptor {
 
@@ -32,16 +36,16 @@ class MachineLearningCompletionLookupUsageDescriptor : LookupUsageDescriptor {
       return emptyList()
     }
     val data = ArrayList<EventPair<*>>()
-    data.add(lookupElementOrigin.with(selectedElement.origin))
+    data.add(LOOKUP_ELEMENT_ORIGIN.with(selectedElement.origin))
 
     lookup.rStatistics?.let { statistics ->
-      data.add(responseReceived.with(statistics.mlCompletionResponseReceived))
-      data.add(contextType.with(statistics.completionContextType))
-      data.add(mlTimeMs.with(statistics.mlCompletionTimeMs))
-      data.add(mlNProposedVariants.with(statistics.mlCompletionNProposedVariants))
-      data.add(mlEnabled.with(statistics.mlCompletionIsEnabled))
-      data.add(mlAppVersion.with(statistics.mlCompletionAppVersion))
-      data.add(mlModelVersion.with(statistics.mlCompletionModelVersion))
+      data.add(RESPONSE_RECEIVED.with(statistics.mlCompletionResponseReceived))
+      data.add(CONTEXT_TYPE.with(statistics.completionContextType))
+      data.add(ML_TIME_MS.with(statistics.mlCompletionTimeMs))
+      data.add(ML_N_PROPOSED_VARIANTS.with(statistics.mlCompletionNProposedVariants))
+      data.add(ML_ENABLED.with(statistics.mlCompletionIsEnabled))
+      data.add(ML_APP_VERSION.with(statistics.mlCompletionAppVersion))
+      data.add(ML_MODEL_VERSION.with(statistics.mlCompletionModelVersion))
     }
     return data
   }
@@ -52,4 +56,36 @@ class MachineLearningCompletionLookupUsageDescriptor : LookupUsageDescriptor {
       isRMachineLearningLookupElement() -> RLookupElementOrigin.ML_COMPLETION
       else -> RLookupElementOrigin.ORIGINAL
     }
+
+  internal class MachineLearningCompletionCollectorExtension : FeatureUsageCollectorExtension {
+    override fun getGroupId(): String {
+      return LookupUsageTracker.GROUP_ID
+    }
+
+    override fun getEventId(): String {
+      return LookupUsageTracker.FINISHED_EVENT_ID
+    }
+
+    override fun getExtensionFields(): List<EventField<*>> {
+      return listOf<EventField<*>>(LOOKUP_ELEMENT_ORIGIN,
+                                   RESPONSE_RECEIVED,
+                                   CONTEXT_TYPE,
+                                   ML_TIME_MS,
+                                   ML_N_PROPOSED_VARIANTS,
+                                   ML_ENABLED,
+                                   ML_APP_VERSION,
+                                   ML_MODEL_VERSION)
+    }
+
+    companion object {
+      val LOOKUP_ELEMENT_ORIGIN = EventFields.Enum<MachineLearningCompletionLookupUsageDescriptor.RLookupElementOrigin>("r_lookup_element_origin")
+      val RESPONSE_RECEIVED = EventFields.Boolean("r_ml_response_received")
+      val CONTEXT_TYPE = EventFields.Enum<MachineLearningCompletionLookupStatistics.CompletionContextType>("r_context_type")
+      val ML_TIME_MS = EventFields.Int("r_ml_time_ms")
+      val ML_N_PROPOSED_VARIANTS = EventFields.Int("r_ml_n_proposed_variants")
+      val ML_ENABLED = EventFields.Boolean("r_ml_enabled")
+      val ML_APP_VERSION = EventFields.StringValidatedByRegexp("r_ml_app_version", "version")
+      val ML_MODEL_VERSION = EventFields.StringValidatedByRegexp("r_ml_app_version", "version")
+    }
+  }
 }
