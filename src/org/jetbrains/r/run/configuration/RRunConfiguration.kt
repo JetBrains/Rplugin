0a792de68a3e918505c1d6db7f80712332a9a1ba
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-03-22 15:44:30 +0100

    cleanup
    GitOrigin-RevId: 8cc592b4a987143ede26092986d3678393f1abd2

--- src/org/jetbrains/r/run/configuration/RRunConfiguration.kt
+++ src/org/jetbrains/r/run/configuration/RRunConfiguration.kt
@@ -2,7 +2,10 @@ package org.jetbrains.r.run.configuration
 
 import com.intellij.execution.Executor
 import com.intellij.execution.configuration.EnvironmentVariablesData
-import com.intellij.execution.configurations.*
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.configurations.RunConfigurationBase
+import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
@@ -16,7 +19,7 @@ class RRunConfiguration(project: Project, factory: ConfigurationFactory): RunCon
   var scriptArguments: String = ""
   var environmentVariablesData: EnvironmentVariablesData = EnvironmentVariablesData.DEFAULT
 
-  override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState? = RCommandLineRunningState(environment)
+  override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState = RCommandLineRunningState(environment)
 
   override fun getConfigurationEditor(): SettingsEditor<out RunConfiguration> = RRunConfigurationEditor()
 
