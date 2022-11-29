4e675578ee21a642e1fc12b4a33672c476ef18de
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2022-11-29 18:06:58 +0100

    Port RGraphicsSettingsDialogEx to Kotlin UI DSL 2
    GitOrigin-RevId: e7d0044543a012085161de133fc43e15f544acaf

--- src/org/jetbrains/r/run/graphics/ui/RGraphicsSettingsDialogEx.kt
+++ src/org/jetbrains/r/run/graphics/ui/RGraphicsSettingsDialogEx.kt
@@ -1,26 +1,34 @@
 package org.jetbrains.r.run.graphics.ui
 
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.ui.layout.*
+import com.intellij.ui.dsl.builder.RightGap
+import com.intellij.ui.dsl.builder.bindIntText
+import com.intellij.ui.dsl.builder.columns
+import com.intellij.ui.dsl.builder.panel
 import org.jetbrains.r.RBundle
 import javax.swing.JComponent
+
+private const val INPUT_COLUMN_COUNT = 7
+private val INPUT_RANGE = IntRange(1, 9999)
 
 class RGraphicsSettingsDialogEx(private var resolution: Int, private val onOk: (Int) -> Unit) :
   DialogWrapper(null, true)
 {
   init {
-    setResizable(false)
-    title = TITLE
+    isResizable = false
+    title = RBundle.message("graphics.panel.settings.dialog.title")
     init()
   }
 
   override fun createCenterPanel(): JComponent {
-    val self = this
     return panel {
-      titledRow(PLOTS_TEXT) {
-        row(RESOLUTION_TEXT) {
-          intTextField(self::resolution, INPUT_COLUMN_COUNT, INPUT_RANGE)
-          label(DPI_TEXT)
+      group(RBundle.message("chunk.graphics.settings.dialog.for.all.plots")) {
+        row(RBundle.message("graphics.panel.settings.dialog.resolution")) {
+          intTextField(INPUT_RANGE)
+            .columns(INPUT_COLUMN_COUNT)
+            .gap(RightGap.SMALL)
+            .bindIntText(::resolution)
+          label(RBundle.message("graphics.panel.settings.dialog.dpi"))
         }
       }
     }
@@ -32,14 +40,6 @@ class RGraphicsSettingsDialogEx(private var resolution: Int, private val onOk: (
   }
 
   companion object {
-    private const val INPUT_COLUMN_COUNT = 7
-    private val INPUT_RANGE = IntRange(1, 9999)
-
-    private val TITLE = RBundle.message("graphics.panel.settings.dialog.title")
-
-    private val PLOTS_TEXT = RBundle.message("chunk.graphics.settings.dialog.for.all.plots")
-    private val RESOLUTION_TEXT = RBundle.message("graphics.panel.settings.dialog.resolution")
-    private val DPI_TEXT = RBundle.message("graphics.panel.settings.dialog.dpi")
 
     fun show(resolution: Int, onOk: (Int) -> Unit) {
       RGraphicsSettingsDialogEx(resolution, onOk).show()
