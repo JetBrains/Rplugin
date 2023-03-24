60fe546b77d3947f43a4183dfc0d4478933686dc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-03-24 15:08:47 +0100

    use komihash for icon classes to align rasterized key with a runtime cache key (same algo)
    GitOrigin-RevId: fec04acb91d4a69d5c50571d3408c9b0ce3fddd4

--- gen/icons/RIcons.java
+++ gen/icons/RIcons.java
@@ -13,47 +13,47 @@ public final class RIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, RIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon ChangeWorkingDirectory = load("icons/changeWorkingDirectory.svg", -1127451687, 2);
+  /** 16x16 */ public static final @NotNull Icon ChangeWorkingDirectory = load("icons/changeWorkingDirectory.svg", -741597682, 2);
 
   public static final class Debug {
-    /** 16x16 */ public static final @NotNull Icon StepIntoMyCode = load("icons/debug/StepIntoMyCode.svg", 25082120, 2);
+    /** 16x16 */ public static final @NotNull Icon StepIntoMyCode = load("icons/debug/StepIntoMyCode.svg", -301389101, 2);
   }
 
   public static final class FileTypes {
-    /** 16x16 */ public static final @NotNull Icon HtmlNotebook = load("icons/fileTypes/htmlNotebook.svg", -1774470494, 0);
-    /** 16x16 */ public static final @NotNull Icon Presentation = load("icons/fileTypes/presentation.svg", -140078036, 0);
-    /** 16x16 */ public static final @NotNull Icon RMarkdown = load("icons/fileTypes/rMarkdown.svg", 958350380, 0);
-    /** 16x16 */ public static final @NotNull Icon Shiny = load("icons/fileTypes/shiny.svg", -1942819096, 0);
+    /** 16x16 */ public static final @NotNull Icon HtmlNotebook = load("icons/fileTypes/htmlNotebook.svg", -1191597828, 0);
+    /** 16x16 */ public static final @NotNull Icon Presentation = load("icons/fileTypes/presentation.svg", 555635250, 0);
+    /** 16x16 */ public static final @NotNull Icon RMarkdown = load("icons/fileTypes/rMarkdown.svg", -1194404262, 0);
+    /** 16x16 */ public static final @NotNull Icon Shiny = load("icons/fileTypes/shiny.svg", -1176787047, 0);
   }
 
-  /** 16x16 */ public static final @NotNull Icon MachineLearning = load("icons/machineLearning.svg", 166393231, 0);
+  /** 16x16 */ public static final @NotNull Icon MachineLearning = load("icons/machineLearning.svg", -239923723, 0);
 
   public static final class Packages {
-    /** 16x16 */ public static final @NotNull Icon DocumentsRoot = load("icons/packages/documentsRoot.svg", 2038110972, 0);
-    /** 16x16 */ public static final @NotNull Icon RTest = load("icons/packages/rTest.svg", -294333552, 0);
-    /** 16x16 */ public static final @NotNull Icon UpgradeAll = load("icons/packages/upgradeAll.svg", -1870405887, 2);
+    /** 16x16 */ public static final @NotNull Icon DocumentsRoot = load("icons/packages/documentsRoot.svg", -2097008969, 0);
+    /** 16x16 */ public static final @NotNull Icon RTest = load("icons/packages/rTest.svg", 1105927713, 0);
+    /** 16x16 */ public static final @NotNull Icon UpgradeAll = load("icons/packages/upgradeAll.svg", 1967176985, 2);
   }
 
-  /** 16x16 */ public static final @NotNull Icon R = load("icons/r.svg", 1168821137, 0);
-  /** 16x16 */ public static final @NotNull Icon Render = load("icons/render.svg", -2075887672, 2);
-  /** 16x16 */ public static final @NotNull Icon Render_dark1 = load("icons/render_dark-1.svg", -1402242468, 0);
-  /** 16x16 */ public static final @NotNull Icon RMarkdown = load("icons/rMarkdown.svg", 958350380, 0);
-  /** 16x16 */ public static final @NotNull Icon ROpenTest = load("icons/rOpenTest.svg", 1028418747, 2);
+  /** 16x16 */ public static final @NotNull Icon R = load("icons/r.svg", 224739393, 0);
+  /** 16x16 */ public static final @NotNull Icon Render = load("icons/render.svg", -967889038, 2);
+  /** 16x16 */ public static final @NotNull Icon Render_dark1 = load("icons/render_dark-1.svg", -689555824, 0);
+  /** 16x16 */ public static final @NotNull Icon RMarkdown = load("icons/rMarkdown.svg", -1194404262, 0);
+  /** 16x16 */ public static final @NotNull Icon ROpenTest = load("icons/rOpenTest.svg", -334127438, 2);
 
   public static final class Run {
-    /** 16x16 */ public static final @NotNull Icon DebugCurrent = load("icons/run/debugCurrent.svg", 1380814703, 2);
-    /** 16x16 */ public static final @NotNull Icon RestartJob = load("icons/run/restartJob.svg", -733785055, 2);
-    /** 16x16 */ public static final @NotNull Icon RunAbove = load("icons/run/runAbove.svg", -1825110854, 2);
-    /** 16x16 */ public static final @NotNull Icon RunBelow = load("icons/run/runBelow.svg", 974741223, 2);
-    /** 16x16 */ public static final @NotNull Icon RunCurrent = load("icons/run/runCurrent.svg", -1237660428, 2);
-    /** 16x16 */ public static final @NotNull Icon RunJob = load("icons/run/runJob.svg", 1954565914, 2);
+    /** 16x16 */ public static final @NotNull Icon DebugCurrent = load("icons/run/debugCurrent.svg", 1582534527, 2);
+    /** 16x16 */ public static final @NotNull Icon RestartJob = load("icons/run/restartJob.svg", 834428329, 2);
+    /** 16x16 */ public static final @NotNull Icon RunAbove = load("icons/run/runAbove.svg", 1524258236, 2);
+    /** 16x16 */ public static final @NotNull Icon RunBelow = load("icons/run/runBelow.svg", -506207761, 2);
+    /** 16x16 */ public static final @NotNull Icon RunCurrent = load("icons/run/runCurrent.svg", -813897266, 2);
+    /** 16x16 */ public static final @NotNull Icon RunJob = load("icons/run/runJob.svg", -1903768841, 2);
   }
 
   public static final class ToolWindow {
-    /** 13x13 */ public static final @NotNull Icon RConsole = load("icons/toolWindow/RConsole.svg", -1252060713, 2);
-    /** 13x13 */ public static final @NotNull Icon RGraph = load("icons/toolWindow/RGraph.svg", 1167846473, 2);
-    /** 13x13 */ public static final @NotNull Icon RHtml = load("icons/toolWindow/RHtml.svg", 1916996486, 2);
-    /** 13x13 */ public static final @NotNull Icon RPackages = load("icons/toolWindow/RPackages.svg", -1421735220, 2);
-    /** 13x13 */ public static final @NotNull Icon RTable = load("icons/toolWindow/RTable.svg", -318631751, 2);
+    /** 13x13 */ public static final @NotNull Icon RConsole = load("icons/toolWindow/RConsole.svg", 1894395341, 2);
+    /** 13x13 */ public static final @NotNull Icon RGraph = load("icons/toolWindow/RGraph.svg", 146929391, 2);
+    /** 13x13 */ public static final @NotNull Icon RHtml = load("icons/toolWindow/RHtml.svg", 443209157, 2);
+    /** 13x13 */ public static final @NotNull Icon RPackages = load("icons/toolWindow/RPackages.svg", 698210638, 2);
+    /** 13x13 */ public static final @NotNull Icon RTable = load("icons/toolWindow/RTable.svg", -1514986702, 2);
   }
 }
