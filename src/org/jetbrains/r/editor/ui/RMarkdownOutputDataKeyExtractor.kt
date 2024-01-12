4883903e39ebdc96bc9633cc4531162b8ba31bbe
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2024-01-12 18:04:49 +0400

    [pycharm] PY-66053 Jupyter: add statistic
    GitOrigin-RevId: 01affd99d082328c16470dc689980a355e33095a

--- src/org/jetbrains/r/editor/ui/RMarkdownOutputDataKeyExtractor.kt
+++ src/org/jetbrains/r/editor/ui/RMarkdownOutputDataKeyExtractor.kt
@@ -4,11 +4,13 @@ import com.intellij.openapi.editor.impl.EditorImpl
 import org.jetbrains.plugins.notebooks.visualization.NotebookCellLines
 import org.jetbrains.plugins.notebooks.visualization.outputs.NotebookOutputDataKey
 import org.jetbrains.plugins.notebooks.visualization.outputs.NotebookOutputDataKeyExtractor
+import org.jetbrains.plugins.notebooks.visualization.outputs.statistic.NotebookOutputKeyType
 import org.jetbrains.plugins.notebooks.visualization.r.inlays.InlayOutput
 import org.jetbrains.r.rendering.chunk.ChunkPath
 import org.jetbrains.r.rendering.chunk.RMarkdownInlayDescriptor
 
 data class RMarkdownInlayOutputDataKey(val inlayOutput: InlayOutput): NotebookOutputDataKey {
+  override val statisticKey = NotebookOutputKeyType.R_MARKDOWN
   override fun getContentForDiffing(): String = inlayOutput.data
 
 }
