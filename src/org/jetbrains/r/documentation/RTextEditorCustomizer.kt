4850da873110d8e440903547b51baab05a76ab97
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-03-20 16:22:38 +0100

    IJPL-846 don't execute TextEditorCustomizer.customize in EDT
    GitOrigin-RevId: 9976877a77fbbf4258c6bcd522e31cf9a43d14fb

--- src/org/jetbrains/r/documentation/RTextEditorCustomizer.kt
+++ src/org/jetbrains/r/documentation/RTextEditorCustomizer.kt
@@ -1,16 +1,22 @@
 package org.jetbrains.r.documentation
 
 import com.intellij.codeInsight.documentation.render.DocRenderManager
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.fileEditor.impl.text.TextEditorCustomizer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.r.rinterop.RSourceFileManager
 
 private class RTextEditorCustomizer : TextEditorCustomizer {
-  override fun customize(textEditor: TextEditor) {
-    val file = textEditor.editor.virtualFile ?: FileDocumentManager.getInstance().getFile(textEditor.editor.document)
+  override suspend fun execute(textEditor: TextEditor) {
+    val file = textEditor.editor.virtualFile ?: serviceAsync<FileDocumentManager>().getFile(textEditor.editor.document)
     if (file != null && RSourceFileManager.isTemporary(file)) {
-      DocRenderManager.setDocRenderingEnabled(textEditor.editor, true)
+      withContext(Dispatchers.EDT) {
+        DocRenderManager.setDocRenderingEnabled(textEditor.editor, true)
+      }
     }
   }
 }
