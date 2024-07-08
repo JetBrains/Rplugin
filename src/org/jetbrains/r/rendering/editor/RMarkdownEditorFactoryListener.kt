5ac34316b86e77d244da811588dc5115b1301526
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2024-07-08 14:52:06 +0200

    [R] move some repeating code to RMarkdownVirtualFile object
    (cherry picked from commit 34fba0fb8ad236a01906c79e91f3c2bd2f2c4fbb)
    
    IJ-CR-139381
    
    GitOrigin-RevId: 1715e94f3aa46e0f239b5c03133084ea20ea5f92

--- src/org/jetbrains/r/rendering/editor/RMarkdownEditorFactoryListener.kt
+++ src/org/jetbrains/r/rendering/editor/RMarkdownEditorFactoryListener.kt
@@ -1,43 +1,21 @@
 package org.jetbrains.r.rendering.editor
 
-import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
 import com.intellij.openapi.editor.impl.EditorImpl
-import com.intellij.openapi.fileTypes.FileTypeRegistry
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiDocumentManager
 import org.jetbrains.plugins.notebooks.visualization.NotebookCellInlayManager
 import org.jetbrains.plugins.notebooks.visualization.NotebookCellLinesProvider
 import org.jetbrains.plugins.notebooks.visualization.NotebookEditorAppearanceProvider
-import org.jetbrains.r.quarto.QuartoFileType
-import org.jetbrains.r.rmarkdown.RMarkdownFileType
+import org.jetbrains.r.rmarkdown.RMarkdownVirtualFile
 
 class RMarkdownEditorFactoryListener : EditorFactoryListener {
 
   override fun editorCreated(event: EditorFactoryEvent) {
-    val virtualFile = getVirtualFile(event.editor)
-    if (virtualFile != null && isRMarkdownOrQuarto(virtualFile)) {
-      onRMarkdownFileEditorCreated(event.editor as EditorImpl)
-    }
-  }
-
-  companion object {
-    fun onRMarkdownFileEditorCreated(editor: EditorImpl) {
+    val editor = event.editor as? EditorImpl ?: return
+    if (RMarkdownVirtualFile.hasVirtualFile(editor)) {
       NotebookCellLinesProvider.install(editor)
       NotebookEditorAppearanceProvider.install(editor)
       NotebookCellInlayManager.install(editor, shouldCheckInlayOffsets = false)
-    }
-
-    fun isRMarkdownOrQuarto(virtualFile: VirtualFile): Boolean {
-      val fileTypeRegistryInstance = FileTypeRegistry.getInstance()
-      return fileTypeRegistryInstance.isFileOfType(virtualFile, RMarkdownFileType) ||
-             fileTypeRegistryInstance.isFileOfType(virtualFile, QuartoFileType)
-    }
-
-    fun getVirtualFile(editor: Editor): VirtualFile? {
-      val project = editor.project ?: return null
-      return PsiDocumentManager.getInstance(project).getPsiFile(editor.document)?.virtualFile
     }
   }
 }
