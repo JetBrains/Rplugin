2e6b794bc7fa629c8c9159aaf1e9590b5e6dffc7
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2024-07-19 15:00:49 +0200

    [R-1562] rm InlaysManager, init InlayDimensions in RMarkdownEditorFactoryListener
    GitOrigin-RevId: bd4e4cdc8b03a36055b50d257f5e4f99921eb34d

--- src/org/jetbrains/r/rendering/editor/RMarkdownEditorFactoryListener.kt
+++ src/org/jetbrains/r/rendering/editor/RMarkdownEditorFactoryListener.kt
@@ -6,16 +6,18 @@ import com.intellij.openapi.editor.impl.EditorImpl
 import org.jetbrains.plugins.notebooks.visualization.NotebookCellInlayManager
 import org.jetbrains.plugins.notebooks.visualization.NotebookCellLinesProvider
 import org.jetbrains.plugins.notebooks.visualization.NotebookEditorAppearanceProvider
+import org.jetbrains.plugins.notebooks.visualization.r.inlays.InlayDimensions
 import org.jetbrains.r.rmarkdown.RMarkdownVirtualFile
 
 class RMarkdownEditorFactoryListener : EditorFactoryListener {
 
   override fun editorCreated(event: EditorFactoryEvent) {
     val editor = event.editor as? EditorImpl ?: return
-    if (RMarkdownVirtualFile.hasVirtualFile(editor)) {
-      NotebookCellLinesProvider.install(editor)
-      NotebookEditorAppearanceProvider.install(editor)
-      NotebookCellInlayManager.install(editor, shouldCheckInlayOffsets = false)
-    }
+    if (!RMarkdownVirtualFile.hasVirtualFile(editor)) return
+
+    InlayDimensions.init(editor)
+    NotebookCellLinesProvider.install(editor)
+    NotebookEditorAppearanceProvider.install(editor)
+    NotebookCellInlayManager.install(editor, shouldCheckInlayOffsets = false)
   }
 }
