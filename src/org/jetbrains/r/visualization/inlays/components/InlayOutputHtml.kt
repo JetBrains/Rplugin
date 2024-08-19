3f411ed19294ec9c7773cce027b9b8703c66d0dd
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2024-08-19 15:16:12 +0200

    [R-1567] move InlayOutputHtml with jcef to Rplugin
    GitOrigin-RevId: dadb5de7733a44b62bdb797898f0c07957f2e3bb

---
+++ src/org/jetbrains/r/visualization/inlays/components/InlayOutputHtml.kt
@@ -0,0 +1,97 @@
+package org.jetbrains.r.visualization.inlays.components
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.invokeLater
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.Disposer
+import com.intellij.ui.jcef.JBCefBrowser
+import com.intellij.ui.jcef.JBCefBrowserBase
+import com.intellij.ui.jcef.JBCefJSQuery
+import org.jetbrains.plugins.notebooks.visualization.r.VisualizationBundle
+import org.jetbrains.plugins.notebooks.visualization.r.inlays.components.GithubMarkdownCss
+import org.jetbrains.plugins.notebooks.visualization.r.inlays.components.InlayOutput
+import org.jetbrains.plugins.notebooks.visualization.r.inlays.components.SaveOutputAction
+import java.util.function.Function
+import javax.swing.SwingUtilities
+
+class InlayOutputHtml(parent: Disposable, editor: Editor)
+  : InlayOutput(parent, editor, loadActions(SaveOutputAction.Companion.ID)), InlayOutput.WithSaveAs {
+
+  private val jbBrowser: JBCefBrowser = JBCefBrowser().also { Disposer.register(parent, it) }
+  private val heightJsCallback = JBCefJSQuery.create(jbBrowser as JBCefBrowserBase)
+  private val saveJsCallback = JBCefJSQuery.create(jbBrowser as JBCefBrowserBase)
+  private var height: Int = 0
+
+  init {
+    heightJsCallback.addHandler {
+      val height = it.toInt()
+      if (this.height != height) {
+        this.height = height
+        invokeLater {
+          SwingUtilities.invokeLater {
+            onHeightCalculated?.invoke(height)
+          }
+        }
+      }
+      JBCefJSQuery.Response("OK")
+    }
+    Disposer.register(jbBrowser, heightJsCallback)
+    toolbarPane.dataComponent = jbBrowser.component
+  }
+
+  override fun acceptType(type: String): Boolean {
+    return type == "HTML" || type == "URL"
+  }
+
+  override fun clear() {}
+
+  private fun notifySize() {
+    jbBrowser.cefBrowser.executeJavaScript(
+      "var body = document.body,"
+      + "html = document.documentElement;"
+      + "var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight , html.offsetHeight );"
+      + "window.${heightJsCallback.funcName}({request: String(height)});",
+      jbBrowser.cefBrowser.url, 0
+    )
+  }
+
+  override fun addData(data: String, type: String) {
+    val isUrl = data.startsWith("file://") || data.startsWith("http://") || data.startsWith("https://")
+    if (isUrl) {
+      jbBrowser.loadURL(data)
+    }
+    else {
+      jbBrowser.loadHTML("<head><style>" + GithubMarkdownCss.Companion.css + " </style></head><body>" + data + "</body>")
+    }
+    jbBrowser.jbCefClient.addLoadHandler(object : org.cef.handler.CefLoadHandlerAdapter() {
+      override fun onLoadingStateChange(browser: org.cef.browser.CefBrowser?, isLoading: Boolean, canGoBack: Boolean, canGoForward: Boolean) {
+        notifySize()
+      }
+    }, jbBrowser.cefBrowser)
+  }
+
+  // For HTML component no need to scroll to top, because it is not scrolling to end.
+  override fun scrollToTop() {}
+
+  override fun getCollapsedDescription(): String {
+    return "html output"
+  }
+
+  override fun saveAs() {
+    val title = VisualizationBundle.message("inlay.action.export.as.txt.title")
+    val description = VisualizationBundle.message("inlay.action.exports.range.csv.description")
+    saveWithFileChooser(title, description, arrayOf("txt"), "output") { destination ->
+      saveJsCallback.addHandler(object : Function<String, JBCefJSQuery.Response> {
+        override fun apply(selection: String): JBCefJSQuery.Response {
+          destination.bufferedWriter().use { out ->
+            out.write(selection)
+          }
+          saveJsCallback.removeHandler(this)
+          return JBCefJSQuery.Response("OK")
+        }
+      })
+      jbBrowser.cefBrowser.executeJavaScript("window.${saveJsCallback.funcName}({request: window.getSelection().toString()})",
+                                             jbBrowser.cefBrowser.url, 0)
+    }
+  }
+}
