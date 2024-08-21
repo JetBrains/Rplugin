9c67c47386a1f8cba45e2357ba41519754fa987a
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2024-08-21 14:03:13 +0200

    [R-1567] simplify code of InlayOutputProgressStatus
    GitOrigin-RevId: 0e89356167402896c16af5f9773afb484321af3b

--- src/org/jetbrains/r/visualization/inlays/components/InlayOutputProgressStatus.kt
+++ src/org/jetbrains/r/visualization/inlays/components/InlayOutputProgressStatus.kt
@@ -1,10 +1,13 @@
 package org.jetbrains.r.visualization.inlays.components
 
+import com.intellij.ide.ui.laf.darcula.ui.DarculaProgressBarUI
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.progress.util.ColorProgressBar
+import com.intellij.ui.JBColor
 import com.intellij.ui.scale.JBUIScale
 import org.jetbrains.plugins.notebooks.ui.visualization.notebookAppearance
 import java.awt.BorderLayout
+import java.awt.Color
 import javax.swing.*
 
 object InlayOutputProgressStatus {
@@ -17,22 +20,35 @@ object InlayOutputProgressStatus {
     val progressPanel = JPanel(BorderLayout()).apply {
       background = editor.notebookAppearance.getTextOutputBackground(editor.colorsScheme)
     }
-    var progressBar: JProgressBar? = null
-    if (progressStatus.progress != RProgressStatus.STOPPED_OK) {
-      progressBar = JProgressBar(0, 100)
-      progressBar.setUI(InlayProgressBarUI(progressStatus.progress))
-      if (progressStatus.progress == RProgressStatus.RUNNING) {
-        progressBar.isIndeterminate = true
-        progressBar.foreground = ColorProgressBar.GREEN
+    val progressBar: JProgressBar? =
+      when (progressStatus.progress) {
+        RProgressStatus.RUNNING ->
+          JProgressBar(0, 100).also { progressBar ->
+            progressBar.setUI(InlayProgressBarUI(progressStatus.progress))
+            progressBar.isIndeterminate = true
+            progressBar.foreground = ColorProgressBar.GREEN
+          }
+        RProgressStatus.STOPPED_ERROR ->
+          JProgressBar(0, 100).also { progressBar ->
+            progressBar.setUI(InlayProgressBarUI(progressStatus.progress))
+            progressBar.value = 100
+          }
+        RProgressStatus.STOPPED_OK -> null
       }
-      else if (progressStatus.progress == RProgressStatus.STOPPED_ERROR) {
-        progressBar.value = 100
-      }
-    }
+
     val label = JLabel(progressStatus.statusText)
     label.border = BorderFactory.createEmptyBorder(TEXT_BORDER, TEXT_BORDER, TEXT_BORDER, 0)
     progressBar?.let { progressPanel.add(it, BorderLayout.PAGE_START) }
     progressPanel.add(label, BorderLayout.CENTER)
     return progressPanel
+  }
+}
+
+private class InlayProgressBarUI(private val status: RProgressStatus) : DarculaProgressBarUI() {
+  override fun getFinishedColor(c: JComponent): Color = if (status == RProgressStatus.STOPPED_OK) {
+    JBColor.GRAY
+  }
+  else {
+    JBColor.RED
   }
 }
