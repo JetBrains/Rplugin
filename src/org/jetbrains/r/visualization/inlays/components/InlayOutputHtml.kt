6ebec61abd7409d8a71d0863c9f01df3ea0b3493
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2024-08-21 16:53:38 +0200

    [R-1567] move GithubMarkdownCss to R plugin
    GitOrigin-RevId: 7fd799b0a9305197f59053076da3d362442843f8

--- src/org/jetbrains/r/visualization/inlays/components/InlayOutputHtml.kt
+++ src/org/jetbrains/r/visualization/inlays/components/InlayOutputHtml.kt
@@ -8,7 +8,6 @@ import com.intellij.ui.jcef.JBCefBrowser
 import com.intellij.ui.jcef.JBCefBrowserBase
 import com.intellij.ui.jcef.JBCefJSQuery
 import org.jetbrains.plugins.notebooks.visualization.r.VisualizationBundle
-import org.jetbrains.plugins.notebooks.visualization.r.inlays.components.GithubMarkdownCss
 import java.util.function.Function
 import javax.swing.SwingUtilities
 
