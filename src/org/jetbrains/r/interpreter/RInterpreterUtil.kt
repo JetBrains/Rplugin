d786ea7b82112a546859d8415e0ba098fca699b9
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2024-08-23 17:09:25 +0200

    [R] remove upper bound for supported versions
    GitOrigin-RevId: cbda5f2e58c71bfb07244439f7512d4c8e99d94a

--- src/org/jetbrains/r/interpreter/RInterpreterUtil.kt
+++ src/org/jetbrains/r/interpreter/RInterpreterUtil.kt
@@ -102,7 +102,7 @@ object RInterpreterUtil {
   }
 
   fun isSupportedVersion(version: Version?): Boolean {
-    return version != null && version.isOrGreaterThan(3, 4) && version.lessThan(4, 5)
+    return version != null && version.isOrGreaterThan(3, 4)
   }
 
   fun suggestAllInterpreters(enabledOnly: Boolean, localOnly: Boolean = false): List<RInterpreterInfo> {
