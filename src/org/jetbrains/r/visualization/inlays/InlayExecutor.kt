75d4c787093271afe339a6ca76b6e29611224bd9
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2025-05-14 19:24:23 +0200

    [R] convert InlayExecutor to object
    GitOrigin-RevId: e0c4984791ba00a20c3947b7cabc611b67529925

--- src/org/jetbrains/r/visualization/inlays/InlayExecutor.kt
+++ src/org/jetbrains/r/visualization/inlays/InlayExecutor.kt
@@ -4,19 +4,9 @@
 
 package org.jetbrains.r.visualization.inlays
 
-import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.service
 import kotlinx.coroutines.sync.Mutex
 
 
-@Service(Service.Level.APP)
-internal class InlayExecutor {
-  private val mutex = Mutex()
-
-  companion object {
-    fun getInstance(): InlayExecutor = service()
-
-    val mutex: Mutex
-      get() = getInstance().mutex
-  }
+object InlayExecutor {
+  val mutex = Mutex()
 }
